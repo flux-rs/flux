9029222d39b7eed01c904a9e51507400e1006a49
Christian Poveda <git@christianpoveda.xyz>
2020-10-11 19:18:42 +0200

    move compiler to its own folder

--- liquid-rust/src/main.rs
+++ liquid-rust/src/main.rs
@@ -1,27 +1,43 @@
 #![feature(rustc_private)]
 
 extern crate rustc_driver;
+extern crate rustc_hir;
 extern crate rustc_interface;
+extern crate rustc_mir;
 
 use rustc_driver::{Callbacks, Compilation};
+use rustc_hir::ItemKind;
 use rustc_interface::{interface::Compiler, Queries};
+use rustc_mir::util::write_mir_pretty;
 
 struct CompilerCalls;
 
 impl Callbacks for CompilerCalls {
     fn after_analysis<'tcx>(
         &mut self,
-        compiler: &Compiler,
+        _compiler: &Compiler,
         queries: &'tcx Queries<'tcx>,
     ) -> Compilation {
+        queries.global_ctxt().unwrap().peek_mut().enter(|tcx| {
+            let hir = tcx.hir();
+
+            for (&hir_id, item) in &tcx.hir().krate().items {
+                if let ItemKind::Fn(_, _, _) = item.kind {
+                    let def_id = hir.local_def_id(hir_id);
+                    // let _mir = tcx.optimized_mir(def_id);
+                    println!("{:?}", hir.attrs(hir_id));
+                    write_mir_pretty(tcx, Some(def_id.to_def_id()), &mut std::io::stdout())
+                        .unwrap();
+                }
+            }
+        });
+
         Compilation::Continue
     }
 }
 
 fn compile_time_sysroot() -> Option<String> {
     if option_env!("RUSTC_STAGE").is_some() {
-        // This is being built as part of rustc, and gets shipped with rustup.
-        // We can rely on the sysroot computation in librustc_session.
         return None;
     }
     let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
@@ -38,7 +54,6 @@ fn run_compiler(mut args: Vec<String>, callbacks: &mut (dyn Callbacks + Send)) -
     if let Some(sysroot) = compile_time_sysroot() {
         let sysroot_flag = "--sysroot";
         if !args.iter().any(|e| e == sysroot_flag) {
-            // We need to overwrite the default that librustc_session would compute.
             args.push(sysroot_flag.to_owned());
             args.push(sysroot);
         }
