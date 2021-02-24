20cd5a2113e121abc009300c777a35d9686fbf86
Christian Poveda <git@christianpoveda.xyz>
2021-02-24 14:22:50 +0100

    add skeleton for fixpoint crate

--- liquid-rust-fixpoint/src/lib.rs
+++ liquid-rust-fixpoint/src/lib.rs
@@ -1,1 +1,34 @@
+use liquid_rust_lrir::ty::{UnOp, BinOp};
 
+pub enum Constraint {
+    Pred(Pred),
+    Conj(Vec<Self>),
+    ForAll(Sort, Pred, Box<Self>),
+    Guard(Pred, Box<Self>),
+}
+
+pub enum Sort {
+    Int,
+    Bool,
+}
+
+pub enum Pred {
+    Const(Constant),
+    Var(Variable),
+    BinaryOp(BinOp, Box<Self>, Box<Self>),
+    UnaryOp(UnOp, Box<Self>),
+}
+
+pub enum Constant {
+    Bool(bool),
+    Int(u128),
+    Unit,
+}
+
+pub struct Variable {
+    index: usize,
+}
+
+pub struct Fixpoint {
+
+}
