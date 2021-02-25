c417b349bf8755bad8820491d48fcd4938541c37
Christian Poveda <git@christianpoveda.xyz>
2021-02-25 10:35:42 +0100

    fix abs test

--- tests/pass/abs.rs
+++ tests/pass/abs.rs
@@ -1,7 +1,7 @@
 #![feature(register_tool)]
 #![register_tool(liquid)]
 
-#[liquid::ty(fn (x: isize) -> {b: isize | b > 0isize})]
+#[liquid::ty(fn (x: isize) -> {b: isize | b >= 0isize})]
 pub fn abs(x: isize) -> isize {
     if x > 0 {
         x
