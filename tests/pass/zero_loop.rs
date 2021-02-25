328070852e0d416bc2201e9fadcbcd4fd44ad2e8
Christian Poveda <git@christianpoveda.xyz>
2021-02-25 13:16:10 +0100

    fix loop test

--- tests/pass/zero_loop.rs
+++ tests/pass/zero_loop.rs
@@ -1,7 +1,7 @@
 #![feature(register_tool)]
 #![register_tool(liquid)]
 
-#[liquid::ty("fn(x: {b: usize | b >= 0usize}) -> {r: usize | r == 0usize}")]
+#[liquid::ty("fn(x: {b: i32 | b >= 0i32}) -> {r: i32 | r == 0i32}")]
 fn zero(mut x: i32) -> i32 {
     while x > 0 {
         x -= 1;
