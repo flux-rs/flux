5f7fd5def6ed2f00dd4d2beaa707810b6cd2cab8
Nico Lehmann <nico.lehmannm@gmail.com>
2021-12-09 09:39:29 -0800

    Add missing test files

---
+++ tests/neg/test01.rs
@@ -0,0 +1,16 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[lr::ty(fn<n: int, m: int{m > n}>(bool, i32@n, i32@m) -> i32{v: v > 0})]
+pub fn ref_join(b: bool, n: i32, m: i32) -> i32 {
+    let mut x = n;
+    let mut y = m;
+    let r;
+    if b {
+        r = &mut x;
+    } else {
+        r = &mut y;
+    }
+    *r += 1;
+    *r - n
+}
