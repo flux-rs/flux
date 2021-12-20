fd17fb56751cfc2b0f00815e6c66f5136fb8b6e9
Nico Lehmann <nico.lehmannm@gmail.com>
2021-12-20 09:34:04 -0800

    Vec tests

--- liquid-rust-driver/tests/pos.rs
+++ liquid-rust-driver/tests/pos.rs
@@ -3,4 +3,6 @@ mod common;
 tests! {
     test00: "../tests/pos/test00.rs" => Safe,
     test01: "../tests/pos/test01.rs" => Safe,
+    // test02: "../tests/pos/test02.rs" => Safe,
+    // heapsort: "../tests/pos/heapsort.rs" => Safe,
 }
