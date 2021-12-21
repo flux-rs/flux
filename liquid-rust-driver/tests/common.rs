5e0b6ce1c8bae664c4798bc08463ef6f5911f65d
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-12-21 10:13:21 -0800

    ok, stuff is running

--- liquid-rust-driver/tests/common.rs
+++ liquid-rust-driver/tests/common.rs
@@ -1,3 +1,24 @@
+extern crate compiletest_rs as compiletest;
+
+use std::path::PathBuf;
+
+fn run_mode(mode: &'static str) {
+    let mut config = compiletest::Config::default();
+    config.mode = mode.parse().expect("Invalid mode");
+    config.src_base = PathBuf::from(format!("tests/{}", mode));
+    config.link_deps(); // Populate config.target_rustcflags with dependencies on the path
+    config.clean_rmeta(); // If your tests import the parent crate, this helps with E0464
+
+    println!("biggity boggity {:?}", config.src_base);
+    compiletest::run_tests(&config);
+}
+
+#[test]
+fn compile_test() {
+    run_mode("run-pass");
+    // run_mode("compile-fail");
+}
+
 pub fn test_file(file: &str, expected: liquid_rust_fixpoint::Safeness) -> bool {
     let s0: String = "liquid-rust".to_string();
     let s1: String = "--crate-type=lib".to_string();
