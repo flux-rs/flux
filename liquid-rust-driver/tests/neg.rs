78f8e83d1916b55d437f6a4ef70025c025606414
Nico Lehmann <nico.lehmannm@gmail.com>
2022-01-12 11:35:31 -0800

    Add inc test

--- liquid-rust-driver/tests/neg.rs
+++ liquid-rust-driver/tests/neg.rs
@@ -4,4 +4,5 @@ tests! {
     test00: "../tests/neg/test00.rs" => Unsafe,
     test01: "../tests/neg/test01.rs" => Unsafe,
     test02: "../tests/neg/test02.rs" => Unsafe,
+    test03: "../tests/neg/test03.rs" => Unsafe,
 }
--- liquid-rust-driver/tests/pos.rs
+++ liquid-rust-driver/tests/pos.rs
@@ -4,5 +4,6 @@ tests! {
     test00: "../tests/pos/test00.rs" => Safe,
     test01: "../tests/pos/test01.rs" => Safe,
     test02: "../tests/pos/test02.rs" => Safe,
+    test03: "../tests/pos/test03.rs" => Safe,
     heapsort: "../tests/pos/heapsort.rs" => Safe,
 }
