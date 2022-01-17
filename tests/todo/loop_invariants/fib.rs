aeb47c977f6325bd3131eed316cbeb4c9a61bde1
Adam T. Geller <atgeller@cs.ubc.ca>
2022-01-17 11:27:05 +0100

    Add recursive variant of fib for reference

--- tests/todo/loop_invariants/fib.rs
+++ tests/todo/loop_invariants/fib.rs
@@ -2,14 +2,13 @@
 #![register_tool(lr)]
 
 #[lr::ty(fn(i32{x: 0 < x}) -> i32{x: 0 < x})]
-fn fib(n: i32) -> i32 {
+fn fib_loop(n: i32) -> i32 {
     let mut k = n;
     let mut i = 1;
     let mut j = 1;
     // need i > 0, and therefore j >= 0 as well
     // i > 0 and j >= 0 on entry
     while k > 2 {
-        // loop_inv(a,b);
         // Loop invariant: i > 0 && j >= 0
         let tmp = i + j;
         // tmp > 0
@@ -23,10 +22,22 @@ fn fib(n: i32) -> i32 {
     i
 }
 
-#[lr::ty(fn<n: int{0 < n}, m: int{0 <= m}>(a: i32@{n}, b: i32@{m}; ref<a>, ref<b>) -> i32; a: i32@{n+m}, b: i32@{n})]
-fn loop_inv(a: &mut i32, b: &mut i32) -> i32 {
-    let tmp = *a + *b;
-    *b = *a;
-    *a = tmp;
-    0
+// For reference, below is recursive variant with explicit invariant using liquid-type: i: i32{x: 0 < x}, j: i32{y: 0 <= y}
+#[lr::ty(fn(i32{x: 0 < x}) -> i32{x: 0 < x})]
+fn fib_recursive(n: i32) -> i32 {
+    let mut k = n;
+    fib_recursive_inv(&mut 1, &mut 1, &mut k)
+}
+
+#[lr::ty(fn(i: i32{x: 0 < x}, j: i32{y: 0 <= y}, k: i32; ref<i>, ref<j>, ref<k>) -> i32{ret: 0 < ret}; i: i32{x: 0 < x}, j: i32{y: 0 <= y}, k: i32)]
+fn fib_recursive_inv(i: &mut i32, j: &mut i32, k: &mut i32) -> i32 {
+    if *k > 2 {
+        let tmp = *i + *j;
+        *j = *i;
+        *i = tmp;
+        *k -= 1;
+        fib_recursive_inv(i, j, k)
+    } else {
+        *i
+    }
 }
