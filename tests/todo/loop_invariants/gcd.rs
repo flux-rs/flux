c95c233b8cb0841652a0b7512a3520361bdb95c3
Adam T. Geller <atgeller@cs.ubc.ca>
2022-01-19 11:10:30 +0100

    Fix typo in GCD example

--- tests/todo/loop_invariants/gcd.rs
+++ tests/todo/loop_invariants/gcd.rs
@@ -3,7 +3,7 @@
 
 #[lr::ty(fn(i32{n: 0 < n}, i32{m: 0 < m}) -> i32{p: 0 < p})]
 fn gcd(mut a: i32, mut b: i32) -> i32 {
-    let mut _x = 1;
+    let mut x = 1;
     // For a % b to be safe, need b > 0, also need a > 0 since b is set to a % b in loop
     // On initial entry, a > 0 && b > 0 from the function signature
     while (a % b > 0) {
