ad6c7ff1c276a65a77d29f0ae985017ac9e4bb95
Nico Lehmann <nico.lehmannm@gmail.com>
2022-02-03 17:05:05 -0800

    missing file

---
+++ tests/pos/fib_loop.rs
@@ -0,0 +1,16 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[lr::ty(fn(i32{x: 0 < x}) -> i32{x: 0 < x})]
+pub fn fib_loop(n: i32) -> i32 {
+    let mut k = n;
+    let mut i = 1;
+    let mut j = 1;
+    while k > 2 {
+        let tmp = i + j;
+        j = i;
+        i = tmp;
+        k -= 1;
+    }
+    i
+}
