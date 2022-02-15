4b6d8e63c9dc87031749c9748eb409a3fdcacde3
Nico Lehmann <nico.lehmannm@gmail.com>
2022-02-15 15:49:01 -0800

    Use a priority queue to honor dominance order traversal (#42)

---
+++ liquid-rust-tests/tests/pos/loops/test00.rs
@@ -0,0 +1,18 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[lr::ty(fn() -> bool)]
+pub fn toss() -> bool {
+    true
+}
+
+#[lr::ty(fn(i32{v: v >= 0}) -> i32 @ 0)]
+pub fn test(mut k: i32) -> i32 {
+    while toss() && k < i32::MAX - 1 {
+        k += 1;
+    }
+    while k > 0 {
+        k -= 1;
+    }
+    k
+}
