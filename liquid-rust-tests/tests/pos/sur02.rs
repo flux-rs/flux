bd4b67e1552300b9bc77323743679993859b34bc
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-02-19 06:59:08 -0800

    add ref test

--- liquid-rust-tests/tests/pos/sur02.rs
+++ liquid-rust-tests/tests/pos/sur02.rs
@@ -1,9 +1,9 @@
 #![feature(register_tool)]
 #![register_tool(lr)]
 
-// TODO: this should say `&mut n` but I can't get it to parse :-(
+// TODO: this should say `&mut n` instead of `& n` but I can't get it to parse :-(
 
-#[lr::sig(fn(x: &n@i32) -> i32; x: i32{v: v == n + 1})]
+#[lr::sig(fn(x: & n@i32) -> i32; x: i32{v: v == n + 1})]
 pub fn inc(x: &mut i32) -> i32 {
     *x += 1;
     0
