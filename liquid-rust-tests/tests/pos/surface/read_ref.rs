1a9ea6d415bfe032b7c6c367080cb9be34db579e
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-02-22 16:27:51 -0800

    port all pos test

---
+++ liquid-rust-tests/tests/pos/surface/read_ref.rs
@@ -0,0 +1,15 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[lr::sig(fn(b:bool{b == true}) -> i32{v:v == 0})]
+pub fn assert(_b: bool) -> i32 {
+    0
+}
+
+#[lr::sig(fn() -> i32{v: v ==0})]
+pub fn read_ref() -> i32 {
+    let x = 1;
+    let r = &x;
+    assert(*r == 1);
+    0
+}
