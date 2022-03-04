e970583290fea82febdea5c986495886203ac15a
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-03-03 16:09:10 -0800

    add bcopy, bsearch

---
+++ liquid-rust-tests/tests/neg/surface/bcopy.rs
@@ -0,0 +1,27 @@
+#![allow(unused_attributes)]
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[path = "../../lib/surface/rvec.rs"]
+pub mod rvec;
+use rvec::RVec;
+
+#[lr::sig(fn(src: & n@RVec<i32>, dst: &mut RVec<i32>[n]) -> i32; dst: RVec<i32>[n])]
+fn bcopy_aux(src: &RVec<i32>, dst: &mut RVec<i32>) -> i32 {
+  let mut i = 0;
+  let n = src.len();
+  while i <= n {
+    let r = dst.get_mut(i); //~ ERROR precondition might not hold
+    *r = *src.get(i); //~ ERROR precondition might not hold
+    i += 1;
+  }
+  0
+}
+
+#[lr::sig(fn(src: & n@RVec<i32>) -> RVec<i32>[n])]
+pub fn bcopy(src: &RVec<i32>) -> RVec<i32> {
+  let sz = src.len();
+  let mut dst = RVec::from_elem_n(0, sz);
+  bcopy_aux(src, &mut dst);
+  dst
+}
