878e95f042c6e4ce8a4093bd11bd75c8bae3656a
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-03-09 20:02:00 -0800

    add mut test for T73

---
+++ liquid-rust-tests/tests/pos/surface/T73m.rs
@@ -0,0 +1,12 @@
+#![allow(unused_attributes)]
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[path = "../../lib/rmat.rs"]
+pub mod rmat;
+use rmat::RMat;
+
+#[lr::sig(fn (arr2: &mut RMat<f32>) -> bool)]
+pub fn is_neg(_arr2: &mut RMat<f32>) -> bool {
+  true
+}
