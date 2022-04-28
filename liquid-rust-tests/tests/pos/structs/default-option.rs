8b6b7c2ffef16f1b1aff458047914d0bfda975ac
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-04-28 14:45:12 -0700

    drop sig

--- liquid-rust-tests/tests/pos/structs/default-option.rs
+++ liquid-rust-tests/tests/pos/structs/default-option.rs
@@ -4,12 +4,11 @@
 // We need this right now to not deal with the enum `Some`
 
 #[lr::assume]
-#[lr::sig(fn(T) -> Option<T>)]
 fn some<T>(x: T) -> Option<T> {
     Option::Some(x)
 }
 
-#[lr::sig(fn() -> i32{v:0 <= v})]
+#[lr::sig(fn() -> i32{v:10 <= v})]
 pub fn test2() -> i32 {
     let opt = some(12);
     opt.unwrap()
