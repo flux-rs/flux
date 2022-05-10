295d90fd211d90cafaae0b7f8da7ea4befe33abb
Adam T. Geller <atgeller@cs.ubc.ca>
2022-05-10 16:27:26 +0200

    Parse CFG map from CFG attribute using Rust's builtin MetaItem structure

--- liquid-rust-driver/src/collector.rs
+++ liquid-rust-driver/src/collector.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 
 use itertools::Itertools;
-use liquid_rust_common::iter::IterExt;
+use liquid_rust_common::{config, iter::IterExt};
 use liquid_rust_syntax::{
     parse_assert_behavior, parse_fn_surface_sig, parse_qualifier, parse_refined_by, parse_ty,
     surface, ParseErrorKind, ParseResult,
 };
-use rustc_ast::{tokenstream::TokenStream, AttrItem, AttrKind, Attribute, MacArgs};
+use rustc_ast::{
+    tokenstream::TokenStream, AttrItem, AttrKind, Attribute, MacArgs, MetaItem, MetaItemKind,
+    NestedMetaItem,
+};
 use rustc_errors::ErrorReported;
 use rustc_hash::FxHashMap;
 use rustc_hir::{
@@ -169,6 +172,12 @@ impl<'tcx, 'a> SpecCollector<'tcx, 'a> {
                     self.parse(tokens.clone(), span.entire(), parse_assert_behavior)?;
                 LiquidAttrKind::AssertBehavior(assert_behavior)
             }
+            ("cfg", MacArgs::Delimited(_, _, _)) => {
+                let cfg = LiquidAttrCFG::parse_cfg(attr_item, self)?;
+                println!("{:?}", cfg);
+                //Ok(cfg)
+                todo!();
+            }
             ("refined_by", MacArgs::Delimited(span, _, tokens)) => {
                 let refined_by = self.parse(tokens.clone(), span.entire(), parse_refined_by)?;
                 LiquidAttrKind::RefinedBy(refined_by)
@@ -225,6 +234,59 @@ impl<'tcx, 'a> SpecCollector<'tcx, 'a> {
         self.error_reported = true;
         self.sess.emit_err(err);
         Err(ErrorReported)
+    }
+
+    fn parse_cfg_item(
+        &mut self,
+        item: &NestedMetaItem,
+        outer_span: Span,
+    ) -> Result<config::AssertBehaviorOptions, ErrorReported> {
+        match item {
+            NestedMetaItem::MetaItem(item) => {
+                if item.name_or_empty().to_ident_string() == String::from("assert") {
+                    let value_str = item
+                        .value_str()
+                        .map(|symbol| symbol.to_ident_string())
+                        .unwrap_or(String::from("ERROR!"));
+                    match value_str.as_str() {
+                        "assume" => Ok(config::AssertBehaviorOptions::Assume),
+                        "ignore" => Ok(config::AssertBehaviorOptions::Ignore),
+                        "check" => Ok(config::AssertBehaviorOptions::Check),
+                        _ => {
+                            return self.emit_err(errors::InvalidCFG {
+                                span: item.name_value_literal_span().unwrap_or(outer_span),
+                            })
+                        }
+                    }
+                } else {
+                    return self.emit_err(errors::InvalidCFG { span: outer_span });
+                }
+            }
+            _ => return self.emit_err(errors::InvalidCFG { span: outer_span }),
+        }
+    }
+
+    fn parse_cfg(&mut self, attr_item: &AttrItem) -> Result<LiquidAttrKind, ErrorReported> {
+        let meta_item_kind = attr_item.meta_kind();
+        let behavior = match meta_item_kind {
+            Some(MetaItemKind::List(items)) => {
+                let mut options: Vec<config::AssertBehaviorOptions> = items
+                    .iter()
+                    .map(|item| self.parse_cfg_item(item, attr_item.span()))
+                    .try_collect()?;
+                // TODO: Transition to check for duplicates
+                if options.len() != 1 {
+                    return self.emit_err(errors::InvalidCFG { span: attr_item.span() });
+                }
+                options.pop().unwrap()
+            }
+            _ => return self.emit_err(errors::InvalidCFG { span: attr_item.span() }),
+        };
+
+        Ok(LiquidAttrKind::AssertBehavior(surface::AssertBehavior {
+            option: behavior,
+            span: attr_item.span(),
+        }))
     }
 }
 
@@ -381,6 +443,70 @@ impl LiquidAttrKind {
     }
 }
 
+#[derive(Debug)]
+struct CFGSetting {
+    setting: String,
+    span: Span,
+}
+
+#[derive(Debug)]
+struct LiquidAttrCFG {
+    map: HashMap<String, CFGSetting>,
+}
+
+impl LiquidAttrCFG {
+    // TODO: Ugly that we have to access the collector for error reporting
+    fn parse_cfg(
+        attr_item: &AttrItem,
+        collector: &mut SpecCollector,
+    ) -> Result<Self, ErrorReported> {
+        let mut cfg = Self { map: HashMap::new() };
+        let meta_item_kind = attr_item.meta_kind();
+        match meta_item_kind {
+            Some(MetaItemKind::List(items)) => {
+                for item in items {
+                    cfg.parse_cfg_item(&item, collector)?;
+                }
+            }
+            _ => return collector.emit_err(errors::InvalidCFG { span: attr_item.span() }),
+        };
+
+        //todo!();
+        Ok(cfg)
+    }
+
+    fn parse_cfg_item(
+        &mut self,
+        nested_item: &NestedMetaItem,
+        collector: &mut SpecCollector,
+    ) -> Result<(), ErrorReported> {
+        match nested_item {
+            NestedMetaItem::MetaItem(item) => {
+                let name = item.name_or_empty().to_ident_string();
+                let span = item.name_value_literal_span().unwrap_or(nested_item.span());
+                if !name.is_empty() {
+                    if self.map.get(&name).is_some() {
+                        return collector.emit_err(errors::DuplicatedCFG { name, span });
+                    }
+
+                    // TODO: support types of values other than strings
+                    let value_str = item.value_str().map(|symbol| symbol.to_ident_string());
+
+                    if !value_str.is_none() {
+                        let value = value_str.unwrap();
+                        let setting = CFGSetting { setting: value, span };
+                        self.map.insert(name, setting);
+                        return Ok(());
+                    }
+                }
+                // Would have returned already if ok
+                collector.emit_err(errors::InvalidCFG { span })
+            }
+            _ => collector.emit_err(errors::InvalidCFG { span: nested_item.span() }),
+        }
+    }
+}
+
 mod errors {
     use rustc_macros::SessionDiagnostic;
     use rustc_span::Span;
@@ -398,6 +524,21 @@ mod errors {
     pub struct InvalidAttr {
         #[message = "invalid liquid attribute"]
         pub span: Span,
+    }
+
+    #[derive(SessionDiagnostic)]
+    #[error = "LIQUID"]
+    pub struct InvalidCFG {
+        #[message = "invalid liquid configuration attribute"]
+        pub span: Span,
+    }
+
+    #[derive(SessionDiagnostic)]
+    #[error = "LIQUID"]
+    pub struct DuplicatedCFG {
+        #[message = "duplicated configuration option `{name}`"]
+        pub span: Span,
+        pub name: String,
     }
 
     #[derive(SessionDiagnostic)]
