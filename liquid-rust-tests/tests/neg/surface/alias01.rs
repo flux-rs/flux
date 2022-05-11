e6dbda277e22065246ebec16c7bcc34ca6615621
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-05-11 16:46:51 -0700

    move alias annot to rust-type alias to allow importing across files

--- liquid-rust-tests/tests/neg/surface/alias01.rs
+++ liquid-rust-tests/tests/neg/surface/alias01.rs
@@ -1,10 +1,8 @@
 #![feature(register_tool)]
 #![register_tool(lr)]
-#![feature(custom_inner_attributes)]
 
 #[path = "../../lib/nat.rs"]
 pub mod nat;
-// use nat::{Lb, Nat};
 
 #[lr::sig(fn(x:Nat) -> Nat)]
 pub fn test0(x: i32) -> i32 { //~ ERROR postcondition
