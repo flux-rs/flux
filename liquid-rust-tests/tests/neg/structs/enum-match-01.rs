d3ed4ac59bdaa3049e13194e2781829acc21f744
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-05-20 15:52:29 -0700

    add test with std::Option

---
+++ liquid-rust-tests/tests/neg/structs/enum-match-01.rs
@@ -0,0 +1,14 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[path = "../../lib/nat.rs"]
+pub mod nat;
+
+#[lr::sig(fn (Option<i32>) -> Nat)]
+pub fn test(x: Option<i32>) -> i32 {
+    //~ ERROR postcondition might not hold
+    match x {
+        Some(n) => n,
+        None => 0,
+    }
+}
