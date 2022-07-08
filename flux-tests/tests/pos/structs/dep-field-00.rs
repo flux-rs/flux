3d6270123396e050ea6622687ec216acaf8a03dc
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-07-08 14:28:19 -0700

    Test cases for dependent fields in structs (#113)
    * add dep-struct example
    
    * add dep-struct example

---
+++ flux-tests/tests/pos/structs/dep-field-00.rs
@@ -0,0 +1,15 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::refined_by(a: int)]
+pub struct Range {
+  #[flux::field(i32[@a])]
+  pub x: i32,
+  #[flux::field(i32{v : a < v})]
+  pub y: i32
+}
+
+pub fn test(k: i32) -> Range {
+    Range { x : k, y : k + 1 }
+}
+
