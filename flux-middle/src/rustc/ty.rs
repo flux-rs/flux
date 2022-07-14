6c35681637198b35f5d47f01d68c1a519044e003
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-07-14 15:14:20 -0700

    ugh, some silly error with new route.

--- flux-middle/src/rustc/ty.rs
+++ flux-middle/src/rustc/ty.rs
@@ -151,15 +151,14 @@ impl std::fmt::Debug for Ty {
     }
 }
 
+// TODO(RJ): TRAIT-OLD-STYLE
 /// We want to build a lookup table: `(trait_f, key) -> trait_f_ty_at_key` that maps
 /// (1) `trait_f` the generic method name `DefId` which appears at usage-sites,
 /// (2) `key` the particular type-args at the usage site
 /// to the `trait_f_ty_at_key` which is the specific method instance at the key
 ///
 /// e.g. Given (std::Iterator.next, Rng) -> Rng.next
-/// Table is creating a map
-///
-///     method_def_id, subst -> impl_def_id
+/// Table is creating a map: (method_def_id, subst) -> impl_def_id
 ///
 pub type TraitRefKey = DefId;
 pub type TraitImplMap = FxHashMap<(DefId, TraitRefKey), DefId>;
