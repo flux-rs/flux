ef447b3bb89a4f3a7b616dffc9ba2996b8da485c
Nico Lehmann <nlehmann@fb.com>
2022-07-28 13:52:55 -0700

    Change qualif syntax

--- flux-tests/tests/pos/user_qualifiers/well_formed.rs
+++ flux-tests/tests/pos/user_qualifiers/well_formed.rs
@@ -1,8 +1,8 @@
 #![feature(register_tool)]
 #![register_tool(flux)]
 #![feature(custom_inner_attributes)]
-#![flux::qualifier(MyQ1(x: int, a: int) -> x == a)]
-#![flux::qualifier(MyQ2(x: int) -> x > 1)]
+#![flux::qualifier(MyQ1(x: int, a: int): x == a)]
+#![flux::qualifier(MyQ2(x: int): x > 1)]
 
 #[flux::sig(fn(i32[@n]) -> i32[n])]
 pub fn dummy(x: i32) -> i32 {
