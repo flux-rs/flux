e814e1b36d003a18d74eef51637942fe9b42b417
Nico Lehmann <nico.lehmannm@gmail.com>
2022-08-10 15:17:13 -0700

    Add missing test files

---
+++ flux-tests/tests/neg/structs/fold-on-drop.rs
@@ -0,0 +1,15 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::refined_by(a: int)]
+pub struct S {
+    #[flux::field({i32[@a] : a >= 0})]
+    f1: i32,
+    _f2: Vec<i32>,
+}
+
+#[flux::sig(fn(S) -> ())]
+pub fn test(mut s: S) {
+    // break the invariant
+    s.f1 -= 1;
+} //~ ERROR fold
