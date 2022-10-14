ea3fe2d1373bcd8e9aa6555a1873bacb38339dc8
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-10-14 15:40:05 -0700

    ugh fix tests

--- flux-tests/tests/pos/surface/ex2_min_index_loop.rs
+++ flux-tests/tests/pos/surface/ex2_min_index_loop.rs
@@ -5,21 +5,6 @@
 mod rvec;
 use rvec::RVec;
 
-pub struct Bob { 
-    #[flux::field(RVec<i32>{n:n>0})]
-    elems: RVec<i32>,
-}
-
-#[flux::sig(fn(&RVec<i32>{n:n > 0}) -> i32)]
-pub fn give(vec: &RVec<i32>) -> i32 {
-    vec[0]
-}
-
-#[flux::sig(fn(&Bob) -> i32)]
-pub fn give2(bob: &Bob) -> i32 {
-    bob.elems[0]
-}
-
 #[flux::sig(fn(n: RVec<i32>{n > 0}) -> usize{x : x < n})]
 pub fn min_index(vec: RVec<i32>) -> usize {
     let sz = vec.len();
