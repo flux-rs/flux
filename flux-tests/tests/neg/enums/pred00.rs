aa1fbf2a2d2991979be51fc8e6b9fd3ea1de37c3
Nico Lehmann <nico.lehmannm@gmail.com>
2022-10-18 15:11:55 -0700

    Add more tests for preds

--- flux-tests/tests/neg/enums/pred00.rs
+++ flux-tests/tests/neg/enums/pred00.rs
@@ -1,12 +1,16 @@
 #![feature(register_tool)]
 #![register_tool(flux)]
 
-#[flux::refined_by(is_var: bool, nnf: bool)]
-#[flux::invariant(is_var => nnf)]
+#[flux::refined_by(is_atom: bool, nnf: bool)]
+#[flux::invariant(is_atom => nnf)]
 pub enum Pred {
     #[flux::variant((i32) -> Pred[true, true])]
     Var(i32),
-    #[flux::variant((Box<Pred[@p]>) -> Pred[false, p.is_var])]
+    #[flux::variant(Pred[true, true])]
+    True,
+    #[flux::variant(Pred[true, true])]
+    False,
+    #[flux::variant((Box<Pred[@p]>) -> Pred[false, p.is_atom])]
     Not(Box<Pred>),
     #[flux::variant((Box<Pred[@p1]>, Box<Pred[@p2]>) -> Pred[false, p1.nnf && p2.nnf])]
     And(Box<Pred>, Box<Pred>),
@@ -19,6 +23,8 @@ impl Pred {
     pub fn into_nnf(self) -> Pred {
         match self {
             Pred::Var(x) => Pred::Var(x),
+            Pred::True => Pred::True,
+            Pred::False => Pred::False,
             Pred::And(p1, p2) => Pred::And(Box::new(p1.into_nnf()), Box::new(p2.into_nnf())),
             Pred::Or(p1, p2) => Pred::Or(Box::new(p1.into_nnf()), Box::new(p2.into_nnf())),
             Pred::Not(p) => {
@@ -35,8 +41,48 @@ impl Pred {
                         Pred::And(Box::new(p1), Box::new(p2))
                     }
                     Pred::Var(x) => Pred::Not(Box::new(Pred::Var(x))),
+                    Pred::True => Pred::Not(Box::new(Pred::True)),
+                    Pred::False => Pred::Not(Box::new(Pred::False)),
                 }
             }
         }
     } //~ ERROR postcondition
+
+    #[flux::sig(fn(Pred) -> Pred)]
+    pub fn simplify(self) -> Pred {
+        match self {
+            Pred::Var(x) => Pred::Var(x),
+            Pred::True => Pred::True,
+            Pred::False => Pred::False,
+            Pred::And(p1, p2) => {
+                match (p1.simplify(), p2.simplify()) {
+                    (Pred::False, _) => Pred::False,
+                    (Pred::True, p2) => p2,
+                    (p1, p2) => Pred::And(Box::new(p1), Box::new(p2)),
+                }
+            }
+            Pred::Or(p1, p2) => {
+                match (p1.simplify(), p2.simplify()) {
+                    (Pred::True, _) => Pred::True,
+                    (Pred::False, p2) => p2,
+                    (p1, p2) => Pred::Or(Box::new(p1), Box::new(p2)),
+                }
+            }
+            Pred::Not(p) => {
+                match *p {
+                    Pred::False => Pred::True,
+                    Pred::True => Pred::False,
+                    Pred::Var(x) => Pred::Not(Box::new(Pred::Var(x))),
+                    Pred::And(..) => unreachable(), //~ ERROR precondition
+                    Pred::Or(..) => unreachable(),  //~ ERROR precondition
+                    Pred::Not(..) => unreachable(), //~ ERROR precondition
+                }
+            }
+        }
+    }
+}
+
+#[flux::sig(fn() -> T requires false)]
+fn unreachable<T>() -> T {
+    unreachable!()
 }
--- flux-tests/tests/pos/enums/pred00.rs
+++ flux-tests/tests/pos/enums/pred00.rs
@@ -47,4 +47,42 @@ impl Pred {
             }
         }
     }
+
+    #[flux::sig(fn(Pred{v : v.nnf}) -> Pred{v : v.nnf})]
+    pub fn simplify(self) -> Pred {
+        match self {
+            Pred::Var(x) => Pred::Var(x),
+            Pred::True => Pred::True,
+            Pred::False => Pred::False,
+            Pred::And(p1, p2) => {
+                match (p1.simplify(), p2.simplify()) {
+                    (Pred::False, _) => Pred::False,
+                    (Pred::True, p2) => p2,
+                    (p1, p2) => Pred::And(Box::new(p1), Box::new(p2)),
+                }
+            }
+            Pred::Or(p1, p2) => {
+                match (p1.simplify(), p2.simplify()) {
+                    (Pred::True, _) => Pred::True,
+                    (Pred::False, p2) => p2,
+                    (p1, p2) => Pred::Or(Box::new(p1), Box::new(p2)),
+                }
+            }
+            Pred::Not(p) => {
+                match *p {
+                    Pred::False => Pred::True,
+                    Pred::True => Pred::False,
+                    Pred::Var(x) => Pred::Not(Box::new(Pred::Var(x))),
+                    Pred::And(..) => unreachable(),
+                    Pred::Or(..) => unreachable(),
+                    Pred::Not(..) => unreachable(),
+                }
+            }
+        }
+    }
+}
+
+#[flux::sig(fn() -> T requires false)]
+fn unreachable<T>() -> T {
+    unreachable!()
 }
