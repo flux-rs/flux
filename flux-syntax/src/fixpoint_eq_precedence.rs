617c2616dfe8985e872c52a511d4d60278dd203e
Nico Lehmann <nico.lehmannm@gmail.com>
2022-10-18 08:59:04 -0700

    typos

--- flux-syntax/src/fixpoint_eq_precedence.rs
+++ flux-syntax/src/fixpoint_eq_precedence.rs
@@ -1,8 +1,8 @@
 #![feature(register_tool, box_patterns)]
 #![register_tool(flux)]
 
-// Fixpoint parser has `=` at two different precedence level dependeing on wether is used on booleans
-// or not. This test checks we are emitting the correct constraint in such a case.
+// Fixpoint parser has `=` at two different precedence levels depending on whether is used on
+// booleans or not. This tests we are emitting the correct constraint in such cases.
 #[flux::sig(fn(i32{v : false == false && v == 42}) -> i32[42])]
 pub fn foo(x: i32) -> i32 {
     x
