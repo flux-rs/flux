dcdf12074be1f2d40128d6364fa6380c15f1ccae
Nico Lehmann <nico.lehmannm@gmail.com>
2022-10-21 20:16:34 -0700

    Do not flux assume in test

--- flux-tests/tests/pos/surface/strg_to_mut.rs
+++ flux-tests/tests/pos/surface/strg_to_mut.rs
@@ -8,9 +8,10 @@ struct Struct;
 #[flux::sig(fn(s: &mut Struct{v : v > 0}))]
 fn take_mut(s: &mut Struct) {}
 
-#[flux::assume]
 #[flux::sig(fn(s: &strg Struct[@n]) ensures s: Struct[n + 1])]
-fn take_strg(s: &mut Struct) {}
+fn take_strg(s: &mut Struct) {
+    unimplemented!()
+}
 
 #[flux::sig(fn(s: &strg Struct{v : v >= 0}) ensures s: Struct{v : v > 0})]
 fn test00(s: &mut Struct) {
