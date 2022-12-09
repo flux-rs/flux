19658bd20d9c461972f7ca5d0f1a42e0bc2a7881
Ranjit Jhala <rjhala@eng.ucsd.edu>
2022-12-09 12:05:17 -0800

    allow empty seqences

--- flux-tests/tests/neg/surface/rvec00.rs
+++ flux-tests/tests/neg/surface/rvec00.rs
@@ -5,6 +5,13 @@
 mod rvec;
 use rvec::RVec;
 
+#[flux::sig(fn() -> RVec<i32>[1])]
+pub fn vec_empty() -> RVec<i32> {
+    let mut mv = rvec![];
+    mv.push(1);
+    mv
+}
+
 pub fn vec_push() -> usize {
     let v = rvec![0, 1];
     let r = v[0];
