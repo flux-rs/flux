5ccf67504f606c21ab77c5b91224bcd8ab0dbaf7
Nico Lehmann <nico.lehmannm@gmail.com>
2022-12-09 22:05:06 -0800

    Add rslice example

---
+++ flux-tests/tests/pos/surface/rslice00.rs
@@ -0,0 +1,13 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[path = "../../lib/rvec.rs"]
+mod rvec;
+use rvec::{rslice::RSlice, RVec};
+
+#[flux::sig(fn(&mut RVec<T>[10]))]
+fn test00<T>(vec: &mut RVec<T>) {
+    let mut s = RSlice::from_vec(vec);
+    let s1 = s.subslice(0, 3);
+    let s2 = s.subslice(4, 5);
+}
