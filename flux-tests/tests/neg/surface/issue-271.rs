a6a65cf1a4b5a6e80f529b5d6488ff8b65de331a
Nico Lehmann <nico.lehmannm@gmail.com>
2022-12-27 18:29:45 -0800

    Generate at least one kvar

---
+++ flux-tests/tests/neg/surface/issue-271.rs
@@ -0,0 +1,20 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+pub struct S;
+
+#[flux::sig(fn(x: u32) -> Result<{S: x < 100}, bool>)]
+pub fn test01(x: u32) -> Result<S, bool> {
+    if x > 100 {
+        return Err(false);
+    }
+    Ok(S) //~ ERROR postcondition
+}
+
+#[flux::sig(fn(x: u32) -> Option<{S: x < 100}>)]
+pub fn test02(x: u32) -> Option<S> {
+    if x > 100 {
+        return None;
+    }
+    Some(S) //~ ERROR postcondition
+}
