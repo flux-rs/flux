ac7f123962a94da654f4edf6da1781124598eaaa
Nico Lehmann <nico.lehmannm@gmail.com>
2023-01-02 18:05:03 -0800

    Convert pointers to references when making an array (#290)

---
+++ flux-tests/tests/pos/surface/arr02.rs
@@ -0,0 +1,19 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::sig(fn() -> i32{v: v == 0})]
+fn test00() -> i32 {
+    let v1 = 0;
+    let v2 = 0;
+    let arr = [&v1, &v2];
+    *arr[0]
+}
+
+#[flux::sig(fn() -> i32{v: v >= 0})]
+fn test01() -> i32 {
+    let mut v1 = 0;
+    let mut v2 = 0;
+    let arr = [&mut v1, &mut v2];
+    *arr[1] += 1;
+    v1
+}
--- flux-typeck/src/constraint_gen.rs
+++ flux-typeck/src/constraint_gen.rs
@@ -6,10 +6,14 @@ use flux_middle::{
     rty::{
         evars::{EVarCxId, EVarSol, UnsolvedEvar},
         fold::TypeFoldable,
-        BaseTy, BinOp, Binders, Constraint, Constraints, EVar, EVarGen, Expr, ExprKind, GenericArg,
-        InferMode, Path, PolySig, PolyVariant, RefKind, RefineArg, Sort, Ty, TyKind, VariantRet,
+        BaseTy, BinOp, Binders, Const, Constraint, Constraints, EVar, EVarGen, Expr, ExprKind,
+        GenericArg, InferMode, Path, PolySig, PolyVariant, RefKind, RefineArg, Sort, Ty, TyKind,
+        VariantRet,
     },
-    rustc::mir::{BasicBlock, SourceInfo},
+    rustc::{
+        self,
+        mir::{BasicBlock, SourceInfo},
+    },
 };
 use itertools::{izip, Itertools};
 use rustc_data_structures::fx::FxIndexMap;
@@ -220,6 +224,42 @@ impl<'a, 'tcx> ConstrGen<'a, 'tcx> {
         rcx.replace_evars(&evars_sol);
 
         Ok(variant.ret.replace_evars(&evars_sol))
+    }
+
+    pub fn check_mk_array(
+        &mut self,
+        rcx: &mut RefineCtxt,
+        env: &mut TypeEnv,
+        args: &[Ty],
+        arr_ty: &rustc::ty::Ty,
+        src_info: SourceInfo,
+    ) -> Result<Ty, CheckerError> {
+        let genv = self.genv;
+
+        let mut infcx = self.infcx(rcx);
+
+        let arr_ty =
+            genv.refine_ty(arr_ty, &mut |sorts| infcx.fresh_kvar(sorts, KVarEncoding::Conj));
+
+        let span = src_info.span;
+        for ty in args {
+            // TODO(nilehmann) We should share this logic with `check_fn_call`
+            match (ty.kind(), arr_ty.kind()) {
+                (TyKind::Ptr(RefKind::Mut, path), TyKind::Ref(RefKind::Mut, bound)) => {
+                    infcx.subtyping(rcx, &env.get(path, Some(span)), bound);
+                    env.update(path, bound.clone());
+                    env.block(path);
+                }
+                (TyKind::Ptr(RefKind::Shr, path), TyKind::Ref(RefKind::Shr, bound)) => {
+                    infcx.subtyping(rcx, &env.get(path, Some(span)), bound);
+                    env.block(path);
+                }
+                _ => infcx.subtyping(rcx, ty, &arr_ty),
+            }
+        }
+        rcx.replace_evars(&infcx.solve()?);
+
+        Ok(Ty::array(arr_ty, Const { val: args.len() }))
     }
 
     fn infcx(&mut self, rcx: &RefineCtxt) -> InferCtxt<'_, 'tcx> {
