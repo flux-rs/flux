313ecc2e6a6510923ad17ef541524b73d5ff82df
Nico Lehmann <nico.lehmannm@gmail.com>
2023-01-04 16:28:53 -0800

    Fix bug in alias expansion (#297)

--- flux-desugar/src/desugar.rs
+++ flux-desugar/src/desugar.rs
@@ -508,8 +508,8 @@ impl<'a, 'tcx> ExprCtxt<'a, 'tcx> {
             surface::ExprKind::UnaryOp(op, box e) => {
                 fhir::ExprKind::UnaryOp(desugar_un_op(op), Box::new(self.desugar_expr(e)?))
             }
-            surface::ExprKind::Dot(var, fld) => {
-                if let fhir::ExprKind::Var(name, span, sym) = self.desugar_var(var)?.kind {
+            surface::ExprKind::Dot(box e, fld) => {
+                if let fhir::ExprKind::Var(name, span, sym) = self.desugar_expr(e)?.kind {
                     let var = fhir::Ident { name, source_info: (sym, span) };
                     fhir::ExprKind::Dot(var, fld.name, fld.span)
                 } else {
---
+++ flux-tests/tests/neg/surface/alias03.rs
@@ -0,0 +1,19 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::refined_by(f: int)]
+pub struct S {
+    #[flux::field(i32[@f])]
+    f: i32,
+}
+
+#[flux::alias(type A(n) = i32{v: v < n.f })]
+type _A = i32;
+
+#[flux::sig(fn(s: S, x: A[s]))]
+pub fn test0(s: S, x: i32) {}
+
+pub fn test1() {
+    let s = S { f: 10 };
+    test0(s, 20); //~ ERROR precondition
+}
