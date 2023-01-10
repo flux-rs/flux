21c2ee73f09cf8b8fd7aa40ff67325c347cf4d26
Nico Lehmann <nico.lehmannm@gmail.com>
2023-01-10 13:41:17 -0800

    Support for division in refinements

--- flux-tests/tests/neg/surface/neq.rs
+++
@@ -1,9 +0,0 @@
-#![feature(register_tool)]
-#![register_tool(flux)]
-
-#[flux::sig(fn(x:i32, y: i32{x != y}))]
-pub fn ipa(x: i32, y: i32) {}
-
-pub fn ris() {
-    ipa(0, 0); //~ ERROR precondition
-}
