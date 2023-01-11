5d4b77f12cad4b72198ba99a9fb59fdb6b3952ec
Nico Lehmann <nico.lehmannm@gmail.com>
2023-01-10 18:01:22 -0800

    Add missing test files

---
+++ flux-tests/tests/pos/surface/ref_cell00.rs
@@ -0,0 +1,9 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+use std::cell::RefCell;
+
+#[flux::sig(fn(&RefCell<i32{v: v >= 0}>) -> i32{v: v >= 0})]
+fn test(cell: &RefCell<i32>) -> i32 {
+    *cell.borrow_mut()
+}
