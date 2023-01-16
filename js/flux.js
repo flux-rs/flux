899b507be48c9c72d9b9259318901a60c4c4e425
nilehmann <nico.lehmannm@gmail.com>
2023-01-16 23:39:48 +0000

    Deploying to gh-pages from @ liquid-rust/flux@895419f8270acac10d2595c97deca0504e71cb05 🚀

--- js/flux.js
+++ js/flux.js
@@ -1,5 +1,5 @@
 window.fetch = (originalFetch => {
-    const fluxPlayground = "https://flux.programming.systems";
+    const fluxPlayground = "https://flux.programming.systems/play.rust-lang.org";
     return (url, options) => {
         const redirected = url.replace(/https:\/\/play.rust-lang.org/, fluxPlayground);
         return originalFetch(redirected, options);
