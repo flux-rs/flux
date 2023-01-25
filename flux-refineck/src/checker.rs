2dcfbf3a430f5a4c2d2e8eb97ad3ca51534069f8
Ranjit Jhala <jhala@cs.ucsd.edu>
2023-01-25 06:19:53 -0800

    Update flux-refineck/src/checker.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- flux-refineck/src/checker.rs
+++ flux-refineck/src/checker.rs
@@ -794,7 +794,7 @@ impl<'a, 'tcx, P: Phase> Checker<'a, 'tcx, P> {
                     }
                 }
             }
-            CastKind::FloatToInt | CastKind::IntToFloat | CastKind::Pointer(_) => {
+            CastKind::FloatToInt | CastKind::IntToFloat | CastKind::Pointer(PointerCast::MutToConstPointer) => {
                 self.genv
                     .refine_ty(to, &mut |sorts| Binders::new(Expr::tt(), sorts))
             }
