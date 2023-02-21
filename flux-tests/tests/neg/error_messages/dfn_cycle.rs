f3c9d89dfa40e8d7dafe94b95b1975865f4da247
Nico Lehmann <nico.lehmannm@gmail.com>
2023-02-20 22:33:06 -0800

    Update toolchain

--- flux-tests/tests/neg/error_messages/dfn_cycle.rs
+++ flux-tests/tests/neg/error_messages/dfn_cycle.rs
@@ -2,8 +2,8 @@
 #![register_tool(flux)]
 #![feature(custom_inner_attributes)]
 #![flux::defs {
-    fn even(x: int) -> bool { x == 0 || odd(x-1) }
-    fn odd(x: int) -> bool { x == 1 || even(x-1) } //~ ERROR cycle
+    fn even(x: int) -> bool { x == 0 || odd(x-1) }  //~ ERROR cycle
+    fn odd(x: int) -> bool { x == 1 || even(x-1) }
 }]
 
 #[flux::sig(fn(x:i32) -> i32[x+1])]
