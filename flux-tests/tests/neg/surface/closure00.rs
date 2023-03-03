bf893b2af884126024211dc94fa58d9a043f5a25
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-03-03 13:40:46 -0800

    closure00 tests work

--- flux-tests/tests/neg/surface/closure00.rs
+++ flux-tests/tests/neg/surface/closure00.rs
@@ -5,3 +5,8 @@
 pub fn test0(c: Option<bool>) -> Option<i32> {
     c.map(|b| if b { 1 } else { 2 }) //~ ERROR: postcondition
 }
+
+#[flux::sig(fn(c: Option<bool>) -> Option<i32{v:1 <= v}>)]
+pub fn test1(c: Option<bool>) -> Option<i32> {
+    c.map(|b| if b { 1 } else { 2 })
+}
