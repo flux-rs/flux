d2a73a71307ce45d03e08cf4859fd24ef60a858b
Ranjit Jhala <jhala@cs.ucsd.edu>
2023-03-06 12:18:37 -0800

    Update flux-refineck/src/checker.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- flux-refineck/src/checker.rs
+++ flux-refineck/src/checker.rs
@@ -135,7 +135,7 @@ impl<'a, 'tcx, P> Checker<'a, 'tcx, P> {
 
 /// Inferred TypeEnv (after the INFER phase) for a single function (DefId)
 pub struct InferResult1 {
-    pub inner: FxHashMap<BasicBlock, TypeEnvInfer>,
+    inner: FxHashMap<BasicBlock, TypeEnvInfer>,
 }
 
 /// Inferred TypeEnv (after the INFER phase) for each "top-level" function + invoked closures
