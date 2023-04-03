709635515e2a7eb26f49b3f3ca2633cc36ff465a
Nico Lehmann <nico.lehmannm@gmail.com>
2023-04-03 08:07:27 -0700

    Parse else if expressions

--- flux-tests/tests/pos/surface/extern_spec_internal.rs
+++ flux-tests/tests/pos/surface/extern_spec_internal.rs
@@ -7,7 +7,7 @@ fn from_ref<T>(s: &T) -> &[T] {
     std::slice::from_ref::<T>(s)
 }
 
-#[flux::sig(fn(&i32) -> &[i32][1])]
+#[flux::sig(fn(&i32) -> &[i32]{n: n > 0})]
 pub fn test(x: &i32) -> &[i32] {
     from_ref(x)
 }
