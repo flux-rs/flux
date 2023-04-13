b8f7dd6d3f5f50edfef9b44663fb956d7e16f897
Nico Lehmann <nico.lehmannm@gmail.com>
2023-04-12 21:01:59 -0700

    Add missing test files

---
+++ flux-tests/tests/pos/surface/unop_overflow.rs
@@ -0,0 +1,8 @@
+#![feature(register_tool, custom_inner_attributes)]
+#![register_tool(flux)]
+#![flux::cfg(check_overflow = true)]
+
+#[flux::sig(fn(a: i32{a != i32::MIN}) -> i32[-a])]
+pub fn neg_overflow_i32(a: i32) -> i32 {
+    -a
+}
