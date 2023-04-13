a0ebf48333b258e89d478183bc3622c486dae852
Cole K <cole-k@users.noreply.github.com>
2023-04-13 14:48:16 -0700

    some code clean up

--- flux-attrs/src/lib.rs
+++ flux-attrs/src/lib.rs
@@ -3,8 +3,8 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote_spanned, ToTokens};
 use syn::{
-    parse_quote_spanned, punctuated::Punctuated, spanned::Spanned, Expr, FnArg, GenericArgument,
-    GenericParam, Token,
+    parse_quote, parse_quote_spanned, punctuated::Punctuated, spanned::Spanned, Expr, FnArg,
+    GenericArgument, GenericParam, Token,
 };
 
 pub fn extern_spec(attr: TokenStream, tokens: TokenStream) -> TokenStream {
@@ -23,73 +23,7 @@ fn transform_extern_spec(attr: TokenStream, tokens: TokenStream) -> syn::Result<
             let (sig, attrs) = extract_sig_from_stub(tokens)?;
             create_dummy_fn(mod_path, None, sig, attrs)
         }
-        Ok(syn::Item::Impl(item_impl)) => {
-            let self_ty_span = item_impl.self_ty.as_ref().span();
-            let mod_path_clone = mod_path.clone();
-            // This field corresponds to the Self type. Reading the mod_path
-            // from the extern_spec attr is probably not strictly necessary, but
-            // we do it so our behavior is consistent.
-            let self_ty = item_impl.self_ty.as_ref();
-            let struct_field: syn::FieldsUnnamed = if let Some(mod_path) = mod_path {
-                parse_quote_spanned! { self_ty_span => ( #mod_path :: #self_ty ) }
-            } else {
-                parse_quote_spanned! { self_ty_span => ( #self_ty ) }
-            };
-            // The name of this struct is mangled to ensure that it doesn't
-            // conflict with an actual impl. Same with the name of the type.
-            let dummy_ident = create_dummy_ident(&mut "FluxExternImplStruct".to_string(), item_impl.self_ty.as_ref())?;
-            let mut dummy_self_ty_path = syn::TypePath {
-                qself: None,
-                path: syn::Path {
-                    leading_colon: None,
-                    segments: Punctuated::new(),
-                }
-            };
-            let generics_span = item_impl.generics.span();
-            let generics = item_impl.generics.clone();
-            let generic_arguments: syn::AngleBracketedGenericArguments = parse_quote_spanned! { generics_span => #generics };
-            dummy_self_ty_path.path.segments.push(syn::PathSegment {
-                ident: dummy_ident.clone(),
-                arguments: syn::PathArguments::AngleBracketed(generic_arguments),
-            });
-            let dummy_self_ty = syn::Type::Path(dummy_self_ty_path);
-            let mut dummy_impl = item_impl.clone();
-            let item_impl_span = item_impl.span();
-            let item_struct = syn::ItemStruct {
-                attrs: item_impl.attrs,
-                vis: syn::Visibility::Inherited,
-                struct_token: syn::token::Struct { span: item_impl.impl_token.span },
-                ident: dummy_ident,
-                generics: item_impl.generics.clone(),
-                fields: syn::Fields::Unnamed(struct_field),
-                semi_token: Some(syn::token::Semi { spans: [item_impl_span] }),
-            };
-            dummy_impl.self_ty = Box::new(dummy_self_ty);
-            dummy_impl.items = item_impl.items.into_iter().map(|impl_item| {
-                match impl_item {
-                    // For whatever reason, syn parses function stubs correctly
-                    // by assuming that the semicolon at their end is wrapped in
-                    // braces (functions require a brace-delimited list of Items
-                    // inside of them, the semicolon gets converted to a
-                    // Verbatim).
-                    //
-                    // This does however mean that we will silently discard the
-                    // contents of any "implementation" of an extern function.
-                    syn::ImplItem::Method(impl_item_method) => {
-                        let span = impl_item_method.span();
-                        let dummy_fn_tokens = create_dummy_fn(mod_path_clone.clone(), Some(self_ty.clone()), impl_item_method.sig, impl_item_method.attrs)?;
-                        Ok(parse_quote_spanned! { span => #dummy_fn_tokens })
-                    }
-                    _ => {
-                        Err(syn::Error::new(
-                            impl_item.span(),
-                            format!("Invalid extern_spec: invalid impl item: {:?}\n extern impls may only contain function stubs such as fn len(v: &Vec<T>) -> usize;", impl_item)
-                        ))
-                    }
-                }
-            }).collect::<syn::Result<Vec<syn::ImplItem>>>()?;
-            Ok(TokenStream::from_iter([item_struct.to_token_stream(), dummy_impl.to_token_stream()].into_iter()))
-        }
+        Ok(syn::Item::Impl(item_impl)) => create_dummy_impl(mod_path, item_impl),
         Ok(_) => Err(syn::Error::new(
             span,
             "Invalid extern_spec: the only items which are supported are structs, impls, and function stubs",
@@ -98,6 +32,118 @@ fn transform_extern_spec(attr: TokenStream, tokens: TokenStream) -> syn::Result<
             Err(syn::Error::new(span, "Invalid extern_spec: could not parse associated item"))
         }
     }
+}
+
+/// Creates a dummy impl and supporting struct.
+///
+/// Here's how it works at a high level.
+///
+/// 1. Mangle the Self type's name and create a struct similar to the one
+/// created from create_dummy_struct. We will make an impl on this struct that
+/// mirrors the extern impl.
+/// 2. Create an impl for the struct made in step 1. that follows the structure
+/// of the original impl.
+/// 3. Convert each method inside of the impl into a dummy function using
+/// create_dummy_fn. We pass both the mod_path and the original Self type so
+/// that create_dummy_fn can create the proper function call.
+///
+/// Example:
+///
+/// ```ignore
+/// #[extern_spec]
+/// impl<T> [T] {
+///     #[flux::sig(fn(&[T][@n]) -> usize[n])]
+///     fn len(v: &[T]) -> usize;
+/// }
+///
+/// =>
+///
+/// #[allow(unused, dead_code)]
+/// struct<T> FluxExternImplStructRefSliceT<T>(&[T]);
+/// #[allow(unused, dead_code)]
+/// impl<T> FluxExternImplStructRefSliceT<T> {
+///     #[flux::extern_spec]
+///     #[flux::sig(fn(&[T][@n]) -> usize[n])]
+///     #[allow(unused, dead_code)]
+///     fn len(v: &[T]) -> usize {
+///         <&[T]>::len(v)
+///     }
+/// }
+/// ```
+fn create_dummy_impl(
+    mod_path: Option<syn::Path>,
+    item_impl: syn::ItemImpl,
+) -> syn::Result<TokenStream> {
+    let self_ty_span = item_impl.self_ty.as_ref().span();
+    let mod_path_clone = mod_path.clone();
+    // This field corresponds to the Self type. Reading the mod_path
+    // from the extern_spec attr is probably not strictly necessary, but
+    // we do it so our behavior is consistent.
+    let self_ty = item_impl.self_ty.as_ref();
+    let struct_field: syn::FieldsUnnamed = if let Some(mod_path) = mod_path {
+        parse_quote_spanned! { self_ty_span => ( #mod_path :: #self_ty ) }
+    } else {
+        parse_quote_spanned! { self_ty_span => ( #self_ty ) }
+    };
+    // The name of this struct is mangled to ensure that it doesn't
+    // conflict with an actual impl. Same with the name of the type.
+    let dummy_ident =
+        create_dummy_ident(&mut "FluxExternImplStruct".to_string(), item_impl.self_ty.as_ref())?;
+    let mut dummy_self_ty_path = syn::TypePath {
+        qself: None,
+        path: syn::Path { leading_colon: None, segments: Punctuated::new() },
+    };
+    let generics_span = item_impl.generics.span();
+    let generics = item_impl.generics.clone();
+    let generic_arguments: syn::AngleBracketedGenericArguments =
+        parse_quote_spanned! { generics_span => #generics };
+    dummy_self_ty_path.path.segments.push(syn::PathSegment {
+        ident: dummy_ident.clone(),
+        arguments: syn::PathArguments::AngleBracketed(generic_arguments),
+    });
+    let dummy_self_ty = syn::Type::Path(dummy_self_ty_path);
+    let mut dummy_impl = item_impl.clone();
+    let item_impl_span = item_impl.span();
+    let mut item_struct = syn::ItemStruct {
+        attrs: item_impl.attrs,
+        vis: syn::Visibility::Inherited,
+        struct_token: syn::token::Struct { span: item_impl.impl_token.span },
+        ident: dummy_ident,
+        generics: item_impl.generics.clone(),
+        fields: syn::Fields::Unnamed(struct_field),
+        semi_token: Some(syn::token::Semi { spans: [item_impl_span] }),
+    };
+    // Both the struct and its impl end up unused so we add these attributes.
+    let allow_attr: syn::Attribute = parse_quote!(#[allow(unused, dead_code)]);
+    item_struct.attrs.push(allow_attr.clone());
+    dummy_impl.attrs.push(allow_attr);
+    dummy_impl.self_ty = Box::new(dummy_self_ty);
+    dummy_impl.items = item_impl.items.into_iter().map(|impl_item| {
+        match impl_item {
+            // For whatever reason, syn parses function stubs correctly
+            // by assuming that the semicolon at their end is wrapped in
+            // braces (functions require a brace-delimited list of Items
+            // inside of them, the semicolon gets converted to a
+            // Verbatim).
+            //
+            // This does however mean that we will silently discard the
+            // contents of any "implementation" of an extern function.
+            syn::ImplItem::Method(impl_item_method) => {
+                let span = impl_item_method.span();
+                let dummy_fn_tokens = create_dummy_fn(mod_path_clone.clone(), Some(self_ty.clone()), impl_item_method.sig, impl_item_method.attrs)?;
+                Ok(parse_quote_spanned! { span => #dummy_fn_tokens })
+            }
+            _ => {
+                Err(syn::Error::new(
+                    impl_item.span(),
+                    format!("Invalid extern_spec: invalid impl item: {:?}\n extern impls may only contain function stubs such as fn len(v: &Vec<T>) -> usize;", impl_item)
+                ))
+            }
+        }
+    }).collect::<syn::Result<Vec<syn::ImplItem>>>()?;
+    Ok(TokenStream::from_iter(
+        [item_struct.to_token_stream(), dummy_impl.to_token_stream()].into_iter(),
+    ))
 }
 
 fn create_dummy_ident(dummy_prefix: &mut String, ty: &syn::Type) -> syn::Result<syn::Ident> {
@@ -231,12 +277,11 @@ fn extract_sig_from_stub(
 /// 1. Mangle the name of the function into flux_extern_spec_{fn_ident} so that it doesn't
 ///    conflict with an existing function. TODO: add some random number to ensure it.
 /// 2. Create a new function with the mangled name and same signature.
-/// 3. Add any generic parameters in the extern spec to the generic params of the new function.
-/// 4. Replace its body with a single expression that is a function call.
-/// 5. The function call is of the form mod_path::fn_ident<generic_args>(params)
+/// 3. Replace its body with a single expression that is a function call.
+/// 4. The function call is of the form mod_path::fn_ident<generic_args>(params)
 ///    where the generic args and params have been modified so that they function as
 ///    arguments to the call instead of being a part of the function signature.
-/// 6. Finally, we suppress dead_code/unused warnings and add the flux::extern_spec
+/// 5. Finally, we suppress dead_code/unused warnings and add the flux::extern_spec
 ///    so that flux knows this is the generated dummy function for the external function
 ///    that is being called.
 fn create_dummy_fn(
