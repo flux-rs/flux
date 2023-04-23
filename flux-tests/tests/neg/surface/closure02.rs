01082746d8fc718d937158eb9ff767f78615918f
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-04-23 06:34:38 -0700

    old tests pass

--- flux-tests/tests/neg/surface/closure02.rs
+++ flux-tests/tests/neg/surface/closure02.rs
@@ -9,5 +9,5 @@ pub struct Foo {
 #[flux::sig(fn(c:Option<bool>) -> Option<Foo>)]
 pub fn test1(c: Option<bool>) -> Option<Foo> {
     let x = 6;
-    c.map(|b| if b { Foo { val: x } } else { Foo { val: 20 } })
+    c.map(|b| if b { Foo { val: x } } else { Foo { val: 20 } }) //~ ERROR: precondition might not hold
 }
