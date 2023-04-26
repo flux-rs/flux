5b7181e86b6f16a4563a1877b9ec09536d353586
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-04-26 09:11:40 -0700

    add explicit false

--- flux-tests/tests/neg/surface/scrape00.rs
+++ flux-tests/tests/neg/surface/scrape00.rs
@@ -1,5 +1,7 @@
 #![feature(register_tool)]
 #![register_tool(flux)]
+#![feature(custom_inner_attributes)]
+#![flux::cfg(scrape_quals = false)]
 
 // test that the (fixpoint) `--scrape` mechanism suffices to get
 // the qualifier needed for the loop invariant below.
