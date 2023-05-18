1a92762e806b4b04b885239e237f28092d35d38d
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-05-18 10:07:24 -0700

    add duplicate param test

---
+++ flux-tests/tests/neg/error_messages/localize00.rs
@@ -0,0 +1,16 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::sig(fn(n:i32{0 <= n}) -> i32{v: 10 <= v && v <= 20})]
+pub fn post(n: i32) -> i32 {
+    n + 10
+}
+
+#[flux::sig(fn(i32{v: 10 <= v && v <= 20}))]
+pub fn pre(_n: i32) {}
+
+pub fn test(n: i32) {
+    if n < 15 {
+        pre(n);
+    }
+}
