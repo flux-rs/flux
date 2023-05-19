e7e5335a5ecac654b33ff6782307e126a0a4fbeb
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-05-19 13:07:24 -0700

    add NOTE tests

--- flux-tests/tests/neg/error_messages/localize00.rs
+++ flux-tests/tests/neg/error_messages/localize00.rs
@@ -3,24 +3,33 @@
 
 // ------ Test 1 -------------------------------------------------
 
-#[flux::sig(fn(i32{v: 10 <= v && v <= 20}))]
+#[flux::sig(fn(i32{v: 10 <= v && v <= 20}))] //~ NOTE this is the condition
 pub fn pre(_n: i32) {}
 
 pub fn test(n: i32) {
     if n < 15 {
         pre(n); //~ ERROR precondition
+                //~| NOTE call site
+                //~| NOTE a precondition cannot be proved
     }
 }
+
 // ------ Test 2 -------------------------------------------------
 
-#[flux::sig(fn(n:i32{0 <= n}) -> i32{v: 100 <= v && v <= 200})]
-pub fn post(n: i32) -> i32 {
-    n + 100 //~ ERROR postcondition
-}
-
-#[flux::sig(fn(n:i32, m:i32[n + 10]))]
+#[flux::sig(fn(n:i32, m:i32[n + 10]))] //~ NOTE this is the condition
 pub fn pre2(_n: i32, _m: i32) {}
 
 pub fn test2(n: i32) {
     pre2(n, n + n); //~ ERROR precondition
+                    //~| NOTE call site
+                    //~| NOTE a precondition cannot be proved
+}
+
+// ------ Test 3 -------------------------------------------------
+
+#[flux::sig(fn(n:i32{0 <= n}) -> i32{v: 100 <= v && v <= 200})] //~ NOTE this is the condition
+pub fn post(n: i32) -> i32 {
+    n + 100 //~ ERROR postcondition
+            //~| NOTE return site
+            //~| NOTE a postcondition cannot be proved
 }
