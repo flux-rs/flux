12440f3200665e128055849fecd1381c65915f56
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-07-21 17:20:41 -0700

    next: resolve_impl_projection

---
+++ flux-tests/tests/todo/trait01.rs
@@ -0,0 +1,21 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+// #[flux::sig(fn(bool[true]))]
+// pub fn assert(_b: bool) {}
+
+#[flux::sig(fn(it: I) where I: Iterator<Item = i32{v:10<=v}>)]
+pub fn foo<I>(_it: I)
+where
+    I: Iterator<Item = i32>,
+{
+    // for x in it {
+    //     assert(0 <= x)
+    // }
+}
+
+#[flux::sig(fn(v: Vec<i32{v:7<=v}>))]
+pub fn test(v: Vec<i32>) {
+    let zig = v.into_iter();
+    foo(zig); //~ ERROR: refinement type
+}
