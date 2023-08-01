a81ffcfb554437fb11004a502c274b9b3e0cf76d
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-07-31 17:04:11 -0700

    add test

---
+++ flux-tests/tests/neg/surface/trait03.rs
@@ -0,0 +1,19 @@
+#![feature(register_tool)]
+#![register_tool(flux)]
+#![feature(custom_inner_attributes)]
+#![flux::cfg(scrape_quals = true)] // TODO: needed due to odd cyclic constraint?
+
+use std::vec::IntoIter;
+
+#[flux::sig(fn(bool[true]))]
+pub fn assert(_b: bool) {}
+
+#[flux::sig(fn(it: IntoIter<i32{v: 5 <= v}>))]
+pub fn goo<I>(it: IntoIter<i32>) {
+    for x in it {
+        assert(0 <= x); // OK
+        assert(1 <= x); // OK
+        assert(5 <= x); // OK
+        assert(6 <= x); //~ ERROR refinement type
+    }
+}
