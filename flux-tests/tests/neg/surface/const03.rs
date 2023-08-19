c1d3697f4eb12adcd7e53742fcd2e743a86baf19
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-08-18 21:37:01 -0700

    restore nuked const03.rs test

---
+++ flux-tests/tests/neg/surface/const03.rs
@@ -0,0 +1,18 @@
+// Test definition and checking of const in struct
+
+#![feature(register_tool)]
+#![register_tool(flux)]
+
+#[flux::constant]
+pub const FORTY_TWO: usize = 21 + 21;
+
+#[flux::refined_by(a:int)]
+pub struct Silly {
+    #[flux::field({usize[@a] | a < FORTY_TWO})]
+    fld: usize,
+}
+
+#[flux::sig(fn(Silly) -> usize{v: v > 45})]
+pub fn bk_silly(s: Silly) -> usize {
+    s.fld //~ ERROR refinement type
+}
