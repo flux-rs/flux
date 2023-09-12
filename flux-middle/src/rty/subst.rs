6a9f9f746d553675ebc46d6a5d923cf238645906
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-09-12 10:54:21 -0700

    asd

--- flux-middle/src/rty/subst.rs
+++ flux-middle/src/rty/subst.rs
@@ -330,36 +330,35 @@ impl TypeFolder for GenericsSubstFolder<'_> {
         }
     }
 
-    /*  [NOTE:index-subst]
+    // [NOTE:index-subst]
 
-    Consider
+    // Consider
 
-    ```rust
-    fn choose <T as base>(b: bool, x: T[@n], y: T[@m]) -> T[if b { n } else { m }])
-    ```
+    // ```rust
+    // fn choose <T as base>(b: bool, x: T[@n], y: T[@m]) -> T[if b { n } else { m }])
+    // ```
 
-    and then a client
+    // and then a client
 
-    ```rust
-    pub fn test01() {
-        assert(choose(true, 0, 1) == 0);
-    }
-    ```
+    // ```rust
+    // pub fn test01() {
+    //     assert(choose(true, 0, 1) == 0);
+    // }
+    // ```
 
-    At the callsite `choose(true, 0, 1)` there are *two* substitutions going on
-    in the signature for `choose` i.e. for the `T[@n]` and `T[@m]`
+    // At the callsite `choose(true, 0, 1)` there are *two* substitutions going on
+    // in the signature for `choose` i.e. for the `T[@n]` and `T[@m]`
 
-    1. `T` -> `i32{v: ...}`
-    2. earlybound `n, m` -> fresh-evars.
+    // 1. `T` -> `i32{v: ...}`
+    // 2. earlybound `n, m` -> fresh-evars.
 
-    The trouble is that if you solely rely on the `bty_for_param` code below,
-    then the `n, m` substitutions "get lost" and so those variables are not
-    "solved for" and you get that pesky instantiation error.
+    // The trouble is that if you solely rely on the `bty_for_param` code below,
+    // then the `n, m` substitutions "get lost" and so those variables are not
+    // "solved for" and you get that pesky instantiation error.
 
-    Instead, we _first_ substitute for the indices, and then let `bty_for_param`
-    do its business.
+    // Instead, we _first_ substitute for the indices, and then let `bty_for_param`
+    // do its business.
 
-    */
     fn fold_ty(&mut self, ty: &Ty) -> Ty {
         match ty.kind() {
             TyKind::Param(param_ty) => self.ty_for_param(*param_ty),
