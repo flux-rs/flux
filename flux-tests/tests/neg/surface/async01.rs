58f31fd9a66c0f65fb7802727a429c525c9a0040
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-09-14 13:30:28 -0700

    consolidate tests

--- flux-tests/tests/neg/surface/async01.rs
+++ flux-tests/tests/neg/surface/async01.rs
@@ -4,7 +4,7 @@
 #[flux::sig(fn(bool[true]))]
 pub fn assert(_b: bool) {}
 
-#[flux::sig(async fn (y:i32{v:0<=v}) -> i32{v:v <= 1000 })]
+#[flux::sig(async fn (y:i32{v:0<=v}) -> i32{v:v <= 1000})]
 pub async fn bar(y: i32) -> i32 {
     let z = if y > 10 { 1 } else { 0 };
     assert(z >= 0);
@@ -18,4 +18,18 @@ pub async fn foo(y: i32) -> i32 {
     assert(z >= 0);
     assert(y >= 0);
     z + 999 //~ ERROR refinement type
+}
+
+#[flux::sig(async fn () -> i32{v:v < 1500 })]
+pub async fn jazz() -> i32 {
+    let apple = bar(10).await;
+    let banana = bar(20).await;
+    apple + banana //~ ERROR refinement type
+}
+
+#[flux::sig(async fn () -> i32{v:v <= 2000 })]
+pub async fn pizzazz() -> i32 {
+    let apple = bar(10).await;
+    let banana = bar(20).await;
+    apple + banana
 }
