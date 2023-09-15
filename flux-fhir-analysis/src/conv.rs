70821e5b22804bf9bb965e0c234669aa457ef49b
Ranjit Jhala <jhala@cs.ucsd.edu>
2023-09-14 19:24:51 -0700

    Update flux-fhir-analysis/src/conv.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- flux-fhir-analysis/src/conv.rs
+++ flux-fhir-analysis/src/conv.rs
@@ -834,11 +834,9 @@ impl Env {
     }
 
     fn early_binders(&self) -> List<rty::Expr> {
-        let mut res = vec![];
-        for idx in 0..self.early_bound.len() {
-            res.push(rty::Expr::early_bvar(idx as u32))
-        }
-        List::from_vec(res)
+        (0..self.early_bound.len())
+            .map(|idx| rty::Expr::early_bvar(idx as u32))
+            .collect()
     }
 }
 
