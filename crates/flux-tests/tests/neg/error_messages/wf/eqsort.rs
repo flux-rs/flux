991b678b7f4933f95912666cf1f914a537db1517
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-10-03 15:50:31 -0700

    add eq-sort test

--- crates/flux-tests/tests/neg/error_messages/wf/eqsort.rs
+++ crates/flux-tests/tests/neg/error_messages/wf/eqsort.rs
@@ -1,16 +1,17 @@
-// #![feature(register_tool)]
-// #![register_tool(flux)]
+#![feature(register_tool)]
+#![register_tool(flux)]
 
 #[flux::opaque]
 #[flux::refined_by(p: int -> bool)]
-struct S;
+pub struct S;
 
 #[flux::sig(fn(x: S) -> bool[set_is_in(x.p, set_singleton(x.p))])] //~ ERROR mismatched sorts
-fn foo(x: S) -> bool {
+                                                                   //~^ ERROR mismatched sorts
+pub fn foo(_x: S) -> bool {
     true
 }
 
 #[flux::sig(fn(S[|x| x > 0]) -> bool[true])]
-fn baz(x: S) -> bool {
+pub fn baz(x: S) -> bool {
     foo(x)
 }
