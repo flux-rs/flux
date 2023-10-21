14085775fbf2e8889cabbb1b37f3864a223d4510
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-10-20 19:14:03 -0700

    next: tweak func-def parser

--- crates/flux-tests/tests/lib/rmap.rs
+++ crates/flux-tests/tests/lib/rmap.rs
@@ -1,32 +1,41 @@
 #![allow(dead_code)]
 #![flux::defs {
-    fn map_set(m:Map<int, int>, k: int, v: int) -> Map<int, int> { map_store(m, k, v) }
-    fn map_get(m: Map<int,int>, k:int) -> int { map_select(m, k) }
-    fn map_def(v:int) -> Map<int, int> { map_default(v) }
+    fn map_set<K, V>(m:Map<K, V>, k: K, v: V) -> Map<K, V> { map_store(m, k, v) }
+    fn map_get<K, V>(m: Map<K, V>, k:K) -> V { map_select(m, k) }
+    fn map_def<K, V>(v:V) -> Map<K, V> { map_default(v) }
 }]
+
+use std::hash::Hash;
 
 /// define a type indexed by a map
 #[flux::opaque]
-#[flux::refined_by(map: Map<int, int>)]
-pub struct RMap {
-    inner: std::collections::HashMap<i32, i32>,
+#[flux::refined_by(map: Map<K, V>)]
+pub struct RMap<K, V> {
+    inner: std::collections::HashMap<K, V>,
 }
 
-impl RMap {
+#[flux::generics(<K as base, V as base>)]
+impl<K, V> RMap<K, V> {
     #[flux::trusted]
     pub fn new() -> Self {
         Self { inner: std::collections::HashMap::new() }
     }
 
     #[flux::trusted]
-    #[flux::sig(fn(self: &strg RMap[@m], k: i32, v: i32) ensures self: RMap[map_set(m, k, v)])]
-    pub fn set(&mut self, k: i32, v: i32) {
+    #[flux::sig(fn(self: &strg RMap<K,V>[@m], k: K, v: V) ensures self: RMap<K,V>[map_set(m, k, v)])]
+    pub fn set(&mut self, k: K, v: V)
+    where
+        K: Eq + Hash,
+    {
         self.inner.insert(k, v);
     }
 
     #[flux::trusted]
-    #[flux::sig(fn(&RMap[@m], k: i32) -> Option<i32[map_get(m, k)]>)]
-    pub fn get(&self, k: i32) -> Option<i32> {
-        self.inner.get(&k).copied()
+    #[flux::sig(fn(&RMap<K, V>[@m], k: K) -> Option<&V[map_get(m, k)]>)]
+    pub fn get(&self, k: K) -> Option<&V>
+    where
+        K: Eq + Hash,
+    {
+        self.inner.get(&k)
     }
 }
