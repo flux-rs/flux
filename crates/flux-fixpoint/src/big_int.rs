e719469d19e230186233f7d9563c464b0eec39b3
Nico Lehmann <nico@cubist.dev>
2023-10-31 15:33:09 -0700

    Remove unused methods in fixpoint::Constant + adjust documentation

--- crates/flux-fixpoint/src/big_int.rs
+++ crates/flux-fixpoint/src/big_int.rs
@@ -4,42 +4,36 @@ use rustc_macros::{Decodable, Encodable};
 
 /// A signed integer in the range [-2^128, 2^128], represented by a `u128` and an explicit sign.
 ///
-/// In the logic, we have to work with natural numbers so we could represent them with arbitrary
-/// precision integers. We instead take the simpler approach of using a fixed size representation
-/// that allows us to store any Rust literal, i.e., we can reprent both `i128::MIN` and `u128::MAX`.
-/// Since we don't do much arithmetic, except for some simple constant folding when simplifying
-/// constraints, we choose to deal with overflow in those cases. We may change the representation in
-/// the future if this become a problem.
-#[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, Encodable, Decodable)]
+/// In the logic, we work with natural numbers so we could represent them with arbitrary precision
+/// integers. We instead take the simpler approach of using a fixed size representation that allows
+/// us to store any Rust literal, i.e., we can reprent both `i128::MIN` and `u128::MAX`. Since we
+/// never do arithmetic, this representation doesn't present any problems. We may choose to change
+/// the representation in the future (and use arbitrary precision integers) if this ever become a
+/// problem, e.g., if we want to do (precise) arithmetic during constant folding.
+#[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, Encodable, Decodable, PartialOrd, Ord)]
 pub struct BigInt {
     sign: Sign,
     val: u128,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Encodable, Decodable)]
+/// This are in order so negative is less than non-negative.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Encodable, Decodable, PartialOrd, Ord)]
 enum Sign {
-    NonNegative,
     Negative,
+    NonNegative,
 }
 
 impl BigInt {
     pub const ZERO: BigInt = BigInt { sign: Sign::NonNegative, val: 0 };
     pub const ONE: BigInt = BigInt { sign: Sign::NonNegative, val: 1 };
 
-    pub fn to_int(self) -> Option<i128> {
-        match self.sign {
-            Sign::NonNegative => i128::try_from(self.val).ok(),
-            Sign::Negative => Some(-(i128::try_from(self.val).ok()?)),
-        }
-    }
-
-    /// Given the bit width of a signed integer type, produces the maximum integer for
+    /// Given the bit width of a signed integer type, produces the minimum integer for
     /// that type, i.e., -2^(bit_width - 1).
     pub fn int_min(bit_width: u32) -> BigInt {
         BigInt { sign: Sign::Negative, val: 1u128 << (bit_width - 1) }
     }
 
-    /// Given the bit width of a signed integer type, produces the minimum integer for
+    /// Given the bit width of a signed integer type, produces the maximum integer for
     /// that type, i.e., 2^(bit_width - 1) - 1.
     pub fn int_max(bit_width: u32) -> BigInt {
         (i128::MAX >> (128 - bit_width)).into()
@@ -73,6 +67,7 @@ impl From<i128> for BigInt {
 
 impl From<i32> for BigInt {
     fn from(val: i32) -> Self {
+        // TODO(nilehmann) use Flux to prove this doesn't overflow
         if val < 0 {
             BigInt { sign: Sign::Negative, val: -(val as i64) as u128 }
         } else {
