91cd204b3dcefaeeb09a32e2c04417f9842e19e1
Nico Lehmann <nico.lehmannm@gmail.com>
2023-12-19 10:48:21 -0300

    Normalize signature on function entry (#580)

---
+++ crates/flux-tests/tests/pos/surface/trait05.rs
@@ -0,0 +1,16 @@
+//! Check we normalize function signature on entry
+
+trait MyTrait {
+    type A;
+}
+
+impl MyTrait for i32 {
+    type A = i32;
+}
+
+fn test<T>(x: <T as MyTrait>::A) -> i32
+where
+    T: MyTrait<A = i32>,
+{
+    x + 1
+}
