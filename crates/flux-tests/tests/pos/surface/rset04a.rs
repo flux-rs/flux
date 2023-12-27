867519a6bc3fc7285ce36f82e3e19dee2b27bd2d
Ranjit Jhala <rjhala@ucsd.edu>
2023-12-26 16:13:20 -0800

    restore rset iter test (#581)
    Co-authored-by: Ranjit Jhala <rjhala@eng.ucsd.edu>

---
+++ crates/flux-tests/tests/pos/surface/rset04a.rs
@@ -0,0 +1,19 @@
+#![flux::cfg(scrape_quals = true)]
+
+#[path = "../../lib/rset.rs"]
+mod rset;
+use rset::RSet;
+
+#[flux::sig(fn (bool[true]))]
+fn assert(_b: bool) {}
+
+pub fn test() {
+    let mut s: RSet<i32> = RSet::new();
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
+
+    for v in s.iter() {
+        assert(*v >= 10);
+    }
+}
