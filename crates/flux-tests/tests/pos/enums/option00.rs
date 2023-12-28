e833c4ae958e37db9512246478fe9ec30422fb85
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-12-28 06:59:20 -0800

    move option extern_spec into lib

--- crates/flux-tests/tests/pos/enums/option00.rs
+++ crates/flux-tests/tests/pos/enums/option00.rs
@@ -1,13 +1,5 @@
-use flux_rs::extern_spec;
-
-#[extern_spec]
-#[flux::refined_by(b:bool)]
-enum Option<T> {
-    #[flux::variant(Option<T>[false])]
-    None,
-    #[flux::variant({T} -> Option<T>[true])]
-    Some(T),
-}
+#[path = "../../lib/option.rs"]
+mod option;
 
 #[flux::trusted]
 #[flux::sig(fn(i32{v: false}) -> T)]
