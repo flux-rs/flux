5b50642df3ba2e676a95dd0425c70bddeb3372f8
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-12-29 10:10:40 -0800

    next: CHECK trait method sig at impl

--- crates/flux-tests/tests/pos/surface/refined_fn_in_trait_01.rs
+++ crates/flux-tests/tests/pos/surface/refined_fn_in_trait_01.rs
@@ -16,3 +16,26 @@ pub fn bar1<T: MyTrait>(x: &T) -> T {
 pub fn bar2<T: MyTrait>(x: &T) -> T {
     x.foo2() // UNSOUNDLY VERIFIED: https://github.com/flux-rs/flux/issues/588
 }
+
+// 1. reject impl of `foo1` rejected!
+
+impl MyTrait for i32 {
+    // #[flux::sig(fn<refine p: int -> bool>(&i32{v: p(v)}) -> i32{v: p(v)})]      // WORKS
+    // #[flux::sig(fn<refine p: Self -> bool>(&Self{v: p(v)}) -> Self{v: p(v)})]
+    fn foo1(&self) -> Self {
+        12 // REJECT
+           // *self // OK
+    }
+
+    fn foo2(&self) -> Self {
+        12 // *self
+    }
+}
+
+#[flux::sig(fn(bool[true]))]
+fn assert(_b: bool) {}
+
+pub fn test() {
+    let x = 42;
+    assert(x.foo1() == 42);
+}
