4af07bcb149c6fed002d936a342c632021c0ce5f
Ranjit Jhala <rjhala@eng.ucsd.edu>
2024-01-13 13:04:32 +0530

    NEXT: todo/assoc_pred01.rs

--- crates/flux-tests/tests/todo/assoc_pred01.rs
+++ crates/flux-tests/tests/todo/assoc_pred01.rs
@@ -4,7 +4,7 @@ pub trait MyTrait {
     fn method(&self) -> i32;
 }
 
-#[flux::trusted]
+// #[flux::trusted]
 #[flux::sig(fn<T as base>(&{T[@x] | <T as MyTrait>::f(x)}))] // TODO: check against trait-spec
 pub fn bob<T: MyTrait>(x: &T) {
     x.method();
