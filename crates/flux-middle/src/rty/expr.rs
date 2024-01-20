e60f98239a51a073dfdd3506d4806e55baa00087
Nico Lehmann <nico.lehmannm@gmail.com>
2024-01-19 16:09:44 -0800

    some comments

--- crates/flux-middle/src/rty/expr.rs
+++ crates/flux-middle/src/rty/expr.rs
@@ -78,7 +78,6 @@ pub enum ExprKind {
     Constant(Constant),
     ConstDefId(DefId),
     BinaryOp(BinOp, Expr, Expr),
-    App(Expr, List<Expr>),
     GlobalFunc(Symbol, FuncKind),
     UnaryOp(UnOp, Expr),
     FieldProj(Expr, FieldProj),
@@ -87,25 +86,34 @@ pub enum ExprKind {
     IfThenElse(Expr, Expr, Expr),
     KVar(KVar),
     AliasPred(AliasPred, List<Expr>),
+    /// Function application. The syntax allows arbitrary expressions in function position, but in
+    /// practice we are restricted by what's possible to encode in fixpoint. In a nutshell, we need
+    /// to make sure that expressions that can't be encoded are eliminated before we generate the
+    /// fixpoint constraint. Most notably, lambda abstractions have to be fully applied before
+    /// encoding into fixpoint (except when they appear as an index at the top-level).
+    App(Expr, List<Expr>),
     /// Lambda abstractions. They are purely syntactic and we don't encode them in the logic. As such,
     /// they have some syntactic restrictions that we must carefully maintain:
     ///
     /// 1. They can appear as an index at the top level.
     /// 2. We can only substitute an abstraction for a variable in function position (or as an index).
     ///    More generaly, we need to partially evaluate expressions such that all abstractions in
-    ///    non-index position are eliminated before encoding into fixpoint. Right now, the implementation
-    ///    only evaluates abstractions that are immediately applied to arguments, thus the restriction.
+    ///    non-index position are eliminated before encoding into fixpoint. Right now, the
+    ///    implementation only evaluates abstractions that are immediately applied to arguments,
+    ///    thus the restriction.
     Abs(Binder<Expr>),
     /// A hole is an expression that must be inferred either *semantically* by generating a kvar or
-    /// *syntactically* by generating an evar. Whether a hole can be inferred semantically or syntactically
-    /// depends on the position it appears: only holes appearing in predicate position can be inferred
-    /// with a kvar (provided it satisfy the fixpoint horn constraints) and only holes used as a refinement
-    /// argument or index (a position that fully determines their value) can be inferred with an evar.
+    /// *syntactically* by generating an evar. Whether a hole can be inferred semantically or
+    /// syntactically depends on the position it appears: only holes appearing in predicate position
+    /// can be inferred with a kvar (provided it satisfies the fixpoint horn constraints) and only
+    /// holes used as an index (a position that fully determines their value) can be inferred with
+    /// an evar.
     ///
-    /// Holes are implicitly defined in a scope, i.e., their solution could mention free and bound variables
-    /// in this scope. This must be considered when generating an inference variables for them (either evar or kvar).
-    /// In fact, the main reason we have holes is that we want to decouple the places where we generate them,
-    /// (where we don't want to worry about the scope) and the places where we infer them (where we do need to worry
+    /// Holes are implicitly defined in a scope, i.e., their solution could mention free and bound
+    /// variables in this scope. This must be considered when generating an inference variables for
+    /// them (either evar or kvar). In fact, the main reason we have holes is that we want to
+    /// decouple the places where we generate holes (where we don't want to worry about the scope),
+    /// and the places where we generate inference variable for them (where we do need to worry
     /// about the scope).
     Hole(HoleKind),
 }
