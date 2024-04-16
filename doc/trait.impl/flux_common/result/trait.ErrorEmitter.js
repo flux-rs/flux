a80ee143e0236ff894e08b2faba3e80d6a244f11
nilehmann <nico.lehmannm@gmail.com>
2024-04-16 01:59:31 +0000

    Deploying to gh-pages from @ flux-rs/flux@3c56bf1894e1601559a546cb17b88945110cb9f3 🚀

--- doc/trait.impl/flux_common/result/trait.ErrorEmitter.js
+++ doc/trait.impl/flux_common/result/trait.ErrorEmitter.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"flux_errors":[["impl ErrorEmitter for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"],["impl ErrorEmitter for <a class=\"struct\" href=\"flux_errors/struct.FluxSession.html\" title=\"struct flux_errors::FluxSession\">FluxSession</a>"]]
+"flux_errors":[["impl ErrorEmitter for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"],["impl ErrorEmitter for <a class=\"struct\" href=\"flux_errors/struct.FluxSession.html\" title=\"struct flux_errors::FluxSession\">FluxSession</a>"]],
+"flux_middle":[["impl <a class=\"trait\" href=\"flux_common/result/trait.ErrorEmitter.html\" title=\"trait flux_common::result::ErrorEmitter\">ErrorEmitter</a> for <a class=\"struct\" href=\"flux_middle/global_env/struct.GlobalEnv.html\" title=\"struct flux_middle::global_env::GlobalEnv\">GlobalEnv</a>&lt;'_, '_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
