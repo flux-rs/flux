57de9c98368057dec224c2e4189722a8061c675a
Ranjit Jhala <rjhala@ucsd.edu>
2024-05-22 13:13:44 -0700

    add stuff to generic_const test

--- tests/tests/todo/generic_const.rs
+++ tests/tests/todo/generic_const.rs
@@ -5,3 +5,16 @@ pub fn from_array<const N: usize>(items: [i32; N]) -> Vec<i32> {
 fn test() -> Vec<i32> {
     from_array([1, 2, 3])
 }
+
+
+fn bad<const N:usize>(arr: &[i32; N]) -> i32 {
+  arr[0] //~ ERROR refinement type
+}
+
+fn good<const N:usize>(arr: &[i32; N]) -> i32 {
+    if (N > 0) {
+      arr[0] //~ ERROR refinement type
+    } else {
+        99
+    }
+}
