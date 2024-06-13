09f3c7bc6d20af6727d23bd5960cffa314b7b463
nilehmann <nico.lehmannm@gmail.com>
2024-06-13 04:41:01 +0000

    Deploying to gh-pages from @ flux-rs/flux@da9fb4ceaf121f9fa1574ea03624ccb3d8eba2d3 🚀

--- doc/flux_middle/rty/evars/sidebar-items.js
+++ doc/flux_middle/rty/evars/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["EVarState"],"mod":["pretty"],"static":["NEXT_CTXT_ID"],"struct":["EVar","EVarCxId","EVarGen","EVarSol","EVid","UnsolvedEvar"]};
+window.SIDEBAR_ITEMS = {"enum":["EVarState"],"mod":["pretty"],"static":["NEXT_CTXT_ID"],"struct":["EVar","EVarCtxt","EVarCxId","EVarGen","EVarSol","EVid","UnsolvedEvar"]};
