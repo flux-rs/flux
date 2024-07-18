07ef86af73467c052e029fb0c92bb29d84fa09dc
nilehmann <nico.lehmannm@gmail.com>
2024-07-18 17:53:23 +0000

    Deploying to gh-pages from @ flux-rs/flux@81e4b3f542ce67b5435f1097fdba47baf3c7f910 🚀

--- doc/flux_desugar/desugar/sidebar-items.js
+++ doc/flux_desugar/desugar/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["collect_generics_in_refined_by","desugar_base_sort","desugar_qualifier","desugar_sort","desugar_spec_func"],"struct":["FluxItemCtxt","RustItemCtxt"],"trait":["DesugarCtxt"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"fn":["collect_generics_in_params","desugar_base_sort","desugar_qualifier","desugar_sort","desugar_spec_func"],"struct":["FluxItemCtxt","RustItemCtxt"],"trait":["DesugarCtxt"],"type":["Result"]};
