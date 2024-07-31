11f318c1250d6f9dde96d68787dec2f2fa726ee9
nilehmann <nico.lehmannm@gmail.com>
2024-07-31 23:51:07 +0000

    Deploying to gh-pages from @ flux-rs/flux@2f2aa86e52065876554c7948b03d5229128683c7 🚀

--- doc/trait.impl/ena/unify/trait.UnifyKey.js
+++ doc/trait.impl/ena/unify/trait.UnifyKey.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"flux_middle":[["impl UnifyKey for <a class=\"struct\" href=\"flux_middle/rty/struct.NumVid.html\" title=\"struct flux_middle::rty::NumVid\">NumVid</a>"],["impl UnifyKey for <a class=\"struct\" href=\"flux_middle/rty/struct.SortVid.html\" title=\"struct flux_middle::rty::SortVid\">SortVid</a>"]]
+"flux_middle":[["impl UnifyKey for <a class=\"struct\" href=\"flux_middle/rty/struct.BvSizeVid.html\" title=\"struct flux_middle::rty::BvSizeVid\">BvSizeVid</a>"],["impl UnifyKey for <a class=\"struct\" href=\"flux_middle/rty/struct.NumVid.html\" title=\"struct flux_middle::rty::NumVid\">NumVid</a>"],["impl UnifyKey for <a class=\"struct\" href=\"flux_middle/rty/struct.SortVid.html\" title=\"struct flux_middle::rty::SortVid\">SortVid</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
