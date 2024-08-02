b708e3a9d052a99ca86e9602a6503534127d328c
nilehmann <nico.lehmannm@gmail.com>
2024-08-02 22:03:51 +0000

    Deploying to gh-pages from @ flux-rs/flux@ecc24f69c65dc9fa18c929243cdc1fab751d42c5 🚀

--- doc/flux_middle/const_eval/sidebar-items.js
+++ doc/flux_middle/const_eval/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["scalar_int_to_constant","scalar_int_to_rty_constant","scalar_int_to_rty_constant2","scalar_to_bits","scalar_to_int","scalar_to_uint","size_of_int_ty","size_of_uint_ty"]};
+window.SIDEBAR_ITEMS = {"fn":["scalar_int_to_mir_constant","scalar_int_to_rty_constant","scalar_int_to_rty_constant2","scalar_to_bits","scalar_to_int","scalar_to_uint","size_of_int_ty","size_of_uint_ty"]};
