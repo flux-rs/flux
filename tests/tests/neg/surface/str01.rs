57da5c4f48560e2da79e1f640878046435cd8441
Ranjit Jhala <rjhala@ucsd.edu>
2024-08-07 16:39:33 -0700

    oops

--- tests/tests/neg/surface/str01.rs
+++ tests/tests/neg/surface/str01.rs
@@ -1,7 +1,15 @@
 // TODO(nilehmann) think how to refine string slices and
+
+#[flux::trusted]
+#[flux::sig(fn(&str[@s]) -> usize[str_len(s)])]
+fn str_len(s: &str) -> usize {
+    s.len()
+}
+
 // move this test to pos
 #[flux::sig(fn() -> usize[3])]
 pub fn str01() -> usize {
     let x = "str";
-    x.len() //~ ERROR refinement type
+    // x.len() //~ ERROR refinement type
+    str_len(x)
 }
--- tests/tests/neg/surface/str02.rs
+++ tests/tests/neg/surface/str02.rs
@@ -6,17 +6,10 @@ pub fn test_cat() {
     require_cat("dog"); //~ ERROR refinement type
 }
 
-// #[flux::sig(fn (&usize[10]))]
-// fn require_ten(_z: &usize) {}
+#[flux::sig(fn (&str[@a], &{str[@b] | a == b}))]
+fn require_eq(_x: &str, _y: &str) {}
 
-// pub fn test_ten() {
-//     require_ten(&11); //~ ERROR refinement type
-// }
-
-// #[flux::sig(fn (&str[@a], &{str[@b] | a == b}))]
-// fn require_eq(_x: &str, _y: &str) {}
-
-// pub fn test_eq() {
-//     require_eq("a", "a");
-//     require_eq("a", "b"); //~ ERROR refinement type
-// }
+pub fn test_eq() {
+    require_eq("a", "a");
+    require_eq("a", "b"); //~ ERROR refinement type
+}
