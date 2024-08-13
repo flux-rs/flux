ec9cee913071463136292653d47fe37ab224a225
Nico Lehmann <nico.lehmannm@gmail.com>
2024-08-13 11:05:07 -0700

    Comment

--- crates/flux-middle/src/queries.rs
+++ crates/flux-middle/src/queries.rs
@@ -55,18 +55,34 @@ pub type QueryResult<T = ()> = Result<T, QueryErr>;
 ///
 /// Both [`QueryErr`] and [`QueryErrAt`] implement [`Diagnostic`]. The implementation for [`QueryErr`]
 /// reports the error at the definition site, while the implementation for [`QueryErrAt`] reports it at
-/// the (attached) use-site span. This allows to play a bit lose because we can emit an error without
+/// the (attached) use-site span. This allows us to play a bit lose because we can emit an error without
 /// attatching a span, but this means we may forget to attach spans at some places. We should consider
 /// not implementing [`Diagnostic`] for [`QueryErr`] such that we always make the distinction between
 /// use-site and def-site explicit, e.g., we could have methods `QueryErr::at_use_site` and
 /// `QueryErr::at_def_site` returning types with different implementations of [`Diagnostic`].
 #[derive(Debug, Clone, Encodable, Decodable)]
 pub enum QueryErr {
-    Unsupported { def_id: DefId, err: UnsupportedErr },
-    Ignored { def_id: DefId },
-    InvalidGenericArg { def_id: DefId },
-    InvalidAssocReft { impl_id: DefId, name: Symbol },
-    Bug { location: String, note: String },
+    Unsupported {
+        def_id: DefId,
+        err: UnsupportedErr,
+    },
+    Ignored {
+        def_id: DefId,
+    },
+    InvalidGenericArg {
+        def_id: DefId,
+    },
+    InvalidAssocReft {
+        impl_id: DefId,
+        name: Symbol,
+    },
+    /// Used to report bugs, typically this means executing an arm in a match we though it was unreachable.
+    /// Use this instead of panicking if it is easy to return a [`QueryErr`]. Use [`QueryErr::bug`] to
+    /// construct this variant.
+    Bug {
+        location: String,
+        note: String,
+    },
     Emitted(ErrorGuaranteed),
 }
 
