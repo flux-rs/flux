d56e3162b3a999bd09a32e3f6b7d5257588c6042
ranjitjhala <jhala@cs.ucsd.edu>
2024-08-17 21:28:28 +0000

    Deploying to gh-pages from @ flux-rs/flux@348cf31f7e369b192630b8be3320a30e0e2275f4 🚀

--- doc/trait.impl/flux_middle/rty/fold/trait.FallibleTypeFolder.js
+++ doc/trait.impl/flux_middle/rty/fold/trait.FallibleTypeFolder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "flux_fhir_analysis":[["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.FallibleTypeFolder.html\" title=\"trait flux_middle::rty::fold::FallibleTypeFolder\">FallibleTypeFolder</a> for <a class=\"struct\" href=\"flux_fhir_analysis/wf/sortck/struct.FullResolver.html\" title=\"struct flux_fhir_analysis::wf::sortck::FullResolver\">FullResolver</a>&lt;'_, '_, '_&gt;"]],
 "flux_middle":[],
-"flux_refineck":[["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.FallibleTypeFolder.html\" title=\"trait flux_middle::rty::fold::FallibleTypeFolder\">FallibleTypeFolder</a> for <a class=\"struct\" href=\"flux_refineck/type_env/place_ty/struct.Unfolder.html\" title=\"struct flux_refineck::type_env::place_ty::Unfolder\">Unfolder</a>&lt;'_, '_, '_, '_&gt;"]]
+"flux_refineck":[["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.FallibleTypeFolder.html\" title=\"trait flux_middle::rty::fold::FallibleTypeFolder\">FallibleTypeFolder</a> for <a class=\"struct\" href=\"flux_refineck/type_env/place_ty/struct.Unfolder.html\" title=\"struct flux_refineck::type_env::place_ty::Unfolder\">Unfolder</a>&lt;'_, '_, '_, '_, '_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
