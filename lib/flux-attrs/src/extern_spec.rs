cffddadac2c1333b7432aef50462b80c3af3310e
Nico Lehmann <nico.lehmannm@gmail.com>
2024-08-30 11:58:16 -0700

    some cleanup

--- lib/flux-attrs/src/extern_spec.rs
+++ lib/flux-attrs/src/extern_spec.rs
@@ -21,16 +21,226 @@ pub(crate) fn transform_extern_spec(
 ) -> syn::Result<TokenStream> {
     let mod_path: Option<syn::Path> =
         if !attr.is_empty() { Some(syn::parse2(attr)?) } else { None };
+    let mod_path = mod_path.as_ref();
     match syn::parse2::<ExternItem>(tokens)? {
-        ExternItem::Struct(item_struct) => extern_struct_spec(mod_path, item_struct),
-        ExternItem::Enum(item_enum) => extern_enum_spec(mod_path, item_enum),
-        ExternItem::Trait(item_trait) => extern_trait_spec(mod_path, item_trait),
-        ExternItem::Fn(mut extern_fn) => {
-            extern_fn.prepare(&mod_path, None, None, true);
-            Ok(extern_fn.into_token_stream())
-        }
-        ExternItem::Impl(extern_item_impl) => extern_impl_spec(mod_path, extern_item_impl),
+        ExternItem::Struct(item_struct) => extern_struct_to_tokens(mod_path, item_struct),
+        ExternItem::Enum(item_enum) => extern_enum_to_tokens(mod_path, item_enum),
+        ExternItem::Trait(item_trait) => extern_trait_to_tokens(mod_path, item_trait),
+        ExternItem::Fn(extern_fn) => extern_fn_to_tokens(mod_path, extern_fn),
+        ExternItem::Impl(extern_item_impl) => extern_impl_to_tokens(mod_path, extern_item_impl),
     }
+}
+
+fn extern_fn_to_tokens(
+    mod_path: Option<&syn::Path>,
+    mut extern_fn: ExternFn,
+) -> syn::Result<TokenStream> {
+    extern_fn.prepare(mod_path, None, None, true);
+    Ok(quote! {
+        const _: () = {
+            #extern_fn
+        };
+    })
+}
+
+/// Create a dummy enum with the same variants and an extra `Fake` variant that contains the original type
+/// Also see the note in [lower_adt_def]
+/// Example:
+///
+/// ```ignore
+/// #[extern_spec]
+/// enum Option<T> {
+///     #[flux::variant(Option<T>[false])]
+///     None,
+///     #[flux::variant({T} -> Option<T>[true])]
+///     Some(T),
+/// }
+///
+/// =>
+///
+/// #[flux::extern_spec]
+/// #[allow(unused, dead_code)]
+/// #[flux::refined_by(b:bool)]
+/// pub enum __FluxExternEnumOption<T> {
+///     #[flux::variant(Option<T>[false])]
+///     None,
+///     #[flux::variant({T} -> Option<T>[true])]
+///     Some(T),
+///     // this fellow is here just so we can get a hold of the original `Option` ...
+///     FluxExternEnumFake(Option<T>),
+/// }
+/// ```
+fn extern_enum_to_tokens(
+    mod_path: Option<&syn::Path>,
+    mut item_enum: syn::ItemEnum,
+) -> syn::Result<TokenStream> {
+    let span = item_enum.span();
+    let ident = item_enum.ident;
+
+    item_enum.ident = format_ident!("__FluxExternEnum{}", ident);
+
+    flux_tool_attrs(&mut item_enum.attrs);
+    for variant in &mut item_enum.variants {
+        flux_tool_attrs(&mut variant.attrs);
+    }
+
+    let dummy_variant_name = format_ident!("__FluxExternVariant");
+    let args = generic_params_to_args(&item_enum.generics.params);
+    let dummy_variant: syn::Variant = if let Some(mod_path) = mod_path {
+        parse_quote_spanned! {span =>
+            #dummy_variant_name ( #mod_path :: #ident < #args > )
+        }
+    } else {
+        parse_quote_spanned! {span =>
+            #dummy_variant_name ( #ident < #args >)
+        }
+    };
+    item_enum.variants.push(dummy_variant);
+
+    Ok(quote_spanned! {span =>
+        const _: () = {
+            #[allow(unused, dead_code)]
+            #[flux_tool::extern_spec]
+            #item_enum
+        };
+    })
+}
+
+/// Create a dummy struct with a single unnamed field that is the external struct.
+///
+/// Example:
+///
+/// ```ignore
+/// #[extern_spec(std::vec)]
+/// #[flux::refined_by(n: int)]
+/// struct Vec<T>;
+///
+/// =>
+///
+/// #[flux::extern_spec]
+/// #[allow(unused, dead_code)]
+/// #[flux::refined_by(n: int)]
+/// struct FluxExternStructVec<T>(std::vec::Vec<T>);
+/// ```
+fn extern_struct_to_tokens(
+    mod_path: Option<&syn::Path>,
+    item_struct: syn::ItemStruct,
+) -> syn::Result<TokenStream> {
+    let item_struct_span = item_struct.span();
+    let fields_span = item_struct.fields.span();
+    let syn::Fields::Unit = item_struct.fields else {
+        return Err(syn::Error::new(
+            fields_span,
+            "invalid extern spec: extern specs on structs cannot have fields, i.e. they must look like struct Vec<T>;",
+        ));
+    };
+
+    let struct_ident = item_struct.ident;
+    let dummy_ident = format_ident!("__FluxExternStruct{}", struct_ident);
+    let mut attrs = item_struct.attrs;
+    flux_tool_attrs(&mut attrs);
+    let generics = item_struct.generics;
+    let args = generic_params_to_args(&generics.params);
+    let field = if let Some(mod_path) = mod_path {
+        quote!(#mod_path :: #struct_ident < #args >)
+    } else {
+        quote!(#struct_ident < #args >)
+    };
+
+    Ok(quote_spanned! {item_struct_span =>
+        const _: () = {
+            #[flux_tool::extern_spec]
+            #[allow(unused, dead_code)]
+            #(#attrs)*
+            struct #dummy_ident #generics (#field);
+        };
+    })
+}
+
+/// Create a dummy trait with a single super-trait that is the external trait
+///
+/// Example:
+///
+/// ```ignore
+/// #[extern_spec(std::vec)]
+/// #[flux::generics(Self as base)]
+/// #[flux::assoc(fn f(self: Self) -> bool)]
+/// trait MyTrait {}
+///
+/// =>
+///
+/// #[flux::extern_spec]
+/// #[allow(unused, dead_code)]
+/// #[flux::generics(Self as base)]
+/// #[flux::assoc(fn f(self: Self) -> bool)]
+/// trait __FluxExternTraitMyTrait: MyTrait {}
+/// ```
+fn extern_trait_to_tokens(
+    mod_path: Option<&syn::Path>,
+    item_trait: syn::ItemTrait,
+) -> syn::Result<TokenStream> {
+    let item_trait_span = item_trait.span();
+    if !item_trait.supertraits.is_empty() {
+        return Err(syn::Error::new(
+            item_trait.supertraits.span(),
+            "invalid extern spec: extern specs on traits cannot have supertraits",
+        ));
+    }
+    if !item_trait.items.is_empty() {
+        return Err(syn::Error::new(
+            item_trait_span,
+            "invalid extern spec: extern specs on traits cannot have items",
+        ));
+    }
+
+    let trait_ident = item_trait.ident;
+    let mut attrs = item_trait.attrs;
+    flux_tool_attrs(&mut attrs);
+
+    let generics = item_trait.generics;
+    let args = generic_params_to_args(&generics.params);
+
+    let dummy_ident = format_ident!("__FluxExternTrait{}", trait_ident);
+
+    let super_trait = if let Some(mod_path) = mod_path {
+        quote_spanned! {item_trait_span =>
+            #mod_path :: #trait_ident < #args >
+        }
+    } else {
+        quote_spanned! {item_trait_span =>
+            #trait_ident < # args >
+        }
+    };
+    Ok(quote_spanned! {item_trait_span =>
+        const _: () = {
+            #[flux_tool::extern_spec]
+            #[allow(unused, dead_code)]
+            #(#attrs)*
+            trait #dummy_ident #generics: #super_trait {}
+        };
+    })
+}
+
+fn extern_impl_to_tokens(
+    mod_path: Option<&syn::Path>,
+    mut extern_item_impl: ExternItemImpl,
+) -> syn::Result<TokenStream> {
+    extern_item_impl.prepare(mod_path);
+
+    let dummy_impl_struct = &extern_item_impl.dummy_ident;
+    let generics = &extern_item_impl.generics;
+    let fields = generic_params_to_fields(&extern_item_impl.generics.params);
+
+    Ok(quote! {
+        #[allow(unused, dead_code, unused_variables)]
+        const _: () = {
+            #[flux_tool::ignore]
+            struct #dummy_impl_struct #generics ( #fields );
+
+            #[flux_tool::extern_spec]
+            #extern_item_impl
+        };
+    })
 }
 
 enum ExternItem {
@@ -55,7 +265,6 @@ struct ExternItemImpl {
     self_ty: Box<Type>,
     brace_token: Brace,
     items: Vec<ExternFn>,
-    mod_path: Option<syn::Path>,
     dummy_ident: syn::Ident,
 }
 
@@ -72,14 +281,12 @@ impl ExternItem {
 }
 
 impl ExternItemImpl {
-    fn prepare(&mut self, mod_path: Option<syn::Path>) -> syn::Result<()> {
-        self.mod_path = mod_path;
+    fn prepare(&mut self, mod_path: Option<&syn::Path>) {
         let trait_ = self.trait_.as_ref().map(|(_, path, _)| path);
 
         for item in &mut self.items {
-            item.prepare(&self.mod_path, Some(&self.self_ty), trait_, false);
+            item.prepare(mod_path, Some(&self.self_ty), trait_, false);
         }
-        Ok(())
     }
 }
 
@@ -117,7 +324,7 @@ impl ToTokens for ExternItemImpl {
 impl ExternFn {
     fn prepare(
         &mut self,
-        mod_path: &Option<syn::Path>,
+        mod_path: Option<&syn::Path>,
         self_ty: Option<&syn::Type>,
         trait_: Option<&syn::Path>,
         mangle: bool,
@@ -153,7 +360,7 @@ impl ExternFn {
 
     fn fill_body(
         &mut self,
-        mod_path: &Option<syn::Path>,
+        mod_path: Option<&syn::Path>,
         self_ty: Option<&syn::Type>,
         trait_: Option<&syn::Path>,
     ) {
@@ -181,7 +388,7 @@ impl ExternFn {
 impl ToTokens for ExternFn {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         debug_assert!(self.block.is_some());
-        quote!(#[flux::extern_spec]).to_tokens(tokens);
+        tokens.extend(quote!(#[flux_tool::extern_spec]));
         tokens.append_all(&self.attrs);
         self.sig.to_tokens(tokens);
         self.block.to_tokens(tokens);
@@ -276,7 +483,6 @@ impl Parse for ExternItemImpl {
             self_ty: Box::new(self_ty),
             brace_token,
             items,
-            mod_path: None,
             dummy_ident,
         })
     }
@@ -328,206 +534,6 @@ fn create_dummy_ident_from_path(dummy_prefix: &str, path: &syn::Path) -> syn::Re
     } else {
         Err(syn::Error::new(path.span(), format!("invalid extern_spec: empty Path {:?}", path)))
     }
-}
-
-/// Create a dummy enum with the same variants and an extra `Fake` variant that contains the original type
-/// Also see the note in [lower_adt_def]
-/// Example:
-///
-/// ```ignore
-/// #[extern_spec]
-/// enum Option<T> {
-///     #[flux::variant(Option<T>[false])]
-///     None,
-///     #[flux::variant({T} -> Option<T>[true])]
-///     Some(T),
-/// }
-///
-/// =>
-///
-/// #[flux::extern_spec]
-/// #[allow(unused, dead_code)]
-/// #[flux::refined_by(b:bool)]
-/// pub enum __FluxExternEnumOption<T> {
-///     #[flux::variant(Option<T>[false])]
-///     None,
-///     #[flux::variant({T} -> Option<T>[true])]
-///     Some(T),
-///     // this fellow is here just so we can get a hold of the original `Option` ...
-///     FluxExternEnumFake(Option<T>),
-/// }
-/// ```
-fn extern_enum_spec(
-    mod_path: Option<syn::Path>,
-    mut item_enum: syn::ItemEnum,
-) -> syn::Result<TokenStream> {
-    let span = item_enum.span();
-    let ident = item_enum.ident;
-
-    item_enum.ident = format_ident!("__FluxExternEnum{}", ident);
-
-    flux_tool_attrs(&mut item_enum.attrs);
-    for variant in &mut item_enum.variants {
-        flux_tool_attrs(&mut variant.attrs);
-    }
-
-    let dummy_variant_name = format_ident!("__FluxExternVariant");
-    let args = generic_params_to_args(&item_enum.generics.params);
-    let dummy_variant: syn::Variant = if let Some(mod_path) = mod_path {
-        parse_quote_spanned! {span =>
-            #dummy_variant_name ( #mod_path :: #ident < #args > )
-        }
-    } else {
-        parse_quote_spanned! {span =>
-            #dummy_variant_name ( #ident < #args >)
-        }
-    };
-    item_enum.variants.push(dummy_variant);
-
-    Ok(quote_spanned! {span =>
-        const _: () = {
-            #[allow(unused, dead_code)]
-            #[flux_tool::extern_spec]
-            #item_enum
-        };
-    })
-}
-
-/// Create a dummy struct with a single unnamed field that is the external struct.
-///
-/// Example:
-///
-/// ```ignore
-/// #[extern_spec(std::vec)]
-/// #[flux::refined_by(n: int)]
-/// struct Vec<T>;
-///
-/// =>
-///
-/// #[flux::extern_spec]
-/// #[allow(unused, dead_code)]
-/// #[flux::refined_by(n: int)]
-/// struct FluxExternStructVec<T>(std::vec::Vec<T>);
-/// ```
-fn extern_struct_spec(
-    mod_path: Option<syn::Path>,
-    item_struct: syn::ItemStruct,
-) -> syn::Result<TokenStream> {
-    let item_struct_span = item_struct.span();
-    let fields_span = item_struct.fields.span();
-    let syn::Fields::Unit = item_struct.fields else {
-        return Err(syn::Error::new(
-            fields_span,
-            "invalid extern spec: extern specs on structs cannot have fields, i.e. they must look like struct Vec<T>;",
-        ));
-    };
-
-    let struct_ident = item_struct.ident;
-    let dummy_ident = format_ident!("__FluxExternStruct{}", struct_ident);
-    let mut attrs = item_struct.attrs;
-    flux_tool_attrs(&mut attrs);
-    let generics = item_struct.generics;
-    let args = generic_params_to_args(&generics.params);
-    let field = if let Some(mod_path) = mod_path {
-        quote!(#mod_path :: #struct_ident < #args >)
-    } else {
-        quote!(#struct_ident < #args >)
-    };
-
-    Ok(quote_spanned! {item_struct_span =>
-        const _: () = {
-            #[flux_tool::extern_spec]
-            #[allow(unused, dead_code)]
-            #(#attrs)*
-            struct #dummy_ident #generics (#field);
-        };
-    })
-}
-
-/// Create a dummy trait with a single super-trait that is the external trait
-///
-/// Example:
-///
-/// ```ignore
-/// #[extern_spec(std::vec)]
-/// #[flux::generics(Self as base)]
-/// #[flux::assoc(fn f(self: Self) -> bool)]
-/// trait MyTrait {}
-///
-/// =>
-///
-/// #[flux::extern_spec]
-/// #[allow(unused, dead_code)]
-/// #[flux::generics(Self as base)]
-/// #[flux::assoc(fn f(self: Self) -> bool)]
-/// trait __FluxExternTraitMyTrait: MyTrait {}
-/// ```
-fn extern_trait_spec(
-    mod_path: Option<syn::Path>,
-    item_trait: syn::ItemTrait,
-) -> syn::Result<TokenStream> {
-    let item_trait_span = item_trait.span();
-    if !item_trait.supertraits.is_empty() {
-        return Err(syn::Error::new(
-            item_trait.supertraits.span(),
-            "invalid extern spec: extern specs on traits cannot have supertraits",
-        ));
-    }
-    if !item_trait.items.is_empty() {
-        return Err(syn::Error::new(
-            item_trait_span,
-            "invalid extern spec: extern specs on traits cannot have items",
-        ));
-    }
-
-    let trait_ident = item_trait.ident;
-    let mut attrs = item_trait.attrs;
-    flux_tool_attrs(&mut attrs);
-
-    let generics = item_trait.generics;
-    let args = generic_params_to_args(&generics.params);
-
-    let dummy_ident = format_ident!("__FluxExternTrait{}", trait_ident);
-
-    let super_trait = if let Some(mod_path) = mod_path {
-        quote_spanned! {item_trait_span =>
-            #mod_path :: #trait_ident < #args >
-        }
-    } else {
-        quote_spanned! {item_trait_span =>
-            #trait_ident < # args >
-        }
-    };
-    Ok(quote_spanned! {item_trait_span =>
-        const _: () = {
-            #[flux_tool::extern_spec]
-            #[allow(unused, dead_code)]
-            #(#attrs)*
-            trait #dummy_ident #generics: #super_trait {}
-        };
-    })
-}
-
-fn extern_impl_spec(
-    mod_path: Option<syn::Path>,
-    mut extern_item_impl: ExternItemImpl,
-) -> syn::Result<TokenStream> {
-    extern_item_impl.prepare(mod_path)?;
-
-    let dummy_impl_struct = &extern_item_impl.dummy_ident;
-    let generics = &extern_item_impl.generics;
-    let fields = generic_params_to_fields(&extern_item_impl.generics.params);
-
-    Ok(quote! {
-        #[allow(unused, dead_code, unused_variables)]
-        const _: () = {
-            #[flux_tool::ignore]
-            struct #dummy_impl_struct #generics ( #fields );
-
-            #[flux_tool::extern_spec]
-            #extern_item_impl
-        };
-    })
 }
 
 // Cribbed from Prusti's extern_spec_rewriter
