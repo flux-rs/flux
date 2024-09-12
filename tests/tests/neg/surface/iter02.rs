4ea0b2662cabd3d1e9e526005446880d8101d11c
Ranjit Jhala <rjhala@ucsd.edu>
2024-09-12 12:06:38 -0700

    Pulling the iter/enum specs out into a separate file (#788)
    * split iter/enum stuff out into separate file in lib

---
+++ tests/tests/neg/surface/iter02.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+use std::{iter::Enumerate, slice::Iter};
+
+#[path = "../../lib/option.rs"]
+mod option;
+
+#[path = "../../lib/slice.rs"]
+mod slice;
+
+#[path = "../../lib/iter.rs"]
+mod iter;
+
+#[flux::sig(fn(bool[true]))]
+pub fn assert(_b: bool) {}
+
+#[flux::sig(fn(slice: &[u8]{n: n > 0}))]
+fn test_iter1_neg(slice: &[u8]) {
+    assert(slice.len() > 0);
+    let mut iter = slice.iter();
+    let next = iter.next();
+    assert(next.is_some());
+    assert(iter.next().is_some()); //~ ERROR refinement type
+}
