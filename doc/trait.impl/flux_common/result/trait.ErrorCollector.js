2f22fdc48ea23d51f0bf02327389092a3057d0ac
ranjitjhala <jhala@cs.ucsd.edu>
2024-09-17 20:11:47 +0000

    Deploying to gh-pages from @ flux-rs/flux@5a766eb3fa1068a35f88c4d1e142161bca3b17c8 🚀

--- doc/trait.impl/flux_common/result/trait.ErrorCollector.js
+++ doc/trait.impl/flux_common/result/trait.ErrorCollector.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "flux_common":[],
-"flux_errors":[["impl <a class=\"trait\" href=\"flux_common/result/trait.ErrorCollector.html\" title=\"trait flux_common::result::ErrorCollector\">ErrorCollector</a>&lt;<a class=\"struct\" href=\"flux_errors/struct.ErrorGuaranteed.html\" title=\"struct flux_errors::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"]]
+"flux_errors":[["impl ErrorCollector&lt;<a class=\"struct\" href=\"flux_errors/struct.ErrorGuaranteed.html\" title=\"struct flux_errors::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
