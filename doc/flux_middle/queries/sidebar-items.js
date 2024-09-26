9d37c0fcb409e97a07042c17f7d74540b8efddc2
nilehmann <nico.lehmannm@gmail.com>
2024-09-26 06:08:08 +0000

    Deploying to gh-pages from @ flux-rs/flux@0fef78de45478d869adaf20d00090d83fb0f9c28 🚀

--- doc/flux_middle/queries/sidebar-items.js
+++ doc/flux_middle/queries/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["QueryErr"],"fn":["lookup_extern","run_with_cache"],"macro":["empty_query"],"struct":["Providers","Queries","QueryErrAt"],"type":["Cache","QueryResult"]};
+window.SIDEBAR_ITEMS = {"enum":["QueryErr","ResolvedDefId"],"fn":["dispatch_query","resolve_id","run_with_cache"],"macro":["empty_query"],"struct":["Providers","Queries","QueryErrAt"],"type":["Cache","QueryResult"]};
