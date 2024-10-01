be342c3cf2e79735b10a0467cadc3a78b59739a7
nilehmann <nico.lehmannm@gmail.com>
2024-10-01 23:15:20 +0000

    Deploying to gh-pages from @ flux-rs/flux@7a4979e6c970298efadd8a89a9a366224347ef87 🚀

--- doc/flux_driver/collector/extern_specs/sidebar-items.js
+++ doc/flux_driver/collector/extern_specs/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["ident_or_def_span"],"mod":["errors"],"struct":["ExternImplItem","ExternSpecCollector"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"fn":["cmp_generic_param_def","ident_or_def_span"],"mod":["errors"],"struct":["ExternImplItem","ExternSpecCollector"],"type":["Result"]};
