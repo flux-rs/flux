6151c5a67cda1e8263f61bec5206f88264c97c4c
Ranjit Jhala <rjhala@ucsd.edu>
2024-10-17 09:11:49 -0700

    move test into right spot

--- tests/tests/pos/structs/union00.rs
+++ tests/tests/pos/structs/union00.rs
@@ -10,3 +10,11 @@ pub fn bob(s: Silly) -> i32 {
 pub fn glob(s: Silly) -> u32 {
     unsafe { s.that }
 }
+
+pub fn new_this(v: i32) -> Silly {
+    Silly { this: v }
+}
+
+pub fn new_that(v: u32) -> Silly {
+    Silly { that: v }
+}
