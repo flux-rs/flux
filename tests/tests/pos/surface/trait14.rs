7022cfd5dbabedfd01cefee4f0d7ceef122729f9
Ranjit Jhala <rjhala@ucsd.edu>
2024-10-28 13:52:29 -0700

    add test for trusted_impl

---
+++ tests/tests/pos/surface/trait14.rs
@@ -0,0 +1,17 @@
+pub trait MyTrait {
+    // #[flux::trusted_impl]
+    fn foo1(&mut self);
+}
+
+impl MyTrait for i32 {
+    #[flux::sig(fn(self: &strg i32[@n] ) ensures self: i32[n+1])]
+    fn foo1(&mut self) {
+        *self += 1;
+    }
+}
+
+fn client() {
+    let mut x = 0;
+    x.foo1();
+    assert!(x == 1);
+}
