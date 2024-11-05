d004a40de4d9953c3fc0368a7b8e1fdb290fc358
Ranjit Jhala <rjhala@ucsd.edu>
2024-11-05 06:04:40 -0800

    Basic support for `for i in a..b { blah }`  (#873)
    * move range.rs to rrange.rs
    
    * basic tests to support for i in a..b {}
    
    * add rrange.rs

--- tests/tests/lib/range.rs
+++ tests/tests/lib/range.rs
@@ -1,57 +1,36 @@
-#[flux::refined_by(lo: int, hi: int)]
-pub struct RngIter {
-    #[flux::field(i32[lo])]
-    _lo: i32,
-    #[flux::field(i32[hi])]
-    hi: i32,
-    #[flux::field(i32{v:lo <= v && v <= hi})]
-    cur: i32,
+#![allow(unused)]
+
+use core::ops::Range;
+
+use flux_rs::extern_spec;
+
+#[flux_rs::extern_spec(core::ops)]
+#[flux_rs::refined_by(start: Idx, end: Idx)]
+struct Range<Idx> {
+    #[field(Idx[start])]
+    start: Idx,
+    #[field(Idx[end])]
+    end: Idx,
 }
 
-impl RngIter {
-    #[flux::sig(fn(lo: i32, hi: i32{lo <= hi}) -> RngIter[lo, hi])]
-    pub fn new(lo: i32, hi: i32) -> RngIter {
-        Self { _lo: lo, hi, cur: lo }
-    }
-}
+// #[flux_rs::extern_spec(core::ops)]
+// #[generics(Self as base, T as base)]
+// #[flux_rs::assoc(fn start(self: Self) -> T)]
+// #[flux_rs::assoc(fn end(self: Self) -> T)]
+// trait RangeBounds<T> {
+//     #[flux_rs::sig(fn(&Self) -> Bound<&T>)]
+//     fn start_bound(&self) -> Bound<&T>;
+//     #[flux_rs::sig(fn(&Self) -> Bound<&T>)]
+//     fn end_bound(&self) -> Bound<&T>;
+// }
 
-#[flux::refined_by(lo: int, hi: int)]
-pub struct Rng {
-    #[flux::field(i32[lo])]
-    lo: i32,
-    #[flux::field({i32[hi] | lo <= hi})]
-    hi: i32,
-}
-
-impl Rng {
-    #[flux::sig(fn(lo:i32, hi:i32{lo <= hi}) -> Rng[lo, hi])]
-    pub fn new(lo: i32, hi: i32) -> Rng {
-        Self { lo, hi }
-    }
-}
-
-impl Iterator for RngIter {
-    type Item = i32;
-
-    #[flux::sig(fn(self: &mut RngIter[@lo, @hi]) -> Option<i32{v: lo <= v && v < hi}>)]
-    fn next(&mut self) -> Option<i32> {
-        let cur = self.cur;
-        let hi = self.hi;
-        if cur < hi {
-            self.cur = cur + 1;
-            Some(cur)
-        } else {
-            None
-        }
-    }
-}
-
-impl IntoIterator for Rng {
-    type Item = i32;
-    type IntoIter = RngIter;
-
-    #[flux::sig(fn(Rng[@lo, @hi]) -> RngIter[lo, hi])]
-    fn into_iter(self) -> RngIter {
-        RngIter::new(self.lo, self.hi)
-    }
-}
+// #[flux_rs::extern_spec(core::ops)]
+// #[generics(T as base)]
+// #[flux_rs::assoc(fn start(self: Range<T>) -> T { self.end })]
+// #[flux_rs::assoc(fn end(self: Range<T>) -> T { self.end })]
+// impl<T> RangeBounds<T> for Range<T> {
+//     #[flux_rs::sig(fn(&Range<T>[@r]) -> Bound<&T>[true, false])]
+//     fn start_bound(&self) -> Bound<&T>;
+//     #[flux_rs::sig(fn(&Range<T>[@r]) -> Bound<&T>[true, false])]
+//     fn end_bound(&self) -> Bound<&T>;
+// }
