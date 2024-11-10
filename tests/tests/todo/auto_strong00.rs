90b3bacbb612fdba45bd48140a3eb90acfbf20ea
Ranjit Jhala <rjhala@ucsd.edu>
2024-11-09 17:16:54 -0800

    next: fold local_ptr to get client_unsafe to be rejected

--- tests/tests/todo/auto_strong00.rs
+++ tests/tests/todo/auto_strong00.rs
@@ -6,16 +6,11 @@ fn incr(x: &mut i32) {
 }
 
 #[flux::sig(fn (x: &mut i32{v: 0<=v}))]
-fn client(z: &mut i32) {
+fn client_safe(z: &mut i32) {
     incr(z);
 }
 
-// #[flux::sig(fn incr(x: &strg i32[@n]) ensures x: i32[n+1])]
-// fn incr(x: &mut i32) {
-//     *x += 1;
-// }
-
-// #[flux::sig(fn (x: &strg i32{v: 0<=v}) ensures x: i32{v: 0<=v})]
-// fn client(z: &mut i32) {
-//     incr(z);
-// }
+#[flux::sig(fn (x: &mut i32{v: v<=10}))]
+fn client_unsafe(z: &mut i32) {
+    incr(z);
+}
