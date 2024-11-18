b1ab06f05353b681fe321ce0f76251f654388bbf
Ranjit Jhala <rjhala@ucsd.edu>
2024-11-18 10:51:35 -0800

    only parse _ ConstArg

--- tests/tests/pos/surface/const06.rs
+++ tests/tests/pos/surface/const06.rs
@@ -1,5 +1,2 @@
-#[flux::sig(fn (head: [i32; N]))]
-fn test1<const N: usize>(head: [i32; N]) {}
-
 #[flux::sig(fn (head: [i32; _]))]
-fn test2<const N: usize>(head: [i32; N]) {}
+pub fn test2<const N: usize>(_head: [i32; N]) {}
