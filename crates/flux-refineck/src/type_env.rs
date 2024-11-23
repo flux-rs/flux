8001e530720052f62a6aa04933ad70bff982fffd
Ranjit Jhala <rjhala@ucsd.edu>
2024-11-22 20:53:06 -0800

    hack move_place -- to basically ignore the 'move' part

--- crates/flux-refineck/src/type_env.rs
+++ crates/flux-refineck/src/type_env.rs
@@ -263,13 +263,13 @@ impl<'a> TypeEnv<'a> {
 
     pub(crate) fn move_place(&mut self, infcx: &mut InferCtxtAt, place: &Place) -> Result<Ty> {
         let result = self.bindings.lookup_unfolding(infcx, place)?;
-        if result.is_constant_index {
+        if result.is_strg {
+            let uninit = Ty::uninit();
+            Ok(result.update(uninit))
+        } else {
             // ignore the 'move' and trust rustc managed the move correctly
             // https://github.com/flux-rs/flux/issues/725#issuecomment-2295065634
             Ok(result.ty)
-        } else {
-            let uninit = Ty::uninit();
-            Ok(result.update(uninit))
         }
     }
 
