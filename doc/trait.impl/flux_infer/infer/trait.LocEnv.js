09e102ecca9c039e7bed18bd4902b47adaae6abc
vrindisbacher <56514534+vrindisbacher@users.noreply.github.com>
2024-11-26 00:16:20 +0000

    Deploying to gh-pages from @ flux-rs/flux@4d49970d1f56aba2d2d7bd5c4b2f4377cd56f554 🚀

--- doc/trait.impl/flux_infer/infer/trait.LocEnv.js
+++ doc/trait.impl/flux_infer/infer/trait.LocEnv.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"flux_refineck":[["impl LocEnv for <a class=\"struct\" href=\"flux_refineck/type_env/struct.TypeEnv.html\" title=\"struct flux_refineck::type_env::TypeEnv\">TypeEnv</a>&lt;'_&gt;"]]
+"flux_refineck":[["impl <a class=\"trait\" href=\"flux_infer/infer/trait.LocEnv.html\" title=\"trait flux_infer::infer::LocEnv\">LocEnv</a> for <a class=\"struct\" href=\"flux_refineck/type_env/struct.TypeEnv.html\" title=\"struct flux_refineck::type_env::TypeEnv\">TypeEnv</a>&lt;'_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
