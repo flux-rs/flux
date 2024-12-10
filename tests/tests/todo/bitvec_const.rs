32993ecf7b4b91064d9dd70f0723966f80f6fbe9
Ranjit Jhala <rjhala@ucsd.edu>
2024-12-10 08:31:00 -0800

    temp chekin

--- tests/tests/todo/bitvec_const.rs
+++ tests/tests/todo/bitvec_const.rs
@@ -2,6 +2,7 @@
     fn is_start(x:bitvec<32>) -> bool { x == ICER_START }
 )]
 
+// reflect_as (fn bob(BV32[@val]) -> sort { expr-over-val })
 #[flux_rs::opaque]
 #[flux_rs::refined_by(x: bitvec<32>)]
 pub struct BV32(u32);
@@ -12,4 +13,4 @@ const fn into_bv32(x: u32) -> BV32 {
     BV32(x)
 }
 
-pub const ICER_START: BV32 = into_bv32(0xE000E180);
+pub const ICER_START: BV32 = into_bv32(0xE000E180); // BV[const_mickey]
