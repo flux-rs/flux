4946ca4cb654352f5b12188646c4933f3061b111
Ranjit Jhala <rjhala@ucsd.edu>
2024-12-12 20:58:55 -0800

    bloop

--- tests/tests/pos/surface/const07.rs
+++ tests/tests/pos/surface/const07.rs
@@ -3,7 +3,7 @@ pub struct Cow {}
 impl Cow {
     const GRASS: usize = 12;
 
-    /// TODO: #[flux_rs::sig(fn () -> usize[12])]
+    #[flux_rs::sig(fn () -> usize[12])]
     pub fn test() -> usize {
         Self::GRASS
     }
