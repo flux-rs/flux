9dcfe49abe1f049dbb159dcd195ae161432777bf
nilehmann <nico.lehmannm@gmail.com>
2025-03-05 19:14:33 +0000

    Deploying to gh-pages from @ flux-rs/flux@b1395baff12ba8bce3b61fafec5ff6a8da9134b1 🚀

--- doc/trait.impl/flux_common/result/trait.ErrorCollector.js
+++ doc/trait.impl/flux_common/result/trait.ErrorCollector.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_common",[]],["flux_errors",[["impl ErrorCollector&lt;<a class=\"struct\" href=\"flux_errors/struct.ErrorGuaranteed.html\" title=\"struct flux_errors::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["flux_common",[]],["flux_desugar",[["impl <a class=\"trait\" href=\"flux_common/result/trait.ErrorCollector.html\" title=\"trait flux_common::result::ErrorCollector\">ErrorCollector</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/nightly-rustc/rustc_span/struct.ErrorGuaranteed.html\" title=\"struct rustc_span::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_desugar/desugar/struct.FluxItemCtxt.html\" title=\"struct flux_desugar::desugar::FluxItemCtxt\">FluxItemCtxt</a>&lt;'_, '_&gt;"],["impl <a class=\"trait\" href=\"flux_common/result/trait.ErrorCollector.html\" title=\"trait flux_common::result::ErrorCollector\">ErrorCollector</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/nightly-rustc/rustc_span/struct.ErrorGuaranteed.html\" title=\"struct rustc_span::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_desugar/desugar/struct.RustItemCtxt.html\" title=\"struct flux_desugar::desugar::RustItemCtxt\">RustItemCtxt</a>&lt;'_, '_, '_&gt;"]]],["flux_errors",[["impl ErrorCollector&lt;<a class=\"struct\" href=\"flux_errors/struct.ErrorGuaranteed.html\" title=\"struct flux_errors::ErrorGuaranteed\">ErrorGuaranteed</a>&gt; for <a class=\"struct\" href=\"flux_errors/struct.Errors.html\" title=\"struct flux_errors::Errors\">Errors</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[18,306]}
+//{"start":57,"fragment_lengths":[18,1031,306]}
