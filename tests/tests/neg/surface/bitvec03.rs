ad81708c56370b9e35df7b65f7f94bc760a5725a
Ranjit Jhala <rjhala@ucsd.edu>
2025-03-07 10:32:11 -0800

    and/or ok

--- tests/tests/neg/surface/bitvec03.rs
+++ tests/tests/neg/surface/bitvec03.rs
@@ -2,38 +2,14 @@
 
 use flux_rs::{attrs::*, bitvec::BV32};
 
-#[sig(fn (x:BV32{x == 1}) ensures (bv_shl(x, 3) == 8))]
-pub fn test_shl_a(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 1}) ensures (x << 3 == 8))]
-pub fn test_shl_b(_x: BV32) {}
-
 #[sig(fn (x:BV32{x == 1}) ensures (x << 3 == 9))]
 pub fn test_shl_b_err(_x: BV32) {} //~ ERROR refinement type
-
-#[sig(fn (x:BV32{x == 8}) ensures (bv_lshr(x, 3) == 1))]
-pub fn test_shr_a(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 8}) ensures (x >> 3 == 1))]
-pub fn test_shr_b(_x: BV32) {}
 
 #[sig(fn (x:BV32{x == 8}) ensures (x >> 3 == 2))]
 pub fn test_shr_b_err(_x: BV32) {} //~ ERROR refinement type
 
-#[sig(fn (x:BV32{x == 8}) ensures (bv_or(x, 3) == 11))]
-pub fn test_or_a(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 8}) ensures (x | 3 == 11))]
-pub fn test_or_b(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 8}) ensures (x | 3 == 12))] //~ ERROR refinement type
-pub fn test_or_b_err(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 10}) ensures (bv_and(x, 3) == 2))]
-pub fn test_and_a(_x: BV32) {}
-
-#[sig(fn (x:BV32{x == 10}) ensures (x & 3 == 2))]
-pub fn test_and_b(_x: BV32) {}
+#[sig(fn (x:BV32{x == 8}) ensures (x | 3 == 12))]
+pub fn test_or_b_err(_x: BV32) {} //~ ERROR refinement type
 
 #[sig(fn (x:BV32{x == 10}) ensures (x & 3 == 20))]
 pub fn test_and_b_err(_x: BV32) {} //~ ERROR refinement type
