88b8ffddd0b118ded98941556ea4bbbace35eebb
Nico Lehmann <nico.lehmannm@gmail.com>
2025-03-12 09:44:03 -0700

    Some improvements to perf script

--- tools/flux-perf.sh
+++ tools/flux-perf.sh
@@ -1,33 +1,40 @@
 #! /usr/bin/bash
 #
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
 # This script wraps `flux-driver` to collect performance profiling data for
 # specific crates using Linux's perf tool (https://perfwiki.github.io/).
 #
 # Usage:
 #   1. Set the `FLUX_DRIVER` environment variable to point to this script
 #   2. Configure the `CRATES` variable below to list the crates you want to profile
-#   3. Run `cargo flux` as normal, and this script will automatically
-#      profile the specified crates
+#   3. Run `cargo flux` as normal, and this script will automatically profile Flux when
+#      checking the specified  crates
 #
 # The script will generate `{crate_name}-perf.data` files that can be analyzed with:
 #   `perf report -i {crate_name}-perf.data`
-# or any other analysis tool compatible with perf.
+# or any other analysis tool compatible with perf. It will also generate a textual version
+# that can be open with the Firefox profiler. If `rustfilt` is present it will also demangled
+# names that were not present in the debug info.
 #
 # Configuration:
 #   - CRATES: Space-separated list of crate names to profile
 #   - PERF_FLAGS: Array of flags passed to 'perf record' command
-#     Default flags:
-#       -F 997: Sample at 997 Hz (prime number close to 1000, reduces aliasing)
-#       --call-graph dwarf,16384: Collect call-graph data using DWARF with 16K stack size
-#       -g: Enable call-graph recording
-# -----------------------------------------------------------------------------
+#          Default flags:
+#            -F 997: Sample at 997 Hz (prime number close to 1000)
+#            --call-graph dwarf,16384: Collect call-graph data using DWARF with 16K stack size
+#            -g: Enable call-graph recording
+#            -z: Compress the perf data file output
+#   - TIMEOUT: Run profiled process for specified duration and then terminate it. Prevents
+#              hanging or excessively long-running processes. Set to 0 to disable timeout.
+# --------------------------------------------------------------------------------------------
 
-# Specify the crate names to profile (space-separated). These are the name of the crates
-# not the packages, i.e., the name of `[lib]` or a `[[bin]`.
-CRATES="crate1 crate2"
+# Specify the crate names to profile (space-separated). These are crate names not  packages, i.e.,
+# the name of `[lib]` or a `[[bin]`.
+CRATES="veriasm"
 # Configure perf sampling parameters
-PERF_FLAGS=("-F" "997" "--call-graph" "dwarf,16384" "-g")
+PERF_FLAGS=("-F" "997" "--call-graph" "dwarf,16384" "-g" "-z")
+# Set timeout to prevent process from running too long
+TIMEOUT=1m
 
 # This will guess the path of the real `flux-driver`. Adjust accordingly if necessary
 export FLUX_SYSROOT="${FLUX_SYSROOT:-$HOME/.flux}"
@@ -47,11 +54,17 @@ for ((i = 0; i < ${#args[@]}; i++)); do
 done
 
 if [[ -n "$crate_name" && " $CRATES " =~ .*\ $crate_name\ .* ]]; then
-    # Run with perf if the current crate is in our target list
     info "Running Flux with 'perf' for crate: $crate_name"
     info "Command: $FLUX_DRIVER" "$@"
-    timeout 60 perf record "${PERF_FLAGS[@]}" -o "$crate_name-perf.data" "$FLUX_DRIVER" "$@"
+    timeout $TIMEOUT perf record "${PERF_FLAGS[@]}" -o "$crate_name-perf.data" "$FLUX_DRIVER" "$@"
+
+    info "Converting perf data to text format: $crate_name.perf"
+    perf script -F +pid -i "$crate_name-perf.data" > "$crate_name.perf"
+
+    if command -v rustfilt >/dev/null 2>&1; then
+        info "Demangling Rust symbols: $crate_name-demangled.perf"
+        rustfilt -i "$crate_name.perf" -o "$crate_name-demangled.perf"
+    fi
 else
-    # Run flux-driver normally for non-targeted crates
     "$FLUX_DRIVER" "$@"
 fi
