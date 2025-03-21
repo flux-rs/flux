5db7570cfae8597c04e0f5f9b1fca72bd60e4424
Cole K <cole-k@users.noreply.github.com>
2025-03-20 21:45:00 -0700

    fix function argument spans

--- crates/flux-infer/src/refine_tree.rs
+++ crates/flux-infer/src/refine_tree.rs
@@ -18,7 +18,7 @@ use flux_middle::{
 };
 use itertools::Itertools;
 use rustc_middle::ty::TyCtxt;
-use rustc_span::Span;
+use rustc_span::{Span, Symbol};
 use serde::Serialize;
 
 use crate::{
@@ -834,8 +834,8 @@ pub enum BinderOriginator {
     Call,
     /// The return of a function call
     CallReturn,
-    /// Arguments from the definition of a function
-    FnDef,
+    /// Argument from the definition of a function
+    FnArg(Option<Symbol>),
     /// Unfold a local pointer
     UnfoldPtr,
     /// Unfold a strong ref
