215c0af62ff2940ffd2edc53e1231841a689743e
nilehmann <nico.lehmannm@gmail.com>
2025-03-24 18:15:16 +0000

    Deploying to gh-pages from @ flux-rs/flux@ae08932919667e243059e26f37c54b8ee7b6a9c8 🚀

--- doc/flux_infer/projections/sidebar-items.js
+++ doc/flux_infer/projections/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Candidate"],"fn":["assemble_candidates_from_predicates"],"struct":["Normalizer","TVarSubst"],"trait":["NormalizeExt"]};
+window.SIDEBAR_ITEMS = {"enum":["Candidate"],"fn":["assemble_candidates_from_predicates","normalize_projection_ty_with_rustc"],"struct":["Normalizer","SortNormalizer","TVarSubst"],"trait":["NormalizeExt"]};
