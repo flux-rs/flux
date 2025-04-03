f6419386cd33f71de7dadda98726ab99401fd207
nilehmann <nico.lehmannm@gmail.com>
2025-04-03 19:25:31 +0000

    Deploying to gh-pages from @ flux-rs/flux@5e0cef37dd009280774a6808fdafef475b4e6e91 🚀

--- doc/trait.impl/core/convert/trait.Into.js
+++ doc/trait.impl/core/convert/trait.Into.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"flux_rs/bitvec/struct.BV32.html\" title=\"struct flux_rs::bitvec::BV32\">BV32</a>"]]]]);
+    var implementors = Object.fromEntries([["flux_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"flux_rs/bitvec/struct.BV32.html\" title=\"struct flux_rs::bitvec::BV32\">BV32</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[375]}
+//{"start":57,"fragment_lengths":[376]}
