7b3b1834c7e4c71095bd198d1198a2d370acda5f
Ranjit Jhala <rjhala@ucsd.edu>
2025-05-05 11:22:58 -0700

    next: why u no check postcondition

---
+++ tests/tests/todo/closure000.rs
@@ -0,0 +1,67 @@
+#[flux::sig(fn (frog: F) -> i32{v:0<=v} where F: FnOnce(i32{v:0 <= v}) -> i32{v:0 <= v})]
+fn test001<F>(frog: F) -> i32
+where
+    F: FnOnce(i32) -> i32,
+{
+    frog(99)
+}
+
+// fn test001_client() {
+//     test001(|x| x);
+// }
+
+// #[flux::sig(fn (f: F) -> i32{v:0<=v}
+//             where F: FnOnce(i32{v:0 <= v}) -> i32{v:0 <= v})]
+// fn test0<F>(f: F) -> i32
+// where
+//     F: FnOnce(i32) -> i32,
+// {
+//     f(99)
+// }
+
+// #[flux::sig(fn (f: F) -> i32{v:0<=v}
+//             where F: FnOnce(i32{v:0 <= v}, i32{v:0<=v})
+//         )]
+// fn test002<F>(f: F) -> i32
+// where
+//     F: FnOnce(i32, i32) -> i32,
+// {
+//     f(99, 100)
+// }
+
+// fn test002_client() {
+//     test002(|x, y| x + y);
+// }
+
+// #[flux::sig(fn (f: F) -> i32[100]
+//             where F: FnOnce(i32[@k]) -> i32[k+1])]
+// fn test0<F>(f: F) -> i32
+// where
+//     F: FnOnce(i32) -> i32,
+// {
+//     f(99)
+// }
+
+// fn test0<F>(f: F) -> bool
+// where
+//     F: FnOnce(i32) -> bool,
+// {
+//     f(99)
+// }
+
+// fn client0_ok() -> bool {
+//     test0(|k| k > 1)
+// }
+
+// fn test0<F>(f: F) -> i32
+// where
+//     F: for<'apple> FnOnce(&'apple i32) -> &'apple i32,
+// {
+//     let z = 99;
+//     f(&z);
+//     z
+// }
+
+// fn client0_ok() {
+//     test0(|k| k);
+// }
