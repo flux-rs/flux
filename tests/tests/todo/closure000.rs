196a2c27962b9ad801800d63f6ee92dd28023183
Ranjit Jhala <rjhala@ucsd.edu>
2025-05-08 10:54:26 -0700

    next: fix closure-client

--- tests/tests/todo/closure000.rs
+++ tests/tests/todo/closure000.rs
@@ -9,6 +9,7 @@
 //     MkBoogle(usize),
 // }
 
+#[flux::trusted]
 #[flux::sig(fn (f: F) -> i32[100]
             where F: FnOnce(i32[@king]) -> i32[king+1])]
 pub fn test0<F>(f: F) -> i32
@@ -17,3 +18,14 @@ where
 {
     f(99)
 }
+
+#[flux::sig(fn () -> i32[100])]
+pub fn client0() -> i32 {
+    test0(|z| z + 1)
+}
+
+// fn client1<F>(f: F) -> i32[100]
+// where F: FnOnce(i32[@kong]) -> i32[kong+1]
+// {
+//     test0(f)
+// }
