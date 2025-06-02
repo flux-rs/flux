bb95a76d6a0c3cfd95b56ee8c479d728dc0169d6
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-02 16:34:06 -0700

    Update crates/flux-middle/src/global_env.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- crates/flux-middle/src/global_env.rs
+++ crates/flux-middle/src/global_env.rs
@@ -418,12 +418,6 @@ impl<'genv, 'tcx> GlobalEnv<'genv, 'tcx> {
     /// corresponds to `LangItem::FnOnceOutput`. However, we may need to do some extra
     /// work to get the correct `ClosureKind`?
     pub fn is_fn_output(&self, def_id: DefId) -> Option<ClosureKind> {
-        // let tcx = self.tcx();
-        // let assoc_item = tcx.opt_associated_item(def_id)?;
-        // let trait_def_id = assoc_item.trait_container(tcx)?;
-        // let res = self.tcx().fn_trait_kind_from_def_id(trait_def_id);
-        // println!("TRACE: is_fn_output ==> {res:?}");
-        // res
         if self
             .tcx()
             .require_lang_item(rustc_hir::LangItem::FnOnceOutput, None)
