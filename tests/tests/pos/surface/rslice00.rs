af56b48f598cf576d498c93a7761da07e47030fc
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-06 09:57:29 -0700

    temp checkin

--- tests/tests/pos/surface/rslice00.rs
+++ tests/tests/pos/surface/rslice00.rs
@@ -20,14 +20,15 @@ fn add(x: &mut [i32], y: &[i32]) {
     let mut i = 0;
     while i < len(x) {
         x[i] += y[i];
+        i += 1;
     }
 }
 
-// #[flux::sig(fn(&mut {RVec<i32>[@n] | n % 2 == 0 && n > 0}))]
-// fn test01(vec: &mut RVec<i32>) {
-//     let n = vec.len();
-//     let mut s = RSlice::from_vec(vec);
-//     let mut s1 = s.subslice(0, n / 2 - 1);
-//     let s2 = s.subslice(n / 2, n - 1);
-//     add(s1.as_mut_slice(), s2.as_slice())
-// }
+#[flux::sig(fn(&mut {RVec<i32>[@n] | n % 2 == 0 && n > 0}))]
+fn test01(vec: &mut RVec<i32>) {
+    let n = vec.len();
+    let mut s = RSlice::from_vec(vec);
+    let mut s1 = s.subslice(0, n / 2 - 1);
+    let s2 = s.subslice(n / 2, n - 1);
+    add(s1.as_mut_slice(), s2.as_slice())
+}
