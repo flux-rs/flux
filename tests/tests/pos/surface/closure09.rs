17b4d36c13aa5cf98f220d7f3a23ccfc91fcae2f
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-09 14:06:21 -0700

    loop self-ty search to parent (if possible)

--- tests/tests/pos/surface/closure09.rs
+++ tests/tests/pos/surface/closure09.rs
@@ -11,13 +11,3 @@ where
 pub fn client0() -> i32 {
     test0(|z| z + 1)
 }
-
-struct Blah(i32);
-
-impl<P: FnOnce(i32) -> i32> Blah {
-    #[flux::sig(fn(self: &Self, f: P) -> i32[100]
-                where P: FnOnce(i32[@king]) -> i32[king+1])]
-    fn test1(self, f: P) -> i32 {
-        f(self.0)
-    }
-}
