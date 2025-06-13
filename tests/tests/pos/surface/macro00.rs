66a0b959c05d75b2b9838aa69d9466db8b87dbd4
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-13 10:54:34 -0700

    add macro test

--- tests/tests/pos/surface/macro00.rs
+++ tests/tests/pos/surface/macro00.rs
@@ -8,3 +8,21 @@ macro_rules! gen_id {
 }
 
 gen_id!(i32);
+
+macro_rules! gen_foo {
+    ($ty:ty) => {
+        #[flux_rs::trusted]
+        #[flux_rs::sig(fn(x: $ty) -> Foo<$ty>[0])]
+        pub fn foo(x: $ty) -> Foo<$ty> {
+            Foo { inner: x }
+        }
+    };
+}
+
+#[flux_rs::opaque]
+#[flux_rs::refined_by(n:int)]
+struct Foo<T> {
+    inner: T,
+}
+
+gen_foo!(i32);
