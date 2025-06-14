c6522cc56029620ee0c19f69e21f83c2fa137265
cole-k <cole-k@users.noreply.github.com>
2025-06-14 19:58:00 +0000

    Deploying to gh-pages from @ flux-rs/flux@39a8aabcccccacefba1485d34bb56ab80a212998 🚀

--- doc/trait.impl/flux_middle/rty/subst/trait.GenericsSubstDelegate.js
+++ doc/trait.impl/flux_middle/rty/subst/trait.GenericsSubstDelegate.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl GenericsSubstDelegate for &amp;<a class=\"struct\" href=\"flux_infer/projections/struct.TVarSubst.html\" title=\"struct flux_infer::projections::TVarSubst\">TVarSubst</a>"]]],["flux_middle",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"flux_middle/rty/subst/trait.GenericsSubstDelegate.html\" title=\"trait flux_middle::rty::subst::GenericsSubstDelegate\">GenericsSubstDelegate</a> for &amp;<a class=\"struct\" href=\"flux_infer/projections/struct.TVarSubst.html\" title=\"struct flux_infer::projections::TVarSubst\">TVarSubst</a>"]]],["flux_middle",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[196,19]}
+//{"start":57,"fragment_lengths":[346,19]}
