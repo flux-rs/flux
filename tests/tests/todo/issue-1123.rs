ebbf5f130c91ff1fc48fd186341427bf33b1db8c
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-17 10:26:03 -0700

    next: sort mismatch

--- tests/tests/todo/issue-1123.rs
+++ tests/tests/todo/issue-1123.rs
@@ -1,7 +1,25 @@
+#![allow(unused)]
+
+use core::cmp::PartialOrd;
+
 #[flux_rs::extern_spec(core::cmp)]
-#[flux_rs::assoc(fn lt(this: Self, other: Self) -> bool)]
-#[flux_rs::assoc(fn le(this: Self, other: Self) -> bool)]
+#[flux_rs::assoc(fn is_lt(this: Self, other: Rhs) -> bool { true })] // dumb default for now?
+#[flux_rs::assoc(fn is_le(this: Self, other: Rhs) -> bool { true })]
 trait PartialOrd<Rhs: ?Sized = Self>: PartialEq<Rhs> {
+    #[flux_rs::sig(fn (&Self[@l], &Rhs[@r]) -> bool[<Self as PartialOrd>::is_lt(l, r)])]
     fn lt(&self, other: &Rhs) -> bool;
+
+    #[flux_rs::sig(fn (&Self[@l], &Rhs[@r]) -> bool[<Self as PartialOrd>::is_le(l, r)])]
     fn le(&self, other: &Rhs) -> bool;
 }
+
+// #[flux_rs::extern_spec(core::cmp)]
+// #[flux_rs::assoc(fn lt(this: Self, other: Rhs) -> bool { true })] // dumb default for now?
+// #[flux_rs::assoc(fn le(this: Self, other: Rhs) -> bool { true })]
+// trait PartialOrd<Rhs: ?Sized = Self> {
+//     #[flux_rs::sig(fn (&Self[@l], &Rhs[@r]) -> bool[<Self as PartialOrd>::lt(l, r)])]
+//     fn lt(&self, other: &Rhs) -> bool;
+
+//     #[flux_rs::sig(fn (&Self[@l], &Rhs[@r]) -> bool[<Self as PartialOrd>::le(l, r)])]
+//     fn le(&self, other: &Rhs) -> bool;
+// }
