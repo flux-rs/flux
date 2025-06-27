13471a760f30a0fef02e94a83abeeecea5cf25d1
Ranjit Jhala <rjhala@ucsd.edu>
2025-06-27 08:57:45 -0700

    add docs

--- crates/flux-config/src/flags.rs
+++ crates/flux-config/src/flags.rs
@@ -18,8 +18,9 @@ pub struct Flags {
     pub check_def: String,
     /// If present, only check files matching a glob pattern. This flag can be specified multiple
     /// times and a file will be checked if it matches any of the patterns. Patterns are checked
-    /// relative to the current working directory. For example, to check all the files in the `ascii`
-    /// module you can `include` the pattern `"src/ascii/*"`
+    /// relative to the current working directory. For example, to check all the files in the
+    /// `ascii` module of a crate, you can `include` the pattern `"src/ascii/*"` (assuming that's
+    /// where the files are located).
     pub include: Option<GlobSet>,
     /// Set the pointer size (either `32` or `64`), used to determine if an integer cast is lossy
     /// (default `64`).
