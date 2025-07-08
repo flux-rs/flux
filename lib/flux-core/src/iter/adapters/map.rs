f99d19cb2673288a2f3908269274a9018416b5c7
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-08 14:51:41 -0700

    move Iterator/Map

--- lib/flux-core/src/iter/adapters/map.rs
+++ lib/flux-core/src/iter/adapters/map.rs
@@ -3,3 +3,9 @@ use flux_attrs::*;
 #[extern_spec(core::iter)]
 #[refined_by(inner: I)]
 struct Map<I, F>;
+
+#[extern_spec(core::iter)]
+#[assoc(fn size(x: Map<I>) -> int { <I as Iterator>::size(x.inner) })]
+#[assoc(fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner)})]
+#[assoc(fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)})]
+impl<B, I: Iterator, F: FnMut(I::Item) -> B> Iterator for Map<I, F> {}
--- tests/tests/lib/iter.rs
+++ tests/tests/lib/iter.rs
@@ -1,13 +1,5 @@
 use flux_rs::extern_spec;
 
-#[extern_spec(std::iter)]
-#[flux::assoc(fn size(x: Map<I>) -> int { <I as Iterator>::size(x.inner) })]
-#[flux::assoc(fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner)})]
-#[flux::assoc(fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)})]
-impl<B, I: Iterator, F: FnMut(I::Item) -> B> Iterator for Map<I, F> {} // orig: where F: FnMut(I::Item) -> B {}
-
-// -------------------------------------------------------------------------------------------------------------------------------------
-
 #[flux_rs::extern_spec(std::iter)]
 trait IntoIterator {
     #[flux_rs::sig(fn(self: Self) -> Self::IntoIter)]
