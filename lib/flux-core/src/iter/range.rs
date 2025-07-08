bd0529833a08ecc28a8ff3a3271cb53cff378583
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-08 11:25:16 -0700

    add doc for Step specs

--- lib/flux-core/src/iter/range.rs
+++ lib/flux-core/src/iter/range.rs
@@ -1,6 +1,9 @@
 use flux_attrs::*;
-
-// TODO(RJ): use default spec `true` for `can_step_forward` and `can_step_backward`
+// The below are "default" implementations of the associated refinements
+// for the `Step` trait, that we put in so that types for which no explicit
+// implementation is given can be analyzed without Flux complaining about missing
+// implementations. Note that the implementations are "uninterpreted" to make verification
+// sound. However, you may get "false positives" if you use these defaults.
 
 defs! {
     fn default_step_can_step_forward<T>(start: T, count: int) -> bool;
@@ -8,6 +11,11 @@ defs! {
     fn default_step_size<T>(lo: T, hi: T) -> int;
 }
 
+/// We define the following associated refinements for the `Step` trait, which are then
+/// used to specify the API for the `Iterator` implementation for `Range<A>`.
+///  - `can_step_forward` checks if we can step forward from `start` by `count`,
+///  - `step_forward` computes the new value after stepping forward from `start` by `count`,
+///  - `size` computes the number of steps needed to go from `lo` to `hi
 #[extern_spec(core::iter)]
 trait Step {
     #![reft(
