15a552c77e66f6eb5246021268e3b230902a5906
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-08 14:49:16 -0700

    move Iterator/Enumerate

--- tests/tests/lib/iter.rs
+++ tests/tests/lib/iter.rs
@@ -1,16 +1,6 @@
 use flux_rs::extern_spec;
 
 #[extern_spec(std::iter)]
-#[flux::assoc(fn size(x: Enumerate<I>) -> int { <I as Iterator>::size(x.inner) })]
-#[flux::assoc(fn done(x: Enumerate<I>) -> bool { <I as Iterator>::done(x.inner)})]
-#[flux::assoc(fn step(x: Enumerate<I>, y: Enumerate<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)})]
-impl<I: Iterator> Iterator for Enumerate<I> {
-    #[flux::sig(fn(self: &strg Enumerate<I>[@curr_s]) -> Option<(usize[curr_s.idx], _)>[!<I as Iterator>::done(curr_s.inner)]
-    ensures self: Enumerate<I>{next_s: curr_s.idx + 1 == next_s.idx && <I as Iterator>::step(curr_s.inner, next_s.inner)})]
-    fn next(&mut self) -> Option<(usize, <I as Iterator>::Item)>;
-}
-
-#[extern_spec(std::iter)]
 #[flux::assoc(fn size(x: Map<I>) -> int { <I as Iterator>::size(x.inner) })]
 #[flux::assoc(fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner)})]
 #[flux::assoc(fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)})]
