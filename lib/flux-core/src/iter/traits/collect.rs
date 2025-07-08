1c6c7e9f69ec5503fe9a2738f5ea188da7349ef3
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-08 15:02:29 -0700

    remove iter.rs

--- lib/flux-core/src/iter/traits/collect.rs
+++ lib/flux-core/src/iter/traits/collect.rs
@@ -3,3 +3,17 @@ use flux_attrs::*;
 #[extern_spec]
 #[flux::assoc(fn with_size(self: Self, n:int) -> bool { true })] // default: don't know!
 trait FromIterator<A> {}
+
+#[extern_spec(core::iter)]
+trait IntoIterator {
+    #[spec(fn(self: Self) -> Self::IntoIter)]
+    fn into_iter(self) -> Self::IntoIter
+    where
+        Self: Sized;
+}
+
+#[extern_spec(core::ops)]
+impl<I: Iterator> IntoIterator for I {
+    #[spec(fn(self: I[@s]) -> I[s])]
+    fn into_iter(self) -> I;
+}
