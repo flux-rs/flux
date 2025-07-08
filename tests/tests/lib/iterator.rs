ed6bebb2fd54859533caa45b3052d0c242645266
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-08 13:35:14 -0700

    remove Skip

--- tests/tests/lib/iterator.rs
+++ tests/tests/lib/iterator.rs
@@ -10,12 +10,3 @@ extern crate flux_core;
 
 #[path = "iter.rs"]
 mod iter;
-
-#[flux_rs::extern_spec(core::iter)]
-#[flux_rs::assoc(fn size(r: Skip<I>) -> int { <I as Iterator>::size(r.inner) } )]
-#[flux_rs::assoc(fn done(r: Skip<I>) -> bool { <I as Iterator>::done(r.inner) } )]
-#[flux_rs::assoc(fn step(self: Skip<I>, other: Skip<I>) -> bool { <I as Iterator>::step(self.inner, other.inner) } )]
-impl<I: Iterator> Iterator for Skip<I> {
-    #[flux_rs::sig(fn(&mut Skip<I>[@n, @inner]) -> Option<I::Item>[<I as Iterator>::done(inner)])]
-    fn next(&mut self) -> Option<I::Item>;
-}
