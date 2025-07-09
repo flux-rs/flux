6acce1e2ff1e4f8620a6ce64d337f8f786905cc5
ranjitjhala <jhala@cs.ucsd.edu>
2025-07-09 20:18:27 +0000

    Deploying to gh-pages from @ flux-rs/flux@6dfb7498c692d63f89c35cedbc3b5c3a2f1c4d1b 🚀

--- doc/flux_middle/rty/canonicalize/sidebar-items.js
+++ doc/flux_middle/rty/canonicalize/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CanonicalTy"],"mod":["pretty"],"struct":["CanonicalConstrTy","Hoister","LocalHoister"],"trait":["HoisterDelegate"]};
+window.SIDEBAR_ITEMS = {"enum":["CanonicalTy"],"fn":["is_indexed_slice"],"mod":["pretty"],"struct":["CanonicalConstrTy","Hoister","LocalHoister"],"trait":["HoisterDelegate"]};
