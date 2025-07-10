0de46673423af2c6c632f6810ff0682c687cc7ea
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-10 10:48:58 -0700

    Update lib/flux-core/src/slice/iter.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- lib/flux-core/src/slice/iter.rs
+++ lib/flux-core/src/slice/iter.rs
@@ -11,9 +11,11 @@ impl<'a, T> Iter<'a, T> {
 }
 
 #[extern_spec(core::slice)]
-#[flux::assoc(fn size(x: Iter) -> int { x.len - x.idx })]
-#[flux::assoc(fn done(x: Iter) -> bool { x.idx >= x.len })]
-#[flux::assoc(fn step(x: Iter, y: Iter) -> bool { x.idx + 1 == y.idx && x.len == y.len})]
+#[assoc(
+    fn size(x: Iter) -> int { x.len - x.idx }
+    fn done(x: Iter) -> bool { x.idx >= x.len }
+    fn step(x: Iter, y: Iter) -> bool { x.idx + 1 == y.idx && x.len == y.len}
+)]
 impl<'a, T> Iterator for Iter<'a, T> {
     #[spec(fn(self: &mut Iter<T>[@curr_s]) -> Option<_>[curr_s.idx < curr_s.len] ensures self: Iter<T>{next_s: curr_s.idx + 1 == next_s.idx && curr_s.len == next_s.len})]
     fn next(&mut self) -> Option<&'a T>;
