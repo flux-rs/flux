347cdcddbafa6fcaca175ccf647487b56b8ea7ad
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-10 12:06:57 -0700

    Update lib/flux-core/src/iter/adapters/map.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- lib/flux-core/src/iter/adapters/map.rs
+++ lib/flux-core/src/iter/adapters/map.rs
@@ -7,8 +7,8 @@ struct Map<I, F>;
 #[extern_spec(core::iter)]
 #[assoc(
     fn size(x: Map<I>) -> int { <I as Iterator>::size(x.inner) }
-    fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner)}
-    fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)}
+    fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner) }
+    fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner) }
 )]
 impl<B, I: Iterator, F: FnMut(I::Item) -> B> Iterator for Map<I, F> {
     #[spec(fn(self: &mut Self[@curr_s]) -> Option<B>[!<Self as Iterator>::done(curr_s)]
