db979890008d623271bb25b1967829aa574b3726
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-10 10:46:43 -0700

    Update lib/flux-core/src/iter/adapters/enumerate.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- lib/flux-core/src/iter/adapters/enumerate.rs
+++ lib/flux-core/src/iter/adapters/enumerate.rs
@@ -5,9 +5,13 @@ use flux_attrs::*;
 struct Enumerate<I>;
 
 #[extern_spec(core::iter)]
-#[assoc(fn size(x: Enumerate<I>) -> int { <I as Iterator>::size(x.inner) })]
-#[assoc(fn done(x: Enumerate<I>) -> bool { <I as Iterator>::done(x.inner)})]
-#[assoc(fn step(x: Enumerate<I>, y: Enumerate<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)})]
+#[assoc(
+    fn size(x: Enumerate<I>) -> int { <I as Iterator>::size(x.inner) }
+    fn done(x: Enumerate<I>) -> bool { <I as Iterator>::done(x.inner) }
+    fn step(x: Enumerate<I>, y: Enumerate<I>) -> bool { 
+        x.idx + 1 == y.idx && <I as Iterator>::step(x.inner, y.inner)
+    }
+)]
 impl<I: Iterator> Iterator for Enumerate<I> {
     #[spec(fn(self: &mut Enumerate<I>[@curr_s]) -> Option<(usize[curr_s.idx], _)>[!<I as Iterator>::done(curr_s.inner)]
            ensures self: Enumerate<I>{next_s: curr_s.idx + 1 == next_s.idx && <I as Iterator>::step(curr_s.inner, next_s.inner)})]
