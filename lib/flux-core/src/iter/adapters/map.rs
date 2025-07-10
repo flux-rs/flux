424efaff57c59d81303c977633a1e3e593000194
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-10 10:57:44 -0700

    add fn next for Map

--- lib/flux-core/src/iter/adapters/map.rs
+++ lib/flux-core/src/iter/adapters/map.rs
@@ -10,4 +10,8 @@ struct Map<I, F>;
     fn done(x: Map<I>) -> bool { <I as Iterator>::done(x.inner)}
     fn step(x: Map<I>, y: Map<I>) -> bool { <I as Iterator>::step(x.inner, y.inner)}
 )]
-impl<B, I: Iterator, F: FnMut(I::Item) -> B> Iterator for Map<I, F> {}
+impl<B, I: Iterator, F: FnMut(I::Item) -> B> Iterator for Map<I, F> {
+    #[spec(fn(self: &mut Self[@curr_s]) -> Option<B>[!<Self as Iterator>::done(curr_s)]
+             ensures self: Self{next_s: <Self as Iterator>::step(curr_s, next_s)})]
+    fn next(&mut self) -> Option<B>;
+}
