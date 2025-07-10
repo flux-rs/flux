ebf45b3c2843d8dd3f994c18ac05826e9646498c
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-10 12:27:03 -0700

    cleanup

--- lib/flux-alloc/src/vec/mod.rs
+++ lib/flux-alloc/src/vec/mod.rs
@@ -22,13 +22,13 @@ impl<T> Vec<T> {
 
 #[extern_spec]
 impl<T, A: Allocator> Vec<T, A> {
-    #[flux::sig(fn(self: &mut Vec<T, A>[@n], T) ensures self: Vec<T, A>[n+1])]
+    #[spec(fn(self: &mut Vec<T, A>[@n], T) ensures self: Vec<T, A>[n+1])]
     fn push(v: &mut Vec<T, A>, value: T);
 
-    #[flux::sig(fn(&Vec<T, A>[@n]) -> usize[n])]
+    #[spec(fn(&Vec<T, A>[@n]) -> usize[n])]
     fn len(v: &Vec<T, A>) -> usize;
 
-    #[flux::sig(fn(self: &mut Vec<T, A>[@n]) -> Option<T> ensures self: Vec<T, A>[if n > 0 { n-1 } else { 0 }])]
+    #[spec(fn(self: &mut Vec<T, A>[@n]) -> Option<T> ensures self: Vec<T, A>[if n > 0 { n-1 } else { 0 }])]
     fn pop(&mut self) -> Option<T>;
 }
 
@@ -36,13 +36,13 @@ impl<T, A: Allocator> Vec<T, A> {
 
 #[extern_spec]
 impl<T, I: SliceIndex<[T]>, A: Allocator> Index<I> for Vec<T, A> {
-    #[flux::sig(fn(&Vec<T, A>[@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
+    #[spec(fn(&Vec<T, A>[@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
     fn index(z: &Vec<T, A>, index: I) -> &<I as SliceIndex<[T]>>::Output;
 }
 
 #[extern_spec]
 impl<T, I: SliceIndex<[T]>, A: Allocator> IndexMut<I> for Vec<T, A> {
-    #[flux::sig(fn(&mut Vec<T,A>[@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
+    #[spec(fn(&mut Vec<T,A>[@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
     fn index_mut(z: &mut Vec<T, A>, index: I) -> &mut <I as SliceIndex<[T]>>::Output;
 }
 
@@ -50,10 +50,10 @@ impl<T, I: SliceIndex<[T]>, A: Allocator> IndexMut<I> for Vec<T, A> {
 
 #[extern_spec]
 impl<'a, T, A: Allocator> IntoIterator for &'a Vec<T, A> {
-    #[flux::sig(fn (&Vec<T, A>[@n]) -> <&Vec<T, A> as IntoIterator>::IntoIter[0,n])]
+    #[spec(fn (&Vec<T, A>[@n]) -> <&Vec<T, A> as IntoIterator>::IntoIter[0,n])]
     fn into_iter(v: &'a Vec<T, A>) -> <&'a Vec<T, A> as IntoIterator>::IntoIter;
 }
 
 #[extern_spec]
-#[flux::assoc(fn with_size(self: Self, n:int) -> bool { self.len == n })]
+#[assoc(fn with_size(self: Self, n:int) -> bool { self.len == n })]
 impl<T> FromIterator<T> for Vec<T> {}
