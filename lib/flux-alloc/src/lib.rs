4bb0eb049a2d0976c529f4fe9159d53885339431
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-11 10:47:00 -0700

    suppress unused warning

--- lib/flux-alloc/src/lib.rs
+++ lib/flux-alloc/src/lib.rs
@@ -7,4 +7,5 @@ pub mod vec;
 // TODO(RJ): I get an "unused extern crate" warning here,
 // but without it, `in_bounds` is not in scope in `lib/vec/mod.rs`.
 #[cfg(flux)]
+#[allow(unused_extern_crates)]
 extern crate flux_core;
