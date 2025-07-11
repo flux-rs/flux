12aaefcce32fd9fb1421bebda053bf9c50e0e982
nilehmann <nico.lehmannm@gmail.com>
2025-07-11 23:04:56 +0000

    Deploying to gh-pages from @ flux-rs/flux@0018b7a356c948010040f0fc278ad443bc4f4684 🚀

--- doc/flux_syntax/lexer/sidebar-items.js
+++ doc/flux_syntax/lexer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Delimiter","LitKind","Token"],"struct":["Cursor","Frame","Lit","Symbols"]};
+window.SIDEBAR_ITEMS = {"enum":["Delimiter","LitKind","TokenKind"],"mod":["token"],"struct":["Cursor","Frame","Lit","Symbols","Token"]};
