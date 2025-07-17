c3d5c92eb4aa7b17f3f9ebef98404979776a8d39
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-17 15:02:34 -0700

    clippy

--- crates/flux-syntax/src/lib.rs
+++ crates/flux-syntax/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(rustc_private, box_patterns, map_try_insert, new_range_api)]
+#![feature(rustc_private, box_patterns, new_range_api)]
 
 extern crate rustc_ast;
 extern crate rustc_errors;
