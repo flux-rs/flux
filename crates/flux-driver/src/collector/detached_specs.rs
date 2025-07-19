faa822e0607e297eacb4092a270c8cc2a494f440
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-18 18:25:30 -0700

    move detached stuff into separate module

---
+++ crates/flux-driver/src/collector/detached_specs.rs
@@ -0,0 +1,116 @@
+use std::collections::HashMap;
+
+use flux_syntax::surface::{self};
+use rustc_errors::ErrorGuaranteed;
+use rustc_hir::{
+    self as hir, Item, ItemKind, OwnerId,
+    def_id::{CRATE_DEF_ID, LocalDefId},
+};
+use rustc_middle::ty::TyCtxt;
+
+use crate::collector::{
+    FluxAttrs, SpecCollector, errors, hir::def_id::LocalModDefId, surface::Ident,
+};
+
+type Result<T = ()> = std::result::Result<T, ErrorGuaranteed>;
+
+pub(super) struct DetachedSpecsCollector<'a, 'sess, 'tcx> {
+    inner: &'a mut SpecCollector<'sess, 'tcx>,
+}
+
+impl<'a, 'sess, 'tcx> DetachedSpecsCollector<'a, 'sess, 'tcx> {
+    pub(super) fn collect(
+        inner: &'a mut SpecCollector<'sess, 'tcx>,
+        attrs: &mut FluxAttrs,
+    ) -> Result {
+        if let Some(detached_specs) = attrs.detached_specs() {
+            Self { inner }.run(detached_specs, CRATE_DEF_ID)?;
+        };
+        Ok(())
+    }
+
+    fn run(&mut self, detached_specs: surface::DetachedSpecs, parent: LocalDefId) -> Result {
+        let mut idents = detached_specs
+            .items
+            .into_iter()
+            .map(|item| (item.ident(), (item, None)))
+            .collect();
+
+        resolve_idents_in_scope(self.inner.tcx, parent, &mut idents);
+
+        for (ident, (item, def_id)) in idents {
+            let Some(def_id) = def_id else {
+                return Err(self.inner.errors.emit(errors::AttrMapErr {
+                    span: ident.span,
+                    message: format!("unresolved identifier `{ident}`"),
+                }));
+            };
+            let owner_id = self.inner.tcx.local_def_id_to_hir_id(def_id).owner;
+            self.collect_detached_item(owner_id, item)?;
+        }
+        Ok(())
+    }
+
+    fn collect_detached_fn_sig(&mut self, owner_id: OwnerId, fn_sig: surface::FnSig) -> Result {
+        let spec = self
+            .inner
+            .specs
+            .fn_sigs
+            .entry(owner_id)
+            .or_insert(surface::FnSpec { fn_sig: None, qual_names: None, reveal_names: None });
+
+        if spec.fn_sig.is_some() {
+            let name = self.inner.tcx.def_path_str(owner_id.to_def_id());
+            return Err(self.inner.errors.emit(errors::AttrMapErr {
+                span: fn_sig.span,
+                message: format!("multiple specs for `{name}`"),
+            }));
+        }
+
+        spec.fn_sig = Some(fn_sig);
+        Ok(())
+    }
+
+    fn collect_detached_item(&mut self, owner_id: OwnerId, item: surface::DetachedItem) -> Result {
+        match item {
+            surface::DetachedItem::FnSig(_, fn_sig) => {
+                self.collect_detached_fn_sig(owner_id, *fn_sig)
+            }
+            surface::DetachedItem::Mod(_, detached_specs) => {
+                self.run(detached_specs, owner_id.def_id)
+            }
+        }
+    }
+}
+
+struct DetachedIdentResolver<'a> {
+    items: &'a mut HashMap<Ident, (surface::DetachedItem, Option<LocalDefId>)>,
+}
+
+impl<'tcx> hir::intravisit::Visitor<'tcx> for DetachedIdentResolver<'_> {
+    fn visit_item(&mut self, item: &'tcx Item<'tcx>) {
+        if let ItemKind::Fn { ident, .. } = item.kind
+            && let Some(val) = self.items.get_mut(&ident)
+            && matches!(val.0, surface::DetachedItem::FnSig(_, _))
+            && val.1.is_none()
+        {
+            val.1 = Some(item.owner_id.def_id);
+        }
+        if let ItemKind::Mod(ident, ..) = item.kind
+            && let Some(val) = self.items.get_mut(&ident)
+            && matches!(val.0, surface::DetachedItem::Mod(_, _))
+            && val.1.is_none()
+        {
+            val.1 = Some(item.owner_id.def_id);
+        }
+    }
+}
+
+fn resolve_idents_in_scope(
+    tcx: TyCtxt,
+    scope: LocalDefId,
+    items: &mut HashMap<Ident, (surface::DetachedItem, Option<LocalDefId>)>,
+) {
+    let scope = LocalModDefId::new_unchecked(scope);
+    tcx.hir_visit_item_likes_in_module(scope, &mut DetachedIdentResolver { items });
+}
