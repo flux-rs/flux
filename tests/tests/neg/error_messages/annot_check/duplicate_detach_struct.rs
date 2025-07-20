bcfa635f85d45ba89c069a0acd630ae556ac3df7
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-19 17:07:49 -0700

    add error tests

---
+++ tests/tests/neg/error_messages/annot_check/duplicate_detach_struct.rs
@@ -0,0 +1,30 @@
+use flux_rs::assert;
+
+#[flux_rs::refined_by(vx: int, vy: int)]
+pub struct MyStruct {
+    #[flux_rs::field(usize[vx])]
+    x: usize,
+    #[flux_rs::field(usize[vx])]
+    y: usize,
+}
+
+pub fn mk_struct(x: usize, y: usize) -> MyStruct {
+    if x < y { MyStruct { x, y } } else { MyStruct { x: y, y: x } }
+}
+
+pub fn use_struct(s: MyStruct) {
+    assert(s.x <= s.y)
+}
+
+#[flux::specs {
+
+    struct MyStruct  //~ ERROR invalid attribute: multiple specs for `MyStruct`
+        refined_by(vx: int, vy: int)
+        invariant(vx <= vy)
+    {
+        x: usize[vx],
+        y: usize[vy],
+    }
+
+}]
+const _: () = ();
