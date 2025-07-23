4f2c441d0ec39c8b185a5e402b4aa3d4fe1594f2
nilehmann <nico.lehmannm@gmail.com>
2025-07-23 21:55:57 +0000

    Deploying to gh-pages from @ flux-rs/flux@72a3c1053c96e875f52b216384e6bcde6ef02195 🚀

--- doc/trait.impl/flux_middle/rty/fold/trait.TypeVisitable.js
+++ doc/trait.impl/flux_middle/rty/fold/trait.TypeVisitable.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl TypeVisitable for <a class=\"enum\" href=\"flux_infer/infer/enum.TypeTrace.html\" title=\"enum flux_infer::infer::TypeTrace\">TypeTrace</a>"]]],["flux_middle",[]],["flux_refineck",[["impl TypeVisitable for <a class=\"struct\" href=\"flux_refineck/type_env/place_ty/struct.PlacesTree.html\" title=\"struct flux_refineck::type_env::place_ty::PlacesTree\">PlacesTree</a>"],["impl TypeVisitable for <a class=\"struct\" href=\"flux_refineck/type_env/struct.BasicBlockEnvData.html\" title=\"struct flux_refineck::type_env::BasicBlockEnvData\">BasicBlockEnvData</a>"]]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.TypeVisitable.html\" title=\"trait flux_middle::rty::fold::TypeVisitable\">TypeVisitable</a> for <a class=\"enum\" href=\"flux_infer/infer/enum.TypeTrace.html\" title=\"enum flux_infer::infer::TypeTrace\">TypeTrace</a>"]]],["flux_middle",[]],["flux_refineck",[["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.TypeVisitable.html\" title=\"trait flux_middle::rty::fold::TypeVisitable\">TypeVisitable</a> for <a class=\"struct\" href=\"flux_refineck/type_env/place_ty/struct.PlacesTree.html\" title=\"struct flux_refineck::type_env::place_ty::PlacesTree\">PlacesTree</a>"],["impl <a class=\"trait\" href=\"flux_middle/rty/fold/trait.TypeVisitable.html\" title=\"trait flux_middle::rty::fold::TypeVisitable\">TypeVisitable</a> for <a class=\"struct\" href=\"flux_refineck/type_env/struct.BasicBlockEnvData.html\" title=\"struct flux_refineck::type_env::BasicBlockEnvData\">BasicBlockEnvData</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[165,19,400]}
+//{"start":57,"fragment_lengths":[297,19,664]}
