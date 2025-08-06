f5586e1bd194e8275540a5fb57931207956d16d6
nilehmann <nico.lehmannm@gmail.com>
2025-08-06 14:19:52 +0000

    Deploying to gh-pages from @ flux-rs/flux@feae280f29a6a3e11ab4673542910d49486547bb 🚀

--- doc/trait.impl/liquid_fixpoint/trait.Identifier.js
+++ doc/trait.impl/liquid_fixpoint/trait.Identifier.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"liquid_fixpoint/trait.Identifier.html\" title=\"trait liquid_fixpoint::Identifier\">Identifier</a> for <a class=\"enum\" href=\"flux_infer/fixpoint_encoding/fixpoint/enum.DataSort.html\" title=\"enum flux_infer::fixpoint_encoding::fixpoint::DataSort\">DataSort</a>"],["impl <a class=\"trait\" href=\"liquid_fixpoint/trait.Identifier.html\" title=\"trait liquid_fixpoint::Identifier\">Identifier</a> for <a class=\"enum\" href=\"flux_infer/fixpoint_encoding/fixpoint/enum.Var.html\" title=\"enum flux_infer::fixpoint_encoding::fixpoint::Var\">Var</a>"],["impl <a class=\"trait\" href=\"liquid_fixpoint/trait.Identifier.html\" title=\"trait liquid_fixpoint::Identifier\">Identifier</a> for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.KVid.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::KVid\">KVid</a>"]]],["liquid_fixpoint",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl Identifier for <a class=\"enum\" href=\"flux_infer/fixpoint_encoding/fixpoint/enum.DataSort.html\" title=\"enum flux_infer::fixpoint_encoding::fixpoint::DataSort\">DataSort</a>"],["impl Identifier for <a class=\"enum\" href=\"flux_infer/fixpoint_encoding/fixpoint/enum.Var.html\" title=\"enum flux_infer::fixpoint_encoding::fixpoint::Var\">Var</a>"],["impl Identifier for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.KVid.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::KVid\">KVid</a>"]]],["liquid_fixpoint",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[895,23]}
+//{"start":57,"fragment_lengths":[553,23]}
