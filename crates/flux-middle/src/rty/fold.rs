e38b521510ad225d309d680c789c41e200135cef
Cole K <cole-k@users.noreply.github.com>
2025-08-07 15:41:13 -0700

    fmt, clippy

--- crates/flux-middle/src/rty/fold.rs
+++ crates/flux-middle/src/rty/fold.rs
@@ -11,9 +11,9 @@ use rustc_hash::FxHashSet;
 use rustc_type_ir::{BoundVar, DebruijnIndex, INNERMOST};
 
 use super::{
-    BaseTy, Binder, BoundVariableKinds, Const, EVid, EarlyReftParam, Ensures,
-    Expr, ExprKind, GenericArg, Name, OutlivesPredicate, PolyFuncSort, PtrKind, ReBound, ReErased,
-    Region, Sort, SubsetTy, Ty, TyKind, TyOrBase, normalize::Normalizer,
+    BaseTy, Binder, BoundVariableKinds, Const, EVid, EarlyReftParam, Ensures, Expr, ExprKind,
+    GenericArg, Name, OutlivesPredicate, PolyFuncSort, PtrKind, ReBound, ReErased, Region, Sort,
+    SubsetTy, Ty, TyKind, TyOrBase, normalize::Normalizer,
 };
 use crate::{
     global_env::GlobalEnv,
@@ -274,14 +274,15 @@ pub trait TypeVisitable: Sized {
             // Here we count all times we see a bvar
             fn visit_expr(&mut self, e: &Expr) -> ControlFlow<Self::BreakTy> {
                 if let ExprKind::Var(Var::Bound(debruijn, BoundReft { var, .. })) = e.kind()
-                    && debruijn == &self.debruijn {
-                        self.bvar_occurrences
-                            .entry(*var)
-                            .and_modify(|count| {
-                                *count += 1;
-                            })
-                            .or_insert(1);
-                    }
+                    && debruijn == &self.debruijn
+                {
+                    self.bvar_occurrences
+                        .entry(*var)
+                        .and_modify(|count| {
+                            *count += 1;
+                        })
+                        .or_insert(1);
+                }
                 e.super_visit_with(self)
             }
             fn visit_ty(&mut self, ty: &Ty) -> ControlFlow<Self::BreakTy> {
@@ -303,14 +304,15 @@ pub trait TypeVisitable: Sized {
                             exprs.iter().for_each(|expr| {
                                 if let ExprKind::Var(Var::Bound(debruijn, BoundReft { var, .. })) =
                                     expr.kind()
-                                    && debruijn == &self.debruijn {
-                                        self.bvar_index_occurrences
-                                            .entry(*var)
-                                            .and_modify(|count| {
-                                                *count += 1;
-                                            })
-                                            .or_insert(1);
-                                    }
+                                    && debruijn == &self.debruijn
+                                {
+                                    self.bvar_index_occurrences
+                                        .entry(*var)
+                                        .and_modify(|count| {
+                                            *count += 1;
+                                        })
+                                        .or_insert(1);
+                                }
                             });
                         }
                         _ => {}
