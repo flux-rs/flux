973b350d7969a253bebb58af3ba7f874e58107e4
Ranjit Jhala <rjhala@ucsd.edu>
2025-09-22 16:56:54 -0700

    Update crates/flux-metadata/src/decoder.rs
    Co-authored-by: Nico Lehmann <nico.lehmannm@gmail.com>

--- crates/flux-metadata/src/decoder.rs
+++ crates/flux-metadata/src/decoder.rs
@@ -122,7 +122,6 @@ impl SpanDecoder for DecodeContext<'_, '_> {
     }
 
     fn decode_syntax_context(&mut self) -> SyntaxContext {
-        // OLD: bug!("cannot decode `SyntaxContext` with `DecodeContext`");
         let syntax_contexts = self.syntax_contexts;
         rustc_span::hygiene::decode_syntax_context(self, &self.hygiene_context, |this, id| {
             // This closure is invoked if we haven't already decoded the data for the `SyntaxContext` we are deserializing.
