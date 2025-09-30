b33be81fb590047e94173ce2fc502a44b60f89ce
Nico Lehmann <nico.lehmannm@gmail.com>
2025-09-30 18:57:20 -0300

    Add tests

---
+++ tests/tests/pos/surface/assoc_reft07.rs
@@ -0,0 +1,25 @@
+// Test we correctly resolve type-relative associated refinements
+
+use flux_rs::attrs::*;
+
+#[assoc(fn pre(x: int) -> bool)]
+trait MyTrait {
+    #[spec(fn(x: i32{ Self::pre(x) }))]
+    fn f(x: i32);
+}
+
+#[assoc(fn pre(x: int) -> bool { x >= 0 })]
+impl MyTrait for i32 {
+    #[spec(fn(x: i32{ Self::pre(x) }))]
+    fn f(x: i32) {}
+}
+
+#[spec(fn(x: i32{ T::pre(x) }))]
+fn test00<T: MyTrait>(x: i32) {
+    T::f(x);
+}
+
+fn test01() {
+    test00::<i32>(0);
+    <i32>::f(0);
+}
