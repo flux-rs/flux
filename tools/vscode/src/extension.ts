c7a276ac70d2432b14ecfeca8ce56eb673ece15d
Ranjit Jhala <rjhala@ucsd.edu>
2025-10-02 16:12:12 -0700

    fix ordering in dbg::statement

--- tools/vscode/src/extension.ts
+++ tools/vscode/src/extension.ts
@@ -315,6 +315,7 @@ async function runCargoFlux(workspacePath: string, file: string, trace: boolean,
   statusBarItem.show();
 
   let fluxFlags = `-Finclude=${includeValue}`;
+  fluxFlags += ` -Fcache=1`;
   if (trace) {
     fluxFlags += ` -Fdump-checker-trace=info`;
   } else {
