b9bb1b9082ae3f63a18b270e61cdeea136d7e4df
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2025-10-03 10:51:35 -0700

    Support more expressions and bitvec constants and theory functions

--- crates/flux-infer/src/lean_format.rs
+++ crates/flux-infer/src/lean_format.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 
 use itertools::Itertools;
-use liquid_fixpoint::{FixpointFmt, Identifier};
+use liquid_fixpoint::{FixpointFmt, Identifier, ThyFunc};
 
 use crate::{
     fixpoint_encoding::fixpoint::{
@@ -17,6 +17,44 @@ struct LeanConstraint<'a>(&'a Constraint);
 struct LeanPred<'a>(&'a Pred);
 struct LeanExpr<'a>(&'a Expr);
 struct LeanVar<'a>(&'a Var);
+struct LeanThyFunc<'a>(&'a ThyFunc);
+
+impl<'a> fmt::Display for LeanThyFunc<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            ThyFunc::IntToBv8 => write!(f, "BitVec.ofInt 8"),
+            ThyFunc::IntToBv32 => write!(f, "BitVec.ofInt 32"),
+            ThyFunc::IntToBv64 => write!(f, "BitVec.ofInt 64"),
+            ThyFunc::Bv8ToInt | ThyFunc::Bv32ToInt | ThyFunc::Bv64ToInt => {
+                write!(f, "BitVec.toNat")
+            }
+            ThyFunc::BvAdd => write!(f, "BitVec.add"),
+            ThyFunc::BvSub => write!(f, "BitVec.sub"),
+            ThyFunc::BvMul => write!(f, "BitVec.mul"),
+            ThyFunc::BvNeg => write!(f, "BitVec.neg"),
+            ThyFunc::BvSdiv => write!(f, "BitVec.sdiv"),
+            ThyFunc::BvSrem => write!(f, "BitVec.srem"),
+            ThyFunc::BvUdiv => write!(f, "BitVec.udiv"),
+            ThyFunc::BvAnd => write!(f, "BitVec.and"),
+            ThyFunc::BvOr => write!(f, "BitVec.or"),
+            ThyFunc::BvXor => write!(f, "BitVec.xor"),
+            ThyFunc::BvNot => write!(f, "BitVec.not"),
+            ThyFunc::BvSle => write!(f, "BitVec.sle"),
+            ThyFunc::BvSlt => write!(f, "BitVec.slt"),
+            ThyFunc::BvUle => write!(f, "BitVec.ule"),
+            ThyFunc::BvUlt => write!(f, "BitVec.ult"),
+            ThyFunc::BvAshr => write!(f, "BitVec.sshiftRight"),
+            ThyFunc::BvLshr => write!(f, "BitVec.ushiftRight"),
+            ThyFunc::BvShl => write!(f, "BitVec.shiftLeft"),
+            ThyFunc::BvSignExtend(size) => write!(f, "BitVec.signExtend {}", size),
+            ThyFunc::BvZeroExtend(size) => write!(f, "BitVec.zeroExtend {}", size),
+            ThyFunc::BvUrem | ThyFunc::BvSge | ThyFunc::BvSgt | ThyFunc::BvUge | ThyFunc::BvUgt => {
+                todo!("No builtin {}, define a local function {} and call it here", self.0, self.0)
+            }
+            func => panic!("Unsupported theory function {}", func),
+        }
+    }
+}
 
 impl<'a> fmt::Display for LeanVar<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -60,7 +98,7 @@ impl<'a> fmt::Display for LeanExpr<'a> {
                     Constant::Boolean(b) => write!(f, "{}", b),
                     Constant::String(s) => write!(f, "{}", s.display()),
                     Constant::Decimal(d) => write!(f, "{}", d.display()),
-                    _ => todo!(),
+                    Constant::BitVec(bv, size) => write!(f, "{}#{}", bv, size),
                 }
             }
             Expr::BinaryOp(bin_op, args) => {
@@ -98,7 +136,37 @@ impl<'a> fmt::Display for LeanExpr<'a> {
             Expr::Or(exprs) => {
                 write!(f, "({})", exprs.iter().map(LeanExpr).format(" || "))
             }
-            _ => todo!(),
+            Expr::Neg(inner) => {
+                write!(f, "(-{})", LeanExpr(inner.as_ref()))
+            }
+            Expr::IfThenElse(ite) => {
+                let [condition, if_true, if_false] = ite.as_ref();
+                write!(
+                    f,
+                    "(ite {} {} {})",
+                    LeanExpr(condition),
+                    LeanExpr(if_true),
+                    LeanExpr(if_false)
+                )
+            }
+            Expr::Not(inner) => {
+                write!(f, "(¬{})", LeanExpr(inner.as_ref()))
+            }
+            Expr::Imp(implication) => {
+                let [lhs, rhs] = implication.as_ref();
+                write!(f, "({} -> {})", LeanExpr(lhs), LeanExpr(rhs))
+            }
+            Expr::Iff(equiv) => {
+                let [lhs, rhs] = equiv.as_ref();
+                write!(f, "({} <-> {})", LeanExpr(lhs), LeanExpr(rhs))
+            }
+            Expr::Let(binder, exprs) => {
+                let [def, body] = exprs.as_ref();
+                write!(f, "(let {} := {}; {})", LeanVar(binder), LeanExpr(def), LeanExpr(body))
+            }
+            Expr::ThyFunc(thy_func) => {
+                write!(f, "{}", LeanThyFunc(thy_func))
+            }
         }
     }
 }
