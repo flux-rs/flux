433d5e625853bfe210baf98ca491a2f383721854
Ranjit Jhala <rjhala@ucsd.edu>
2025-10-03 16:45:22 -0700

    move vec specs into alloc

--- tests/tests/pos/surface/clone_spec.rs
+++ tests/tests/pos/surface/clone_spec.rs
@@ -2,12 +2,6 @@ extern crate flux_alloc;
 use flux_alloc::string::mk_string;
 use flux_rs::assert;
 
-// #[trusted]
-// #[spec(fn(&str[@s]) -> String[s])]
-// fn mk_string(s: &str) -> String {
-//     s.to_string()
-// }
-
 pub fn test_clone() {
     let bob1 = mk_string("bob");
     let bob2 = bob1.clone();
