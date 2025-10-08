aa279b4d100d28eeb6c34c2602970bc57eb73233
nilehmann <nico.lehmannm@gmail.com>
2025-10-08 14:58:41 +0000

    Deploying to gh-pages from @ flux-rs/flux@8a9ea5a0fb037a149bf11754606d3d51f617915a 🚀

--- doc/trait.impl/rustc_hir/intravisit/trait.Visitor.js
+++ doc/trait.impl/rustc_hir/intravisit/trait.Visitor.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_desugar",[["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_desugar/resolver/struct.CrateResolver.html\" title=\"struct flux_desugar::resolver::CrateResolver\">CrateResolver</a>&lt;'_, 'tcx&gt;"],["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_desugar/resolver/struct.OpaqueTypeCollector.html\" title=\"struct flux_desugar::resolver::OpaqueTypeCollector\">OpaqueTypeCollector</a>&lt;'_&gt;"]]],["flux_driver",[["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_driver/collector/struct.SpecCollector.html\" title=\"struct flux_driver::collector::SpecCollector\">SpecCollector</a>&lt;'_, 'tcx&gt;"]]]]);
+    var implementors = Object.fromEntries([["flux_desugar",[["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_desugar/desugar/struct.OpaqueTypeCollector.html\" title=\"struct flux_desugar::desugar::OpaqueTypeCollector\">OpaqueTypeCollector</a>&lt;'_&gt;"],["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_desugar/resolver/struct.CrateResolver.html\" title=\"struct flux_desugar::resolver::CrateResolver\">CrateResolver</a>&lt;'_, 'tcx&gt;"]]],["flux_driver",[["impl&lt;'tcx&gt; Visitor&lt;'tcx&gt; for <a class=\"struct\" href=\"flux_driver/collector/struct.SpecCollector.html\" title=\"struct flux_driver::collector::SpecCollector\">SpecCollector</a>&lt;'_, 'tcx&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[452,229]}
+//{"start":57,"fragment_lengths":[450,229]}
