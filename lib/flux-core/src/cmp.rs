9b9557086c8f92338a3ca52cb44d32a7dc16f304
Ranjit Jhala <rjhala@ucsd.edu>
2025-10-09 12:20:50 -0700

    move eq-macro from under the cfg-flux gate (#1319)

--- lib/flux-core/src/cmp.rs
+++ lib/flux-core/src/cmp.rs
@@ -14,21 +14,3 @@ trait PartialEq<Rhs: PointeeSized = Self>: PointeeSized {
     #[spec(fn(&Self[@s], &Rhs[@t]) -> bool{v: Self::is_ne(s, t, v)})]
     fn ne(&self, other: &Rhs) -> bool;
 }
-
-#[macro_export]
-macro_rules! eq {
-    ($type_name:path) => {
-        #[cfg_attr(flux, flux::specs {
-                    impl std::cmp::PartialEq for $type_name {
-                        #[reft] fn is_eq(self: $type_name, other: $type_name, res: bool) -> bool {
-                            res <=> (self == other)
-                        }
-                        #[reft] fn is_ne(self: $type_name, other: $type_name, res: bool) -> bool {
-                            res <=> (self != other)
-                        }
-                        fn eq(&$type_name[@v1], other: &$type_name[@v2]) -> bool[v1 == v2];
-                    }
-                })]
-        const _: () = ();
-    };
-}
