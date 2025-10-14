f9771e5a998b1888fba1c831e45a8c92ae82de81
Nico Lehmann <nico.lehmannm@gmail.com>
2025-10-14 11:06:50 -0300

    Add tests

---
+++ tests/tests/pos/surface/assoc_reft08.rs
@@ -0,0 +1,22 @@
+// Test we normalize the sorts of associated refinements during sort checking
+
+use flux_rs::attrs::*;
+
+#[assoc(fn foo(x: Self::Assoc) -> bool)]
+trait MyTrait {
+    type Assoc;
+}
+
+struct S;
+
+#[assoc(fn foo(x: int) -> bool { x >= 0 })]
+impl MyTrait for S {
+    type Assoc = i32;
+}
+
+#[spec(fn(x: <S as MyTrait>::Assoc{v: <S as MyTrait>::foo(v) }))]
+fn test00(x: <S as MyTrait>::Assoc) {}
+
+fn test01() {
+    test00(0);
+}
