5e529026e744dcd48874e889f15bc73c451e27b8
nilehmann <nico.lehmannm@gmail.com>
2025-10-14 19:08:34 +0000

    Deploying to gh-pages from @ flux-rs/flux@6beed3d285470b800264ddda6976599f2f0a8355 🚀

--- doc/trait.impl/liquid_fixpoint/trait.FixpointFmt.js
+++ doc/trait.impl/liquid_fixpoint/trait.FixpointFmt.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl FixpointFmt for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.SymStr.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::SymStr\">SymStr</a>"]]],["flux_middle",[["impl FixpointFmt for <a class=\"struct\" href=\"flux_middle/rty/struct.Real.html\" title=\"struct flux_middle::rty::Real\">Real</a>"]]],["liquid_fixpoint",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl FixpointFmt for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.SymStr.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::SymStr\">SymStr</a>"]]],["liquid_fixpoint",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[203,154,23]}
+//{"start":57,"fragment_lengths":[203,23]}
