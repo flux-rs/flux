178a0c3379f45084fa47e04b75f31c70fa07b8d7
Nico Lehmann <nico.lehmannm@gmail.com>
2025-10-15 12:31:09 -0300

    Separate construction of dependency graph from fragments

--- lib/liquid-fixpoint/src/constraint_solving.rs
+++ lib/liquid-fixpoint/src/constraint_solving.rs
@@ -39,30 +39,62 @@ impl<T: Types> Constraint<T> {
         }
     }
 
-    #[allow(clippy::type_complexity)]
-    pub fn kvar_mappings(
-        &self,
-    ) -> (HashMap<T::KVar, Vec<Constraint<T>>>, HashMap<T::KVar, Vec<T::KVar>>) {
+    pub(crate) fn kvar_mappings(&self) -> HashMap<T::KVar, Vec<Constraint<T>>> {
         let mut kvar_to_fragments: HashMap<T::KVar, Vec<Constraint<T>>> = HashMap::new();
-        let mut kvar_to_dependencies: HashMap<T::KVar, Vec<T::KVar>> = HashMap::new();
         for frag in self.depth_first_fragments() {
             if let Some(kvar) = frag.fragment_kvar_head() {
-                kvar_to_dependencies
-                    .entry(kvar.clone())
-                    .or_insert_with(Vec::new)
-                    .extend_from_slice(&frag.kvar_deps().into_iter().unique().collect_vec());
                 kvar_to_fragments
                     .entry(kvar.clone())
                     .or_insert_with(Vec::new)
                     .push(frag);
             }
         }
-        (kvar_to_fragments, kvar_to_dependencies)
+        kvar_to_fragments
     }
 
-    pub fn topo_order_fragments(&self) -> Vec<Self> {
-        let (mut kvar_to_fragments, kvar_to_dependencies) = self.kvar_mappings();
-        let topologically_ordered_kvids = topological_sort_sccs(&kvar_to_dependencies);
+    /// Computes the kvar dependency graph as an adjacency list.
+    ///
+    /// There's an edge $k0 -> $k1, if $k1 appears as an assumption when $k0 is a head.
+    pub(crate) fn kvar_dep_graph(&self) -> HashMap<T::KVar, Vec<T::KVar>> {
+        fn go<T: Types>(
+            cstr: &Constraint<T>,
+            deps: &mut Vec<T::KVar>,
+            graph: &mut HashMap<T::KVar, Vec<T::KVar>>,
+        ) {
+            match cstr {
+                Constraint::Pred(pred, _) => {
+                    for kvar in pred.kvars() {
+                        graph
+                            .entry(kvar.clone())
+                            .or_default()
+                            .extend(deps.iter().cloned());
+                    }
+                }
+                Constraint::Conj(cstrs) => {
+                    for cstr in cstrs {
+                        let n = deps.len();
+                        go(cstr, deps, graph);
+                        deps.truncate(n);
+                    }
+                }
+                Constraint::ForAll(bind, cstr) => {
+                    deps.extend(bind.pred.kvars());
+                    go(cstr, deps, graph);
+                }
+            }
+        }
+        let mut graph = Default::default();
+        go(self, &mut vec![], &mut graph);
+        graph
+            .into_iter()
+            .map(|(kvid, deps)| (kvid, deps.into_iter().dedup().collect()))
+            .collect()
+    }
+
+    pub(crate) fn topo_order_fragments(&self) -> Vec<Self> {
+        let dep_graph = self.kvar_dep_graph();
+        let mut kvar_to_fragments = self.kvar_mappings();
+        let topologically_ordered_kvids = topological_sort_sccs(&dep_graph);
         topologically_ordered_kvids
             .into_iter()
             .rev()
