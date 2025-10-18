7af4c40b69752b3e9191291e3ea257c119a6369d
Cole K <cole-k@users.noreply.github.com>
2025-10-18 15:52:21 -0700

    burning through ci is my passion

--- crates/flux-infer/src/fixpoint_encoding.rs
+++ crates/flux-infer/src/fixpoint_encoding.rs
@@ -1567,7 +1567,9 @@ impl<'genv, 'tcx> ExprEncodingCtxt<'genv, 'tcx> {
             Ok(var)
         } else {
             let fresh = self.local_var_env.fresh_name();
-            self.local_var_env.reverse_map.insert(fresh.clone(), arg.clone());
+            self.local_var_env
+                .reverse_map
+                .insert(fresh, arg.clone());
             let pred = fixpoint::Expr::eq(fixpoint::Expr::Var(fresh.into()), farg);
             bindings.push(fixpoint::Bind {
                 name: fresh.into(),
