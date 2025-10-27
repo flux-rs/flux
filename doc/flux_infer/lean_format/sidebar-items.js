8d9b09503c51f62f95abecd9f90841cd77e3a259
petros-marko <31206917+petros-marko@users.noreply.github.com>
2025-10-27 17:29:55 +0000

    Deploying to gh-pages from @ flux-rs/flux@813ea8fa5b59bdbf6e95cf1376f0035f56b23455 🚀

--- doc/flux_infer/lean_format/sidebar-items.js
+++ doc/flux_infer/lean_format/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["LeanConstDecl","LeanConstraint","LeanDataDecl","LeanDataField","LeanExpr","LeanFunDef","LeanPred","LeanSort","LeanSortDecl","LeanSortVar","LeanThyFunc","LeanVar"]};
+window.SIDEBAR_ITEMS = {"struct":["LeanConstDecl","LeanConstraint","LeanDataDecl","LeanDataField","LeanExpr","LeanFunDef","LeanKConstraint","LeanKVarDecl","LeanPred","LeanSort","LeanSortDecl","LeanSortVar","LeanThyFunc","LeanVar"]};
