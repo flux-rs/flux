642214ba066757b781a4811c6c8f868c3575f0d9
nilehmann <nico.lehmannm@gmail.com>
2025-11-17 18:48:16 +0000

    Deploying to gh-pages from @ flux-rs/flux@c3a901ddda092636cb5cda2b6c3f6e5ff704af85 🚀

--- doc/trait.impl/flux_middle/rty/canonicalize/trait.HoisterDelegate.js
+++ doc/trait.impl/flux_middle/rty/canonicalize/trait.HoisterDelegate.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl HoisterDelegate for <a class=\"struct\" href=\"flux_infer/infer/struct.Unpacker.html\" title=\"struct flux_infer::infer::Unpacker\">Unpacker</a>&lt;'_, '_, '_, '_&gt;"]]],["flux_middle",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"flux_middle/rty/canonicalize/trait.HoisterDelegate.html\" title=\"trait flux_middle::rty::canonicalize::HoisterDelegate\">HoisterDelegate</a> for <a class=\"struct\" href=\"flux_infer/infer/struct.Unpacker.html\" title=\"struct flux_infer::infer::Unpacker\">Unpacker</a>&lt;'_, '_, '_, '_&gt;"]]],["flux_middle",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[192,19]}
+//{"start":57,"fragment_lengths":[344,19]}
