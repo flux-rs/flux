4dfd89233d0af11f1da41734f80b0db33b5db9fd
Ranjit Jhala <rjhala@ucsd.edu>
2025-12-02 07:31:35 -0800

    done step6

---
+++ tools/vscode/src/commands/index.ts
@@ -0,0 +1,5 @@
+// Re-export all command registration functions from a single entry point
+
+export * from "./killProcessCommand";
+export * from "./showDiagnosticCommand";
+export * from "./toggleCommand";
