5ff30703761cd2a2976c4434c2616e5c5b059cd3
Andrew Cheung <a7cheung@ucsd.edu>
2025-12-09 17:47:47 -0800

    Update test to reflect intended behavior of no_panic closures

--- tests/tests/pos/surface/no_panic05.rs
+++ tests/tests/pos/surface/no_panic05.rs
@@ -5,17 +5,20 @@ fn okay_if_i_panic() {
     bad_thing(1);
 }
 
+// This is okay; `foo` has not been called with some `f` that panics.
 #[flux::no_panic]
-fn i_cant_panic() {
-    let good_thing = |x: i32| { 
-        {
-            x
-        }
-    };
-    good_thing(1);
+fn foo(f: fn(i32) -> i32) {
+    f(2);
 }
 
+// This is bad, we try to call `f` with some function that _does_ panic.
 #[flux::no_panic]
+fn bar(f: fn(i32) -> i32) {
+    f(3);
+}
+
+// Observe `main` is not annotated with `no_panic`, yet...
 fn main() {
-    let x = 12;
+    // ...this should not be okay because `bar` requires its argument to be `no_panic`.
+    bar(|x| x); //~ ERROR may panic (doesn't error right now, but we want it to)
 }
