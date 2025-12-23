e87925d4978af55599019cd2a98d0036326d45dc
petros-marko <31206917+petros-marko@users.noreply.github.com>
2025-12-23 18:02:37 +0000

    Deploying to gh-pages from @ flux-rs/flux@763ad5475c8cb13aeafd4d837f7358be1845b049 🚀

--- doc/trait.impl/liquid_fixpoint/parser/trait.FromSexp.js
+++ doc/trait.impl/liquid_fixpoint/parser/trait.FromSexp.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl FromSexp&lt;<a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/fixpoint_generated/struct.FixpointTypes.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::fixpoint_generated::FixpointTypes\">FixpointTypes</a>&gt; for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/struct.SexpParseCtxt.html\" title=\"struct flux_infer::fixpoint_encoding::SexpParseCtxt\">SexpParseCtxt</a>"]]],["liquid_fixpoint",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl FromSexp&lt;<a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/fixpoint_generated/struct.FixpointTypes.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::fixpoint_generated::FixpointTypes\">FixpointTypes</a>&gt; for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/struct.SexpParseCtxt.html\" title=\"struct flux_infer::fixpoint_encoding::SexpParseCtxt\">SexpParseCtxt</a>&lt;'_&gt;"]]],["liquid_fixpoint",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[431,23]}
+//{"start":57,"fragment_lengths":[441,23]}
