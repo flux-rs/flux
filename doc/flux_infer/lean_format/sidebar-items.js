e936c2c408973f504b604a107607db6014bc6823
petros-marko <31206917+petros-marko@users.noreply.github.com>
2026-01-05 18:35:08 +0000

    Deploying to gh-pages from @ flux-rs/flux@7eeb04a1a1310c15ddd5dcc5c63e83aa07d95783 🚀

--- doc/flux_infer/lean_format/sidebar-items.js
+++ doc/flux_infer/lean_format/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["def_id_to_pascal_case","snake_case_to_pascal_case"],"struct":["ConstraintFormatter","LeanAdt","LeanCtxt","LeanDataProj","LeanField","LeanKConstraint","LeanThyFunc","WithLeanCtxt"],"trait":["FormatNested","LeanFmt"]};
+window.SIDEBAR_ITEMS = {"enum":["BoolMode"],"fn":["def_id_to_pascal_case","snake_case_to_pascal_case"],"struct":["ConstraintFormatter","LeanAdt","LeanCtxt","LeanDataProj","LeanField","LeanKConstraint","LeanThyFunc","WithLeanCtxt"],"trait":["FormatNested","LeanFmt"]};
