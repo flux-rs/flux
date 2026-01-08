783ef0fa81bf086b0f6e09382278ba3215688cc3
Andrew Cheung <a7cheung@ucsd.edu>
2026-01-07 23:36:55 -0800

    Remove unnecessary test comment

--- tests/tests/pos/surface/no_panic05.rs
+++ tests/tests/pos/surface/no_panic05.rs
@@ -12,11 +12,5 @@ fn wont_panic() -> i32 {
 
 #[flux::no_panic]
 fn foo() {
-    // This is fine because `blah` never gets called.
-    // let blah = |x| panic!("blah");
-
-    // the line below: `foo` is in trouble because we've promised that `foo` can't panic.
-    // because of this promise, the argument to `bar` must be a function that cannot panic.
-    // there may be some sort of analysis that can prove this, but we'll need to merge #1387 to get that infrastructure.
     bar(|x| if x == 3 { wont_panic() } else { x + 1 });
 }
