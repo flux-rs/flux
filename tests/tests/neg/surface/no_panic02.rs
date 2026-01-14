8783963cbc7b7e9a7503fb6f7c3f4d12475b1c4f
Andrew Cheung <a7cheung@ucsd.edu>
2026-01-14 14:36:05 -0800

    fix a darn thing

--- tests/tests/neg/surface/no_panic02.rs
+++ tests/tests/neg/surface/no_panic02.rs
@@ -6,11 +6,8 @@ fn bar<F: Fn(i32) -> i32>(f: F) -> i32 {
 }
 
 fn foo() {
-    // This is fine because `blah` never gets called.
-    // let blah = |x| panic!("blah");
-
     // the line below: `foo` is in trouble because we've promised that `foo` can't panic.
     // because of this promise, the argument to `bar` must be a function that cannot panic.
     // there may be some sort of analysis that can prove this, but we'll need to merge #1387 to get that infrastructure.
-    bar(|x| 3); //~ ERROR precondition
+    bar(|x| 3); //~ ERROR refinement
 }
