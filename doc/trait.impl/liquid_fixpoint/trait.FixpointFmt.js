032571b08dacdd37fcc08cb59f4549305967b0fd
nilehmann <nico.lehmannm@gmail.com>
2026-01-19 15:12:37 +0000

    Deploying to gh-pages from @ flux-rs/flux@1779b31e630d98f4bf3b6f956470405fdcc50a94 🚀

--- doc/trait.impl/liquid_fixpoint/trait.FixpointFmt.js
+++ doc/trait.impl/liquid_fixpoint/trait.FixpointFmt.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"liquid_fixpoint/trait.FixpointFmt.html\" title=\"trait liquid_fixpoint::FixpointFmt\">FixpointFmt</a> for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.SymStr.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::SymStr\">SymStr</a>"]]],["liquid_fixpoint",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl FixpointFmt for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/struct.SymStr.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::SymStr\">SymStr</a>"]]],["liquid_fixpoint",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[319,23]}
+//{"start":57,"fragment_lengths":[203,23]}
--- doc/trait.impl/liquid_fixpoint/trait.Types.js
+++ doc/trait.impl/liquid_fixpoint/trait.Types.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_infer",[["impl <a class=\"trait\" href=\"liquid_fixpoint/trait.Types.html\" title=\"trait liquid_fixpoint::Types\">Types</a> for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/fixpoint_generated/struct.FixpointTypes.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::fixpoint_generated::FixpointTypes\">FixpointTypes</a>"]]],["liquid_fixpoint",[]]]);
+    var implementors = Object.fromEntries([["flux_infer",[["impl Types for <a class=\"struct\" href=\"flux_infer/fixpoint_encoding/fixpoint/fixpoint_generated/struct.FixpointTypes.html\" title=\"struct flux_infer::fixpoint_encoding::fixpoint::fixpoint_generated::FixpointTypes\">FixpointTypes</a>"]]],["liquid_fixpoint",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[361,23]}
+//{"start":57,"fragment_lengths":[257,23]}
