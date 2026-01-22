881629016cb30ed2655957c2145373079785f8e0
Andrew Cheung <a7cheung@ucsd.edu>
2026-01-21 21:42:26 -0800

    Make test marginally more complex

--- tests/tests/pos/surface/no_panic05.rs
+++ tests/tests/pos/surface/no_panic05.rs
@@ -1,6 +1,6 @@
 // This is bad, we try to call `f` with some function that _does_ panic.
 #[flux::no_panic]
-#[flux::sig(fn(f: F) -> i32 requires F::no_panic())]
+// #[flux::sig(fn(f: F) -> i32 requires F::no_panic())]
 fn bar<F: Fn(i32) -> i32>(f: F) -> i32 {
     f(3) // check here: is the type of `f` such that it cannot panic? Yes! So `bar` is not in trouble.
 }
@@ -8,5 +8,10 @@ fn bar<F: Fn(i32) -> i32>(f: F) -> i32 {
 #[flux::no_panic]
 fn foo() {
     // This is OK because `foo` is marked `no_panic`, therefore all closures within `foo` are also no_panic.
-    bar(|x| 3);
+    bar(|x| blah(3));
+}
+
+#[flux::no_panic]
+fn blah(x: i32) -> i32 {
+    3
 }
