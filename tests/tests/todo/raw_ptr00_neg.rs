e7f5c95f30b4522c6eb956fd196e6fe84422fa7b
Ranjit Jhala <rjhala@ucsd.edu>
2026-02-04 10:04:56 -0800

    next: add flag before issuing raw-ptr checks

--- tests/tests/todo/raw_ptr00_neg.rs
+++ tests/tests/todo/raw_ptr00_neg.rs
@@ -1,16 +1,9 @@
-// #[flux::spec(fn (ptr: *const {v: v > 0} i32) -> i32)]
 #[flux::opts(check_raw_pointer = "checked")]
-fn read(x: *const i32) -> i32 {
-    unsafe { *x } //~ ERROR refinement type
+// #[flux::spec(fn (ptr: *const {v:v > 0} i32) -> i32)]
+pub fn read(x: *const i32) -> i32 {
+    unsafe { *x } //~ ERROR raw pointer dereference may be unsafe
 }
 
-fn read_i32(x: i32) -> i32 {
-    x
-}
-
-fn read_i32_ref(x: &i32) -> i32 {
-    *x
-}
 // // #[flux::spec(fn (ptr: *mut{v: v > 0} T, value: T))]
 // #[flux::opts(check_raw_pointer = "checked")]
 // fn write(ptr: *mut i32, value: i32) {
