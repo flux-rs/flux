04f26f5ce9ea312a7fa738e4e876249415824402
Nico Lehmann <nico.lehmannm@gmail.com>
2026-02-09 12:51:20 -0300

    Recursively normalize projection types

---
+++ tests/tests/pos/surface/issue-1449.rs
@@ -0,0 +1,35 @@
+#![allow(unused)]
+
+trait FromState {
+    type Role;
+}
+
+trait Choice {
+    type Session: FromState;
+}
+
+struct End<R>(R);
+
+struct B;
+
+struct MyChoice {}
+
+impl<R> FromState for End<R> {
+    type Role = R;
+}
+
+impl Choice for MyChoice {
+    type Session = End<B>;
+}
+
+fn select<Q, C>()
+where
+    C: Choice,
+    C::Session: FromState<Role = Q>,
+    // <<C as Choice>::Session as FromState>::Role == Q
+{
+}
+
+fn test_select() {
+    select::<B, MyChoice>();
+}
