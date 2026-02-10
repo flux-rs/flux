f0256f487fc5ff93349392dd0cecff7e08d51c7c
Nico Lehmann <nico.lehmannm@gmail.com>
2026-02-10 16:15:56 -0300

    Remove encoding HACK for core

--- crates/flux-driver/src/callbacks.rs
+++ crates/flux-driver/src/callbacks.rs
@@ -132,16 +132,6 @@ fn collect_specs(genv: GlobalEnv) -> Specs {
 }
 
 fn encode_and_save_metadata(genv: GlobalEnv) {
-    // HACK(nilehmann) do not encode metadata for `core`, this is so verify-rust-std works even
-    // if it has unsupported items. We report errors lazily so partially analyzing the crate should
-    // skip the error, except that encoding the metadata for the crate will trigger conversion for
-    // all items which can trigger the error even if not included for analysis. To fix this properly
-    // we should consider how to handle metadata encoding if only part of the crate is included for
-    // analysis.
-    if genv.tcx().crate_name(LOCAL_CRATE) == flux_syntax::symbols::sym::core {
-        return;
-    }
-
     // We only save metadata when `--emit=metadata` is passed as an argument. In this case, we save
     // the `.fluxmeta` file alongside the `.rmeta` file. This setup works for `cargo flux`, which
     // wraps `cargo check` and always passes `--emit=metadata`. Tests also explicitly pass this flag.
