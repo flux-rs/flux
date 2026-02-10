9dbb92900c0b62422b7bfae152f47f93a575a8c9
Ranjit Jhala <rjhala@ucsd.edu>
2026-02-09 16:30:12 -0800

    add read/write specs

--- lib/flux-core/src/ptr/mod.rs
+++ lib/flux-core/src/ptr/mod.rs
@@ -1,4 +1,19 @@
 #[cfg(flux)]
+use flux_attrs::*;
+
+#[cfg(flux)]
 mod const_ptr;
+
 #[cfg(flux)]
 mod mut_ptr;
+
+
+#[cfg(flux)]
+#[extern_spec(core::ptr)]
+#[spec(fn (src: *const{p: p > 0} T) ->  T)]
+fn read<T>(src: *const T) -> T;
+
+#[cfg(flux)]
+#[extern_spec(core::ptr)]
+#[spec(fn (src: *const{p: p > 0} T, src: T))]
+fn write<T>(src: *mut T, src: T);
