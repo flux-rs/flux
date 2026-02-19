3f4f4d1b3b78027230f5ecfb6287aa811aaccd90
Ranjit Jhala <rjhala@ucsd.edu>
2026-02-19 11:56:30 -0800

    add field to flags

--- crates/flux-config/src/flags.rs
+++ crates/flux-config/src/flags.rs
@@ -111,7 +111,7 @@ impl Default for Flags {
 pub(crate) static FLAGS: LazyLock<Flags> = LazyLock::new(|| {
     let mut flags = Flags::default();
     let mut includes: Vec<String> = Vec::new();
-    let mut trusted_patterns: Vec<String> = Vec::new();
+    let mut trusteds: Vec<String> = Vec::new();
     for arg in env::args() {
         let Some((key, value)) = parse_flux_arg(&arg) else { continue };
 
@@ -136,7 +136,7 @@ pub(crate) static FLAGS: LazyLock<Flags> = LazyLock::new(|| {
             "summary" => parse_bool(&mut flags.summary, value),
             "cache" => parse_opt_path_buf(&mut flags.cache, value),
             "include" => parse_opt_include(&mut includes, value),
-            "trusted-pattern" => parse_opt_include(&mut trusted_patterns, value),
+            "trusted-pattern" => parse_opt_include(&mut trusteds, value),
             "verify" => parse_bool(&mut flags.verify, value),
             "full-compilation" => parse_bool(&mut flags.full_compilation, value),
             "trusted" => parse_bool(&mut flags.trusted_default, value),
@@ -160,8 +160,8 @@ pub(crate) static FLAGS: LazyLock<Flags> = LazyLock::new(|| {
         });
         flags.include = Some(include);
     }
-    if !trusted_patterns.is_empty() {
-        let trusted = IncludePattern::new(trusted_patterns).unwrap_or_else(|err| {
+    if !trusteds.is_empty() {
+        let trusted = IncludePattern::new(trusteds).unwrap_or_else(|err| {
             eprintln!("error: invalid trusted pattern: {err}");
             process::exit(1);
         });
