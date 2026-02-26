25cb0d7d64584284ed52f650063844505c812416
Andrew Cheung <a7cheung@ucsd.edu>
2026-02-25 16:56:25 -0800

    Update tests

--- tests/tests/pos/surface/no_panic06.rs
+++ tests/tests/pos/surface/no_panic06.rs
@@ -1,5 +1,17 @@
-#[flux_rs::sig(fn(f: F) -> i32)]
-#[flux_rs::no_panic_if(F::no_panic())]
-fn foo<F: FnOnce(i32) -> i32>(f: F) -> i32 {
+#[flux::sig(fn(f: F) -> i32)]
+#[flux::no_panic_if(F::no_panic())]
+fn foo<F: Fn(i32) -> i32>(f: F) -> i32 {
+    f(3)
+}
+
+#[flux::sig(fn(f: F) -> i32)]
+#[flux::no_panic_if(F::no_panic())]
+fn foo2<F: FnOnce(i32) -> i32>(f: F) -> i32 {
+    f(3)
+}
+
+#[flux::sig(fn(f: F) -> i32)]
+#[flux::no_panic_if(F::no_panic())]
+fn foo3<F: FnMut(i32) -> i32>(mut f: F) -> i32 {
     f(3)
 }
