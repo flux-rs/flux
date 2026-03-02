dde20f04e62b969a3f5e02bb82f781d8b11f46bc
Andrew Cheung <a7cheung@ucsd.edu>
2026-03-02 14:33:40 -0800

    Update the test to be for realsies

--- tests/tests/neg/surface/no_panic07.rs
+++ tests/tests/neg/surface/no_panic07.rs
@@ -5,14 +5,23 @@ trait Ticks {
     fn foo(&self) -> Self;
 }
 
-#[flux::no_panic]
+// `call_ticks_0` and `call_ticks_1` should have identical behavior, but
+// right now, they don't because of the way we handle closures: closures
+// only inherit their `no_panic` status from the first parent who is annotated
+// with `no_panic`, effectively ignoring the `no_panic_if` annotation on `call_ticks_0`.
+// Boo!
+#[flux::sig(fn (_) -> ())]
+#[flux::no_panic_if(true)]
 fn call_ticks_0<T: Ticks>(a: T) {
-    // this (correctly) errors
-    a.foo(); //~ ERROR may panic
+    let x = || {
+        a.foo(); //~ ERROR may panic
+    };
 }
 
-#[flux::no_panic_if(true)]
+#[flux::sig(fn (_) -> ())]
+#[flux::no_panic]
 fn call_ticks_1<T: Ticks>(a: T) {
-    // this (buggy) for now, does not.
-    a.foo(); //~ ERROR may panic
+    let x = || {
+        a.foo(); //~ ERROR may panic
+    };
 }
