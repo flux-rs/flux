9378407ac497ed2e06de55028039e433c538fc58
Andrew Cheung <a7cheung@ucsd.edu>
2026-03-10 16:31:16 -0700

    Remove outdated documentation of test

--- tests/tests/neg/surface/no_panic07.rs
+++ tests/tests/neg/surface/no_panic07.rs
@@ -5,11 +5,6 @@ trait Ticks {
     fn foo(&self) -> Self;
 }
 
-// `call_ticks_0` and `call_ticks_1` should have identical behavior, but
-// right now, they don't because of the way we handle closures: closures
-// only inherit their `no_panic` status from the first parent who is annotated
-// with `no_panic`, effectively ignoring the `no_panic_if` annotation on `call_ticks_0`.
-// Boo!
 #[flux::sig(fn (_) -> ())]
 #[flux::no_panic_if(true)]
 fn call_ticks_0<T: Ticks>(a: T) {
