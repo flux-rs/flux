d52b21151ba1014ff9b78e05199c81da23360da8
Stefan Weigl-Bosker <stefan@s00.xyz>
2026-04-01 13:27:09 -0400

    Add saturating_add/saturating_sub specs for u64

--- lib/flux-core/src/num/mod.rs
+++ lib/flux-core/src/num/mod.rs
@@ -25,3 +25,16 @@ impl u32 {
     #[spec(fn(num: u32, rhs: u32) -> u32[if num + rhs > u32::MAX { u32::MAX } else { num + rhs }])]
     fn saturating_add(self, rhs: u32) -> u32;
 }
+
+#[extern_spec(core::num)]
+impl u64 {
+    /// Core impl: https://github.com/rust-lang/rust/blob/0e95a0f4c677002a5d4ac5bc59d97885e6f51f71/library/core/src/num/uint_macros.rs#L2384-L2400
+    #[no_panic]
+    #[spec(fn(num: u64, rhs: u64) -> u64[if num < rhs { 0 } else { num - rhs }])]
+    fn saturating_sub(self, rhs: u64) -> u64;
+
+    /// Core impl: https://github.com/rust-lang/rust/blob/0e95a0f4c677002a5d4ac5bc59d97885e6f51f71/library/core/src/num/uint_macros.rs#L2340-L2356
+    #[no_panic]
+    #[spec(fn(num: u64, rhs: u64) -> u64[if num + rhs > u64::MAX { u64::MAX } else { num + rhs }])]
+    fn saturating_add(self, rhs: u64) -> u64;
+}
