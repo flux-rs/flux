51df09697d2cd2b4b6f9d7ade3ac35f27971c614
Stefan Weigl-Bosker <stefan@s00.xyz>
2026-04-01 13:46:49 -0400

    Add saturating_add/saturating_sub specs for u64 (#1540)

--- tests/tests/neg/extern_specs/flux_core_num00.rs
+++ tests/tests/neg/extern_specs/flux_core_num00.rs
@@ -17,3 +17,11 @@ pub fn test_saturating_u32(x: u32, y: u32, z: u32) {
     // Not true if x + y > u32::MAX, as it saturates
     assert(x.saturating_add(y) == x + y); //~ ERROR refinement type error
 }
+
+pub fn test_saturating_u64(x: u64, y: u64, z: u64) {
+    // Not true if x < y, as it saturates
+    let sub = x - y; //~ ERROR arithmetic operation may underflow 
+    assert(x.saturating_sub(y) == sub); //~ ERROR refinement type error
+    // Not true if x + y > u64::MAX, as it saturates
+    assert(x.saturating_add(y) == x + y); //~ ERROR refinement type error
+}
--- tests/tests/pos/extern_specs/flux_core_num00.rs
+++ tests/tests/pos/extern_specs/flux_core_num00.rs
@@ -28,3 +28,17 @@ pub fn saturating_u32() {
     // saturating_add: saturated case
     assert(z.saturating_add(y) == u32::MAX);
 }
+
+pub fn test_saturating_u64() {
+    let x: u64 = 5;
+    let y: u64 = 10;
+    let z: u64 = u64::MAX - 5;
+    // saturating_sub: unsaturated case
+    assert(y.saturating_sub(x) == 5);
+    // saturating_sub: saturated case
+    assert(x.saturating_sub(y) == 0);
+    // saturating_add: unsaturated case
+    assert(x.saturating_add(y) == 15);
+    // saturating_add: saturated case
+    assert(z.saturating_add(y) == u64::MAX);
+}
