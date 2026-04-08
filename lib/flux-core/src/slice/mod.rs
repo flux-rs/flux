8d672c2330a1937a1a3db12e0a8ed83ef621090e
Evan Johnson <e5johnso@ucsd.edu>
2026-04-08 11:15:02 -0700

    add 5 specs for slice

--- lib/flux-core/src/slice/mod.rs
+++ lib/flux-core/src/slice/mod.rs
@@ -6,72 +6,51 @@ use flux_attrs::*;
 #[extern_spec(core::slice)]
 impl<T> [T] {
     #[no_panic]
-    #[sig(fn(&Self[@n]) -> usize[n])]
+    #[spec(fn(&Self[@n]) -> usize[n])]
     fn len(&self) -> usize;
 
-    #[sig(fn(&Self[@n]) -> bool[n == 0])]
+    #[spec(fn(&Self[@n]) -> bool[n == 0])]
     fn is_empty(&self) -> bool;
 
-    #[sig(fn(&Self[@n]) -> Option<&T>[n != 0])]
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L154
+    #[spec(fn(&Self[@n]) -> Option<&T>[n != 0])]
     fn first(&self) -> Option<&T>;
 
-    #[sig(fn(&Self[@n]) -> Iter<T>[0, n])]
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L280
+    #[no_panic]
+    #[spec(fn(&Self[@n]) -> Option<&T>[n != 0])]
+    fn last(&self) -> Option<&T>;
+
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L570
+    #[no_panic]
+    #[spec(fn(&Self[@n], I[@idx]) -> Option<&I::Output>[<I as SliceIndex<[T]>>::in_bounds(idx, n)])]
+    fn get<I: SliceIndex<[T]>>(&self, index: I) -> Option<&I::Output>;
+
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L197
+    #[no_panic]
+    #[spec(fn(&Self[@n]) -> Option<(&T, &[T][n - 1])>[n != 0])]
+    fn split_first(&self) -> Option<(&T, &[T])>;
+
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L239
+    #[no_panic]
+    #[spec(fn(&Self[@n]) -> Option<(&T, &[T][n - 1])>[n != 0])]
+    fn split_last(&self) -> Option<(&T, &[T])>;
+
+    #[spec(fn(&Self[@n]) -> Iter<T>[0, n])]
     fn iter(&self) -> Iter<'_, T>;
 
     #[no_panic]
-    #[sig(fn(&Self[@n], mid: usize{mid <= n}) -> (&[T][mid], &[T][n - mid]))]
+    #[spec(fn(&Self[@n], mid: usize{mid <= n}) -> (&[T][mid], &[T][n - mid]))]
     fn split_at(&self, mid: usize) -> (&[T], &[T]);
 
-    #[sig(fn(&Self[@n]) -> *const{p: ptr_size(p) == n} T)]
+    /// Core impl: https://github.com/rust-lang/rust/blob/c871d09d1cc32a649f4c5177bb819646260ed120/library/core/src/slice/mod.rs#L2185
+    #[no_panic]
+    #[spec(fn(&Self[@n], usize[@mid]) -> Option<(&[T][mid], &[T][n - mid])>[mid <= n])]
+    fn split_at_checked(&self, mid: usize) -> Option<(&[T], &[T])>;
+
+    #[spec(fn(&Self[@n]) -> *const{p: ptr_size(p) == n} T)]
     fn as_ptr(&self) -> *const T;
 
-    #[sig(fn(&mut Self[@n]) -> *mut{p: ptr_size(p) == n} T)]
+    #[spec(fn(&mut Self[@n]) -> *mut{p: ptr_size(p) == n} T)]
     fn as_mut_ptr(&mut self) -> *mut T;
-}
-
-#[cfg(flux_sysroot_test)]
-mod tests {
-    #![allow(dead_code)]
-
-    use flux_attrs::*;
-
-    #[sig(fn(bool[true]))]
-    fn assert(_: bool) {}
-
-    #[should_fail]
-    fn test01(xs: &[i32]) {
-        let _x = xs[0];
-    }
-
-    fn test_len(xs: &[i32]) {
-        if xs.len() > 0 {
-            let _x = xs[0];
-        }
-    }
-
-    fn test_is_empty(xs: &[i32]) {
-        if !xs.is_empty() {
-            let _x = xs[0];
-        }
-    }
-
-    fn test_first00(xs: &[i32]) {
-        if xs.len() > 0 {
-            assert(xs.first().is_some());
-        }
-        if xs.is_empty() {
-            assert(xs.first().is_none());
-        }
-    }
-
-    #[should_fail]
-    fn test_first01(xs: &[i32]) {
-        assert(xs.first().is_some());
-    }
-
-    fn test_iter00(xs: &[i32]) {
-        let it = xs.iter();
-        let ys = it.as_slice();
-        assert(xs.len() == ys.len());
-    }
 }
