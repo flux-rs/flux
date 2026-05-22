ad94c76bb57e7745eb0a346c5a61ad22b06c99e1
Nico Lehmann <nico.lehmannm@gmail.com>
2026-05-22 16:28:18 -0400

    Remove lib tests (#1618)

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,5 +1,4 @@
 pub const FLUX_SYSROOT: &str = "FLUX_SYSROOT";
-pub const FLUX_SYSROOT_TEST: &str = "FLUX_SYSROOT_TEST";
 
 /// Rustc flags to pass Flux when running tests
 pub fn default_flags() -> Vec<String> {
