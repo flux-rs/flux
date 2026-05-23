7c5ae1e19d008db36de363871406dfd789e5e37d
Ranjit Jhala <rjhala@ucsd.edu>
2026-05-23 15:53:19 -0700

    add indexmut too

--- lib/flux-core/src/array/mod.rs
+++ lib/flux-core/src/array/mod.rs
@@ -21,3 +21,21 @@ where
     #[sig(fn(&Self, {I[@idx] | <[T] as Index<I>>::in_bounds(N, idx)}) -> &<[T; N] as Index<I>>::Output{out: <[T] as Index<I>>::output_pred(N, idx, out)})]
     fn index(&self, index: I) -> &<[T; N] as Index<I>>::Output;
 }
+
+#[extern_spec(core::array)]
+impl<T, I, const N: usize> const IndexMut<I> for [T; N]
+where
+    [T]: IndexMut<I>,
+{
+    #![assoc(
+        fn in_bounds(len: (), idx: I) -> bool {
+            <[T] as Index<I>>::in_bounds(N, idx)
+        }
+
+        fn output_pred(len: (), idx: I, out: <[T] as Index<I>>::Output) -> bool {
+            <[T] as Index<I>>::output_pred(N, idx, out)
+        }
+    )]
+
+    fn index_mut(&mut self, index: I) -> &mut <[T; N] as IndexMut<I>>::Output
+}
