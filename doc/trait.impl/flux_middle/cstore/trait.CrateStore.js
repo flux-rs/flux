753e4857db10e2bbad9774c81b0a4e93069159f8
nilehmann <nico.lehmannm@gmail.com>
2026-06-05 15:07:47 +0000

    Deploying to gh-pages from @ flux-rs/flux@9dece888fa1208ffd6b2e48fcaf8f5f98a68bf36 🚀

--- doc/trait.impl/flux_middle/cstore/trait.CrateStore.js
+++ doc/trait.impl/flux_middle/cstore/trait.CrateStore.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["flux_metadata",[["impl CrateStore for <a class=\"struct\" href=\"flux_metadata/struct.CStore.html\" title=\"struct flux_metadata::CStore\">CStore</a>"]]]]);
+    var implementors = Object.fromEntries([["flux_metadata",[["impl <a class=\"trait\" href=\"flux_middle/cstore/trait.CrateStore.html\" title=\"trait flux_middle::cstore::CrateStore\">CrateStore</a> for <a class=\"struct\" href=\"flux_metadata/struct.CStore.html\" title=\"struct flux_metadata::CStore\">CStore</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[155]}
+//{"start":57,"fragment_lengths":[276]}
