80e8d5f6412fbe86610631aa0659319ab32e1bb3
Ranjit Jhala <rjhala@ucsd.edu>
2026-06-12 14:18:45 -0700

    temp chekin

--- tests/tests/pos/surface/hof00.rs
+++ tests/tests/pos/surface/hof00.rs
@@ -12,18 +12,9 @@
         cmp0(n, lt)
     }
 
-    fn pos_lam(n: int) -> bool {
-        cmp0(n, |x, y| { x < y } )
-    }
-
-  )]
+)]
 
 #[flux::spec(fn() -> i32{v:pos(v)})]
 fn test0() -> i32 {
-    29
-}
-
-#[flux::spec(fn() -> i32{v:pos_lam(v)})]
-fn test1() -> i32 {
     29
 }
