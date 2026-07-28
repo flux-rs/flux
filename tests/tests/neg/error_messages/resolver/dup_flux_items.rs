b9b6d36bbe83444933300584c0f40aba983d9625
Nico Lehmann <nico.lehmannm@gmail.com>
2026-07-28 15:07:44 -0400

    add tests

--- tests/tests/neg/error_messages/resolver/dup_flux_items.rs
+++ tests/tests/neg/error_messages/resolver/dup_flux_items.rs
@@ -5,6 +5,10 @@ defs! {
 
     fn foo() -> int; //~ ERROR name `foo` is defined multiple times
 
+    opaque sort Bag;
+
+    opaque sort Bag; //~ ERROR name `Bag` is defined multiple times
+
     // Qualifiers are checked in a separate, crate-global namespace, so this doesn't clash
     // with `foo` above.
     qualifier foo(x: int) {
@@ -13,5 +17,27 @@ defs! {
 
     qualifier foo(x: int) { //~ ERROR name `foo` is defined multiple times
         x > 0
+    }
+}
+
+// Qualifiers (and primop-props) are global: `bar` clashes across module boundaries too, unlike
+// funcs/sorts which are scoped per-module.
+mod mod_a {
+    use flux_attrs::*;
+
+    defs! {
+        qualifier bar(x: int) {
+            x > 0
+        }
+    }
+}
+
+mod mod_b {
+    use flux_attrs::*;
+
+    defs! {
+        qualifier bar(x: int) { //~ ERROR name `bar` is defined multiple times
+            x > 0
+        }
     }
 }
