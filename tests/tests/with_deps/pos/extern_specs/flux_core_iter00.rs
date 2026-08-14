d31f9de9c173fb1149906ffd006886fccb04d115
Ranjit Jhala <rjhala@ucsd.edu>
2026-08-14 10:11:30 -0700

    add test for Take

--- tests/tests/with_deps/pos/extern_specs/flux_core_iter00.rs
+++ tests/tests/with_deps/pos/extern_specs/flux_core_iter00.rs
@@ -16,3 +16,19 @@ pub fn test_position_safe_index(xs: &[i32]) {
         let _ = xs[i];
     }
 }
+
+// --- take ---
+
+#[flux_rs::spec(fn(target: &mut [i32][@len], n: usize{n <= len}, iter: I))]
+pub fn test_take<I>(target: &mut [i32], n: usize, iter: I)
+where
+    I: IntoIterator<Item = i32>,
+{
+    let mut iter = iter.into_iter();
+    let mut pushed = n;
+    let to_take = target.len() - pushed;
+    for element in iter.by_ref().take(to_take) {
+        target[pushed] = element;
+        pushed += 1;
+    }
+}
