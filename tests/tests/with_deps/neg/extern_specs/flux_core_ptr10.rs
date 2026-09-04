bf779cf94a316ab0fb739c47a84a4ba7a8e7f917
ryanfanchiotti <ryan@rur.com>
2026-09-04 14:32:45 -0400

    tests for pointer comparisons

--- tests/tests/with_deps/neg/extern_specs/flux_core_ptr10.rs
+++ tests/tests/with_deps/neg/extern_specs/flux_core_ptr10.rs
@@ -8,3 +8,65 @@ pub fn test_ptr_neq(p: *mut i32) {
     let p1 = p;
     assert(!(p1.eq(&p))) //~ ERROR refinement type error
 }
+
+#[flux::spec(fn(p1: *const[@base, @addr, @size] i32, p2: *const[base, 0, size] i32))]
+pub fn test_ptr_id(p1: *const i32, p2: *const i32) {
+    assert(p1.eq(&p2)) //~ ERROR refinement type error
+}
+
+// --- ne ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_neq_sym(p: *const i32) {
+    unsafe {
+        let p1 = p.add(1);
+        let p0 = p1.sub(1);
+        assert(p != p0) //~ ERROR refinement type error
+    }
+}
+
+#[flux::spec(fn(p1: *const[@base, @addr, @size] i32, p2: *const[base, addr, size] i32))]
+pub fn test_ptr_id_sym(p1: *const i32, p2: *const i32) {
+    assert(p1 != p2) //~ ERROR refinement type error
+}
+
+// --- lt ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_lt(p: *const i32) {
+    unsafe {
+        let p1 = p.add(1);
+        assert(p1 < p) //~ ERROR refinement type error
+    }
+}
+
+// --- le ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_le(p: *const i32) {
+    unsafe {
+        let p1 = p.add(1);
+        assert(p1 <= p) //~ ERROR refinement type error
+    }
+}
+
+// --- gt ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 40}))]
+pub fn test_ptr_gt(p: *const i32) {
+    unsafe {
+        let p1 = p.add(4);
+        assert(p > p1) //~ ERROR refinement type error
+    }
+}
+
+// --- ge ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 16}))]
+pub fn test_ptr_ge(p: *const i32) {
+    unsafe {
+        let p1 = p.add(2);
+        let p2 = p1.sub(1);
+        assert(p2 >= p1) //~ ERROR refinement type error
+    }
+}
--- tests/tests/with_deps/pos/extern_specs/flux_core_ptr11.rs
+++ tests/tests/with_deps/pos/extern_specs/flux_core_ptr11.rs
@@ -4,7 +4,7 @@ use flux_rs::assert;
 
 // --- eq ---
 
-#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && addr > 0 && size == 8 && addr % 4 == 0}))]
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
 pub fn test_ptr_eq(p: *const i32) {
     unsafe {
         let p1 = p.add(1);
@@ -13,7 +13,7 @@ pub fn test_ptr_eq(p: *const i32) {
     }
 }
 
-#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && addr > 0 && size == 8 && addr % 4 == 0}))]
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
 pub fn test_ptr_eq_sym(p: *const i32) {
     unsafe {
         let p1 = p.add(1);
@@ -30,4 +30,44 @@ pub fn test_ptr_id_sym(p1: *const i32, p2: *const i32) {
 #[flux::spec(fn(p1: *const[@base, @addr, @size] i32, p2: *const[base, addr, size] i32))]
 pub fn test_ptr_id(p1: *const i32, p2: *const i32) {
     assert(p1.eq(&p2))
+}
+
+// --- lt ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_lt(p: *const i32) {
+    unsafe {
+        let p1 = p.add(1);
+        assert(p < p1)
+    }
+}
+
+// --- le ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_le(p: *const i32) {
+    unsafe {
+        let p1 = p.add(1);
+        assert(p <= p1)
+    }
+}
+
+// --- gt ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 40}))]
+pub fn test_ptr_gt(p: *const i32) {
+    unsafe {
+        let p1 = p.add(4);
+        assert(p1 > p)
+    }
+}
+
+// --- ge ---
+
+#[flux::spec(fn (ptr: {*const[@base, @addr, @size] i32 | addr >= base && size == 8}))]
+pub fn test_ptr_ge(p: *const i32) {
+    unsafe {
+        let p1 = p.add(0);
+        assert(p1 >= p)
+    }
 }
