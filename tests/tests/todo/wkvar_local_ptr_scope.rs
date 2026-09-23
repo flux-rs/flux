bbc84fcb327d9717a1f89d255550e6c1f8f37146
Cole K <cole-k@users.noreply.github.com>
2026-09-22 19:41:44 -0700

    add repro for nico

---
+++ tests/tests/todo/wkvar_local_ptr_scope.rs
@@ -0,0 +1,48 @@
+// compile-flags: -Fsuggestions-z3=process
+// Run with `cargo x --suggestions run tests/tests/todo/wkvar_local_ptr_scope.rs -- -Fsuggestions-z3=process`.
+#[path = "../lib/rvec.rs"]
+mod rvec;
+use rvec::RVec;
+
+#[derive(Clone, Copy)]
+enum SubscriptionInner {
+    Clock,
+    Fd,
+}
+
+#[derive(Clone, Copy)]
+struct Subscription {
+    userdata: u64,
+    subscription_inner: SubscriptionInner,
+}
+
+#[flux::trusted]
+#[flux::sig(fn() -> Result<Subscription, ()>)]
+fn read_subscription() -> Result<Subscription, ()> {
+    Err(())
+}
+
+pub fn poll_parse_clock(timeouts: &mut RVec<(u64, u64)>, userdata: u64) -> Result<(), ()> {
+    timeouts.push((userdata, userdata));
+    Ok(())
+}
+
+fn poll_parse_fds() -> Result<(), ()> {
+    Ok(())
+}
+
+#[flux::sig(fn(tos: &strg RVec<(u64, u64)>[@timeouts_old]) -> Result<(), ()>
+    ensures tos: RVec<(u64, u64)>[#timeouts_new]
+)]
+pub fn parse_subscriptions(
+    timeouts: &mut RVec<(u64, u64)>,
+) -> Result<(), ()> {
+    let subscription = read_subscription()?;
+    match subscription.subscription_inner {
+        SubscriptionInner::Clock => {
+            poll_parse_clock(timeouts, subscription.userdata)?;
+        }
+        SubscriptionInner::Fd => poll_parse_fds()?,
+    }
+    Ok(())
+}
